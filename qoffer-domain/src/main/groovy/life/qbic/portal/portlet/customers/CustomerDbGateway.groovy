8857b9afd360c6c37a6394e1fceafce577f8ac11
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-07-31 13:28:04 +0200

    Add empty classes
    This commits creates empty classes and their structure.
    
    Descriptions of the classes will follow in following commits

---
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/CustomerDbGateway.groovy
@@ -0,0 +1,15 @@
+package life.qbic.portal.portlet.customers
+
+
+/**
+ * <short description>
+ *
+ * <detailed description>
+ *
+ * @since: <versiontag>
+ * @author: Tobias Koch
+ * * */
+
+interface CustomerDbGateway {
+
+}
---
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/create/CreateCustomerOutput.groovy
@@ -0,0 +1,13 @@
+package life.qbic.portal.portlet.customers.create
+/**
+ * <short description>
+ *
+ * <detailed description>
+ *
+ * @since: 1.0.0
+ * @author: Tobias Koch
+ *
+ **/
+interface CreateCustomerOutput {
+
+}
