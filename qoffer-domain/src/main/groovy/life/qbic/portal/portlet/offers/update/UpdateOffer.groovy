6d943dd8da81d0427fcdfdeda7362e16bc0f4adf
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-07-31 13:28:04 +0200

    Add empty classes
    This commits creates empty classes and their structure.
    
    Descriptions of the classes will follow in following commits

---
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/offers/update/UpdateOffer.groovy
@@ -0,0 +1,13 @@
+package life.qbic.portal.portlet.offers.update
+
+/**
+ * <short description>
+ *
+ * <detailed description>
+ *
+ * @since: <versiontag>
+ * @author: Tobias Koch
+ *
+ * * */
+class UpdateOffer {
+}
