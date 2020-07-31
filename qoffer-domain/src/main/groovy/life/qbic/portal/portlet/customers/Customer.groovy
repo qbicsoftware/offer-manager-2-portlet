a681ceea584ad16c4d73cc8e03ed0cf67c46e903
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-07-31 16:44:12 +0200

    Add buisness data objects
    These objects are meant to be handled inside the application. Only DTOs should cross outside the module.

---
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/Customer.groovy
@@ -0,0 +1,12 @@
+package life.qbic.portal.portlet.customers
+
+/**
+ * An object containing logic associated with a customer
+ *
+ * This object represents a customer. It does also provide operations on a customer.
+ *
+ * @since: 1.0.0
+ * @author: Tobias Koch
+ */
+class Customer {
+}
---
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/offers/Offer.groovy
@@ -0,0 +1,12 @@
+package life.qbic.portal.portlet.offers
+
+/**
+ * <short description>
+ *
+ * <detailed description>
+ *
+ * @since: 1.0.0
+ * @author: Tobias Koch
+ */
+class Offer {
+}
