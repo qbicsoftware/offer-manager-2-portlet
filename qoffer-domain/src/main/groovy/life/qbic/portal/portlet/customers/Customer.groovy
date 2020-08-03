0f2b96627f6aabc0b7a1affb674fa4ce96ce5518
Tobias Koch <KochTobi@users.noreply.github.com>
2020-08-03 09:42:55 +0200

    Add detailed description to the Customer.groovy
    Co-authored-by: Sven F. <sven.fillinger@qbic.uni-tuebingen.de>

--- qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/Customer.groovy
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/Customer.groovy
@@ -1,9 +1,12 @@
 package life.qbic.portal.portlet.customers
 
 /**
- * An object containing logic associated with a customer
+ * A QBiC customer in the context of finance and accounting.
  *
- * This object represents a customer. It does also provide operations on a customer.
+ * This object represents a business customer at QBiC. A customer usually contains one or more affiliations.
+ * A customer is created when a research project request is sent to QBiC in order to request an offer for it.
+ * 
+ * Customer objects are immutable and changes can only be made, with the according database interface methods (link here)
  *
  * @since: 1.0.0
  * @author: Tobias Koch
