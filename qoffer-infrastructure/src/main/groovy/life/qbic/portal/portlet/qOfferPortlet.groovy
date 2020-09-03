924ea70ad90e167168ffb1afe1f28b40bb8a896c
Steffengreiner <Steffen.Greiner@gmx.de>
2020-09-03 15:00:44 +0200

    Removed Controller from both classes and add formlayout class to Portlet instead of gridlayout

--- qoffer-infrastructure/src/main/groovy/life/qbic/portal/portlet/qOfferPortlet.groovy
+++ qoffer-infrastructure/src/main/groovy/life/qbic/portal/portlet/qOfferPortlet.groovy
@@ -2,11 +2,10 @@ package life.qbic.portal.portlet
 import com.vaadin.annotations.Theme
 import com.vaadin.annotations.Widgetset
 import com.vaadin.server.VaadinRequest
+import com.vaadin.ui.HorizontalLayout
 import com.vaadin.ui.Layout
-import com.vaadin.ui.VerticalLayout
 import groovy.transform.CompileStatic
 import groovy.util.logging.Log4j2
-import life.qbic.Controller
 import life.qbic.ViewModel
 
 /**
@@ -22,15 +21,17 @@ import life.qbic.ViewModel
 @Log4j2
 @CompileStatic
 class qOfferPortlet extends QBiCPortletUI {
-    Controller controller
-    ViewModel viewmodel
-
+    ViewModel viewModel
     private CreateCustomerView createCustomerView
     @Override
     protected Layout getPortletContent(final VaadinRequest request) {
-        VerticalLayout layout = new VerticalLayout()
+        HorizontalLayout layout = new HorizontalLayout()
+        layout.setSpacing(true);
+        layout.setMargin(true);
+        layout.setWidth("100%");
+        layout.setHeight("100%");
 
-        def customview = new CreateCustomerView(controller, viewmodel)
+        def customview = new CreateCustomerView(viewModel)
         layout.addComponent(customview)
         return layout
 
