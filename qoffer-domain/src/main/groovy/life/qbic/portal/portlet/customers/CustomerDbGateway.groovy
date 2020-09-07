143e899ea5d5f285645db2e9bf988691f1c4920e
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2020-09-07 14:45:11 +0200

    update methods of CustomerDbGatewy

--- qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/CustomerDbGateway.groovy
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/CustomerDbGateway.groovy
@@ -1,7 +1,7 @@
 package life.qbic.portal.portlet.customers
 
-import life.qbic.datamodel.persons.Person
 
+import life.qbic.portal.portlet.offers.SearchCriteria
 
 /**
  * A gateway to access information from a customer database
@@ -9,25 +9,33 @@ import life.qbic.datamodel.persons.Person
  * This class specifies how the application can access external resources.
  * It is meant to be implemented outside the domain layer.
  *
- * @since: 1.0.0
- * @author: Tobias Koch
+ * @since: 1.0.0* @author: Tobias Koch
  *
  */
 interface CustomerDbGateway {
 
-        /**
-         *
-         * @param criteria a map with search criteria
-         * @return a person with affiliation and contact information
-         * @see life.qbic.datamodel.persons.Person* @since 1.0.0
-        */
-        Person searchCustomer(Map criteria)
+    /**
+     * This method returns a customer matching the given search criteria
+     *
+     * @param criteria a map with search criteria
+     * @return a person with affiliation and contact information
+     */
+    List<Customer> findCustomer(SearchCriteria criteria)
 
-        /**
-         *
-         * @param customer a person to be added to known customers
-         * @see life.qbic.datamodel.persons.Person* @since 1.0.0
-        */
-        void saveCustomer(Person customer)
+    /**
+     * Adds a customer to the user database
+     *
+     * @param customer a person to be added to known customers
+     */
+    void addCustomer(Customer customer)
+
+
+    /**
+     * Updates the information of a given customer specified by a customer ID
+     *
+     * @param customerId to specify the customer to be updated
+     * @param updatedCustomer customer containing all information and the updates of a customer
+     */
+    void updateCustomer(String customerId, Customer updatedCustomer)
 
 }
