8a004142b262a5fcde16e56ff028ffe2fea47522
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2020-09-11 14:12:10 +0200

    add missing files

---
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/CriteriaType.groovy
@@ -0,0 +1,25 @@
+package life.qbic.portal.portlet
+
+/**
+ * This enum represents all criteria for which a customer can be searched
+ *
+ * A customer can be searched based on
+ *
+ * @since: <versiontag>
+ * @author: Jennifer Bödker
+ *
+ */
+enum CriteriaType {
+
+    LAST_NAME("last name"),
+    ADD_ADDRESS("additional address"),
+    GROUP_NAME("group name"),
+    CITY("city")
+
+    final String value
+
+    CriteriaType(String value){
+        this.value = value
+    }
+
+}
---
+++ qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/customers/CustomerDatabaseQueries.groovy
@@ -0,0 +1,120 @@
+package life.qbic.portal.qoffer2.customers
+
+import life.qbic.portal.portlet.customers.Customer
+import life.qbic.portal.qoffer2.database.DatabaseSession
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import java.sql.SQLException
+
+/**
+ * This class contains all queries on the customer database
+ *
+ * All database queries for the customer database are collected here. Only here the {@link DatabaseSession} is used to connect
+ * to the database
+ *
+ * @since: 1.0.0
+ * @author: Jennifer Bödker
+ *
+ */
+class CustomerDatabaseQueries {
+
+    Connection databaseConnection
+
+    private static final Logger LOG = LogManager.getLogger(CustomerDatabaseQueries.class)
+
+    CustomerDatabaseQueries(Connection connection){
+        this.databaseConnection = connection
+    }
+
+    /**
+     * Searches for a customer based on its last name
+     *
+     * @param lastName of the customer
+     * @return a list of customers with a matching last name
+     */
+    List<Customer> findCustomerByName(String lastName){
+        List<String> res = []
+        try{
+            String sql = "SELECT id from persons WHERE family_name = ?"
+
+            PreparedStatement statement = null
+            try{
+                statement = databaseConnection.prepareStatement(sql)
+                statement.setString(1, lastName)
+                ResultSet rs = statement.executeQuery()
+                println rs
+
+                if (rs.next()) {
+                    println rs.getString(1)
+                    res << rs.getString(1)
+                }
+            } catch (SQLException e) {
+                LOG.error("SQL operation unsuccessful: " + e.getMessage())
+                e.printStackTrace()
+            } finally {
+                DatabaseSession.logout(databaseConnection)
+            }
+            return null
+        }catch(RuntimeException e) {
+            LOG.error e
+            DatabaseSession.logout(databaseConnection)
+        }
+        return null
+    }
+
+    /**
+     * Searches for a customer based on an additional address, which can be either an department or an institute
+     *
+     * @param addAddress of the customer specifying his location
+     * @return a list of customers with a matching additional address
+     */
+    List<Customer> findCustomerByAdditionalAddress(String addAddress){
+
+        return null
+    }
+
+    /**
+     * Searches for a customer based on a city in which the customers group is located
+     *
+     * @param cityName of the customer specifying his location
+     * @return a list of customers with a matching city
+     */
+    List<Customer> findCustomerByCity(String cityName){
+
+        return null
+    }
+
+    /**
+     * Searches for a customer based on a e.g research group
+     *
+     * @param groupName of the group of which the customer is part of
+     * @return a list of customers with a matching group name
+     */
+    List<Customer> findCustomerByGroup(String groupName){
+
+        return null
+    }
+
+    /**
+     * Add a customer to the database
+     *
+     * @param customer which needs to be added to the database
+     */
+    void addCustomer(Customer customer){
+
+    }
+
+    /**
+     * Searches for a customer by its ID and updates the customer information according to the new information
+     *
+     * @param customerId which identifies the user to be updated
+     * @param updatedInformation containing the new information about the user
+     */
+    void updateCustomer(String customerId, Customer updatedInformation){
+
+    }
+}
