d4d2609f2bf4e4d101f300b72af0f157ddcf6842
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2020-09-11 14:25:07 +0200

    Revert "Revert "add missing files""
    This reverts commit cd1d05672e3da45ad1f97aeeb5b3ae1031d200b3.

---
+++ qoffer-infrastructure/src/test/java/life/qbic/portal/portlet/qOfferPortletIntegrationTest.java
@@ -0,0 +1,15 @@
+package life.qbic.portal.portlet;
+
+import life.qbic.portal.qoffer2.QOfferPortlet;
+import org.junit.Test;
+
+/**
+ * Tests for {@link QOfferPortlet}.
+ */
+public class qOfferPortletIntegrationTest {
+
+    @Test
+    public void testPortletConnectsToRemoteServer() {
+        
+    }
+}
