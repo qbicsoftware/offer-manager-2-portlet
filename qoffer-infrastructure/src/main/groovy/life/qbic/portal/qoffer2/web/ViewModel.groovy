2e56e1fb2670d5d44283e0093885dab1cb070c57
Steffengreiner <Steffen.Greiner@gmx.de>
2020-09-14 09:29:20 +0200

    Add second Affiliation to check if combobox selection of affiliation in Crea<teCustomerview works

--- qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/ViewModel.groovy
+++ qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/ViewModel.groovy
@@ -25,7 +25,8 @@ class ViewModel {
               List<String> failureNotifications) {
         //this.affiliations = new ObservableList(affiliations)
         Affiliation affiliation = new Affiliation(5,"groupname","acronym","organihzation","institute","faculty","contact","head","street","zip","city","country","webpage")
-        this.affiliations = new ObservableList([affiliation,affiliation,affiliation])
+        Affiliation affiliation2 = new Affiliation(4,"groupname2","acronym2","organihzation2","institute2","faculty2","contact2","head2","street2","zip2","city2","country2","webpage2")
+        this.affiliations = new ObservableList([affiliation,affiliation2])
         this.successNotifications = new ObservableList(successNotifications)
         this.failureNotifications = new ObservableList(failureNotifications)
     }
