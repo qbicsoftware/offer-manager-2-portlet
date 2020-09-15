39bdcd2969bb8a1f5c84f42721e470ea66571471
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-09-15 13:48:30 +0200

    Indicate that db gateway methods throw exceptions

--- qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/CustomerDbGateway.groovy
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/CustomerDbGateway.groovy
@@ -2,6 +2,7 @@ package life.qbic.portal.portlet.customers
 
 
 import life.qbic.portal.portlet.SearchCriteria
+import life.qbic.portal.portlet.exceptions.DatabaseQueryException
 
 /**
  * A gateway to access information from a customer database
@@ -27,16 +28,18 @@ interface CustomerDbGateway {
      * Adds a customer to the user database
      *
      * @param customer a person to be added to known customers
+     * @throws DatabaseQueryException When a customer could not been added to the customer database
      */
-    void addCustomer(Customer customer)
-
+    void addCustomer(Customer customer) throws DatabaseQueryException
 
     /**
      * Updates the information of a given customer specified by a customer ID
      *
      * @param customerId to specify the customer to be updated
      * @param updatedCustomer customer containing all information and the updates of a customer
+     * @throws DatabaseQueryException When a customer could not been updated in the customer
+     * database
      */
-    void updateCustomer(String customerId, Customer updatedCustomer)
+    void updateCustomer(String customerId, Customer updatedCustomer) throws DatabaseQueryException
 
 }
