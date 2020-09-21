c0ffe57b1b9cfb519043e5eedc5fd5a21c6d32b3
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2020-09-21 17:02:25 +0200

    Remove listeners from controller and make controller simple adapter

--- qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/controllers/CreateCustomerController.groovy
+++ qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/controllers/CreateCustomerController.groovy
@@ -1,16 +1,9 @@
 package life.qbic.portal.qoffer2.web.controllers
 
-import com.vaadin.data.ValidationResult
-import com.vaadin.data.ValueContext
-import com.vaadin.data.validator.EmailValidator
-import com.vaadin.data.validator.StringLengthValidator
-import com.vaadin.server.UserError
 
 import life.qbic.datamodel.dtos.business.Affiliation
 import life.qbic.datamodel.dtos.business.Customer
 import life.qbic.portal.portlet.customers.create.CreateCustomerInput
-import life.qbic.portal.qoffer2.web.views.CreateCustomerView
-
 
 /**
  * Controller class responsible for the data flow into qoffer-2
@@ -22,90 +15,15 @@ import life.qbic.portal.qoffer2.web.views.CreateCustomerView
  */
 class CreateCustomerController {
 
-    CreateCustomerView view
     CreateCustomerInput useCaseInput
 
-    private String firstName
-    private String lastName
-    private String title = "title"
-    private String email
-    private Affiliation affiliation
-
-    CreateCustomerController(CreateCustomerView view, CreateCustomerInput useCaseInput) {
-        this.view = view
+    CreateCustomerController(CreateCustomerInput useCaseInput) {
         this.useCaseInput = useCaseInput
-        setupViewElements()
     }
 
-    private void setupViewElements(){
-        //Add Listeners to all Fields in the Formlayout
-        view.firstNameField.addValueChangeListener({ event ->
-            ValidationResult result = new StringLengthValidator("Please input a valid first " +
-                "Name", 1, null).apply(event.getValue(), new ValueContext(view.firstNameField))
-            if (result.isError()) {
-                UserError error = new UserError(result.getErrorMessage())
-                view.firstNameField.setComponentError(error)
-                view.firstName = null
-            } else {
-                view.firstNameField.setComponentError(null)
-                view.firstName = event.getValue().toString()
-
-                firstName = event.getValue().toString()
-            }
-        })
-
-        view.lastNameField.addValueChangeListener({ event ->
-            ValidationResult result = new StringLengthValidator("Please input a valid last Name",
-                1, null).apply(event.getValue(), new ValueContext(view.lastNameField))
-            if (result.isError()) {
-                UserError error = new UserError(result.getErrorMessage())
-                view.lastNameField.setComponentError(error)
-                view.lastName = null
-            } else {
-                view.lastNameField.setComponentError(null)
-                view.lastName = event.getValue().toString()
-                view.customerInfo.put("Last Name", view.lastName)
-
-                lastName = event.getValue().toString()
-            }
-        })
-
-        view.emailField.addValueChangeListener({ event ->
-            ValidationResult result = new EmailValidator("Please input a valid email address")
-                .apply(event.getValue(), new ValueContext(view.emailField))
-            if (result.isError()) {
-                UserError error = new UserError(result.getErrorMessage())
-                view.email = null
-                view.emailField.setComponentError(error)
-            } else {
-                view.emailField.setComponentError(null)
-                view.email = event.getValue().toString()
-                view.customerInfo.put("email", view.email)
-
-                email = event.getValue().toString()
-            }
-        })
-
-        view.affiliationComboBox.addSelectionListener({ event ->
-            view.affiliation = event.getValue() as Affiliation
-            view.customerInfo.put("Affiliation", view.affiliation)
-
-            affiliation = event.getValue() as Affiliation
-        })
-
-        view.submitButton.addClickListener({ event ->
-            createNewCustomer()
-        })
-
-    }
-
-    private void createNewCustomer() {
-        Customer customer = new Customer(
-            view.firstName,
-            view.lastName,
-            "None",
-            view.email,
-            [view.affiliationComboBox.getValue() as Affiliation])
+    public void createNewCustomer(String firstName, String lastName, String title, String email, List<? extends Affiliation> affiliations) {
+        //todo add title to the dto
+        Customer customer = new Customer(firstName, lastName, title, email, affiliations as List<Affiliation>)
         this.useCaseInput.createCustomer(customer)
     }
 }
