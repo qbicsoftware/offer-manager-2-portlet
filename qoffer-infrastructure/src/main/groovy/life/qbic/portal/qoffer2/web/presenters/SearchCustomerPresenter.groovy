e2ed91964a351a0409fd22a4f26a8770b81ea135
Steffengreiner <Steffen.Greiner@gmx.de>
2020-10-30 16:24:26 +0100

    Implement SearchCustomerPresenter which provides a dummy list to emulate the backend connection

--- qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/presenters/SearchCustomerPresenter.groovy
+++ qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/presenters/SearchCustomerPresenter.groovy
@@ -1,12 +1,39 @@
 package life.qbic.portal.qoffer2.web.presenters
 
+import life.qbic.datamodel.dtos.business.AcademicTitle
+import life.qbic.datamodel.dtos.business.Customer
+import life.qbic.portal.qoffer2.web.viewmodel.SearchCustomerViewModel
+import life.qbic.portal.qoffer2.web.viewmodel.ViewModel
+
 /**
- * <class short description - One Line!>
- * 
- * <More detailed description - When to use, what it solves, etc.>
+ * Presenter for the SearchCustomerView
  *
- * @since: <version tag>
+ * This presenter handles the output of the Search Customer use case and prepares it for the
+ * SearchCustomerView
  *
+ * @since: 1.0.0
  */
 class SearchCustomerPresenter {
+    private ViewModel viewModel
+    private SearchCustomerViewModel searchCustomerViewModel
+
+    SearchCustomerPresenter(ViewModel viewModel, SearchCustomerViewModel searchCustomerViewModel) {
+        this.viewModel = viewModel
+        this.searchCustomerViewModel = searchCustomerViewModel
+        //ToDo Remove this list as soon as the backend is implemented
+        List<Customer> customerList = new ArrayList<Customer>()
+        def customer1 = new Customer("Ash", "Ketchum", AcademicTitle.NONE, "gottacatchem@all.de", [])
+        def customer2 = new Customer("Samuel", "Oak", AcademicTitle.PROFESSOR, "giveMeMyPokemon@geezer.de", [])
+        def customer3 = new Customer("Pikachu", "Pichu", AcademicTitle.DOCTOR, "Pokdex@25.de", [])
+        customerList.add(customer1)
+        customerList.add(customer2)
+        customerList.add(customer3)
+        reportFoundCustomers(customerList)
+    }
+
+    void reportFoundCustomers(List<Customer> customers) {
+        searchCustomerViewModel.customers.clear()
+        searchCustomerViewModel.customers.addAll(customers)
+    }
+
 }
