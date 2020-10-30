06871c033084fdab8ebbe471fb910999ff76b3d3
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2020-10-30 14:36:45 +0100

    add method description

--- qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/search/SearchCustomerInput.groovy
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/search/SearchCustomerInput.groovy
@@ -10,5 +10,11 @@ package life.qbic.portal.portlet.customers.search
  */
 interface SearchCustomerInput {
 
+    /**
+     * This method triggers the search for a customer based on the full name
+     * @param firstName of the customer
+     * @param lastName of the customer
+     * @since 1.0.0
+     */
     void searchCustomer(String firstName, String lastName)
 }
