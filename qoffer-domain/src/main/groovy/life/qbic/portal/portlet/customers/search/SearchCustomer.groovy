1862542c6026be1e6a06f5b45f46f97a65dacb94
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-11-09 10:15:54 +0100

    Submit success, if search result has hits.

--- qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/search/SearchCustomer.groovy
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/search/SearchCustomer.groovy
@@ -24,20 +24,15 @@ class SearchCustomer implements SearchCustomerInput{
 
     @Override
     void searchCustomer(String firstName, String lastName) {
-
         try {
             List<Customer> foundCustomer = dataSource.findCustomer(firstName, lastName)
-            int numberOfFoundCustomers = foundCustomer.size()
-            // Todo implement interface method
-            //output.successNotification("Found $numberOfFoundCustomers customers matching
-            // $firstName $lastName")
             if (foundCustomer.isEmpty()) {
                 output.failNotification("Could not find a customer for $firstName $lastName")
+            } else {
+                output.successNotification(foundCustomer)
             }
         } catch (Exception ignored) {
             output.failNotification("Unexpected error when searching for the customer $firstName $lastName")
         }
-
-
     }
 }
