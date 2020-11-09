fe82accaaeeb76ac3f21df486c5decccaf88af17
Sven F <sven.fillinger@qbic.uni-tuebingen.de>
2020-11-09 10:28:45 +0100

    Refactor use case search customer (#87)
    This PR does some small refactoring for the use case output of SearchCustomer.

--- qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/search/SearchCustomerOutput.groovy
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/customers/search/SearchCustomerOutput.groovy
@@ -1,13 +1,23 @@
 package life.qbic.portal.portlet.customers.search
 
+import life.qbic.datamodel.dtos.business.Customer
 import life.qbic.portal.portlet.UseCaseFailure
 
 /**
- * Output interface for the {@link life.qbic.portal.portlet.customers.search.SearchCustomer} use case
+ * Output interface for the {@link life.qbic.portal.portlet.customers.search.SearchCustomer} use
+ * case.
  *
  * @since: 1.0.0
  *
  */
 interface SearchCustomerOutput extends UseCaseFailure {
 
+  /**
+   * This method is called by the use case on success.
+   *
+   * It passes the search result for a given search query.
+   *
+   * @param foundCustomers A list of {@link Customer}.
+   */
+  void successNotification(List<Customer> foundCustomers)
 }
