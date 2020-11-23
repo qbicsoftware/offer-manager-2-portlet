b8696eb7f9ff16c7fc4c315b8405f655cbe4b0ca
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2020-11-23 12:48:18 +0100

    implement datasource for create offer

--- qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/offers/OfferDbConnector.groovy
+++ qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/offers/OfferDbConnector.groovy
@@ -1,12 +1,20 @@
 package life.qbic.portal.qoffer2.offers
 
-
+import groovy.util.logging.Log4j2
+import life.qbic.datamodel.dtos.business.Affiliation
 import life.qbic.datamodel.dtos.business.Offer
+import life.qbic.datamodel.dtos.general.Person
 import life.qbic.portal.portlet.exceptions.DatabaseQueryException
 import life.qbic.portal.portlet.offers.create.CreateOfferDataSource
+import life.qbic.portal.qoffer2.database.ConnectionProvider
 import life.qbic.portal.qoffer2.database.DatabaseSession
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+
+import java.sql.Connection
+import java.sql.Date
+import java.sql.PreparedStatement
+import java.sql.ResultSet
 
 /**
  * Handles the connection to the offer database
@@ -17,19 +25,147 @@ import org.apache.logging.log4j.Logger
  * @author: Jennifer Bödker
  *
  */
+@Log4j2
 class OfferDbConnector implements CreateOfferDataSource{
 
     DatabaseSession session
+    ConnectionProvider connectionProvider
 
     private static final Logger LOG = LogManager.getLogger(OfferDbConnector.class)
+
+    private static final String OFFER_INSERT_QUERY = "INSERT INTO offer (modificationDate, expirationDate, customerId, projectManagerId, projectTitle, projectDescription, totalPrice, customerAffiliationId)"
+
 
     OfferDbConnector(DatabaseSession session){
         this.session = session
     }
 
+    /**
+     * Constructor for a CustomerDbConnector
+     * @param connection a connection to the customer db
+     * @see Connection
+     */
+    OfferDbConnector(ConnectionProvider connectionProvider) {
+        this.connectionProvider = connectionProvider
+    }
+
     @Override
     void store(Offer offer) throws DatabaseQueryException {
-        //TODO implement
-        throw new Exception("Method not implemented.")
+        try {
+            Connection connection = connectionProvider.connect()
+            connection.setAutoCommit(false)
+
+            connection.withCloseable {it ->
+                try {
+                    int projectManagerId = getPersonId(offer.projectManager)
+                    int customerId = getPersonId(offer.customer)
+                    int affiliationId = getAffiliationId(offer.selectedCustomerAffiliation)
+                    createOffer(it, offer, projectManagerId, customerId, affiliationId)
+                    connection.commit()
+                } catch (Exception e) {
+                    log.error(e.message)
+                    log.error(e.stackTrace.join("\n"))
+                    connection.rollback()
+                    connection.close()
+                    throw new DatabaseQueryException("Could not create offer.")
+                }
+            }
+        } catch (DatabaseQueryException ignored) {
+            throw new DatabaseQueryException("The offer could not be created: ${offer.toString()}")
+        } catch (Exception e) {
+            log.error(e)
+            log.error(e.stackTrace.join("\n"))
+            throw new DatabaseQueryException("The offer could not be created: ${offer.toString()}")
+        }
+    }
+
+    /**
+     *
+     * @param offer
+     * @return
+     */
+    private static int createOffer(Connection connection, Offer offer, int projectManagerId, int customerId, int affiliationId){
+        String sqlValues = "VALUE(?,?,?,?,?,?,?,?)"
+        String queryTemplate = OFFER_INSERT_QUERY + " " + sqlValues
+        List<Integer> generatedKeys = []
+
+        connection.withCloseable {
+            PreparedStatement preparedStatement = it.prepareStatement(queryTemplate)
+            preparedStatement.setDate(1, offer.modificationDate as Date)
+            preparedStatement.setDate(2,offer.expirationDate as Date)
+            preparedStatement.setInt(3,customerId)
+            preparedStatement.setInt(4,projectManagerId)
+            preparedStatement.setString(5,offer.projectTitle)
+            preparedStatement.setString(6,offer.projectDescription)
+            preparedStatement.setDouble(7,offer.totalPrice)
+            preparedStatement.setInt(8,affiliationId)
+
+            preparedStatement.execute()
+            def keys = preparedStatement.getGeneratedKeys()
+            while (keys.next()){
+                generatedKeys.add(keys.getInt(1))
+            }
+        }
+
+
+        return generatedKeys[0]
+    }
+
+    /**
+     * Searches for a person and returns the matching ID from the person table
+     * @param person The database entry is searched for a person
+     * @return the ID of the database entry matching the person
+     */
+    int getPersonId(Person person) {
+        String query = "SELECT id FROM person WHERE first_name = ? AND last_name = ? AND email = ?"
+        Connection connection = connectionProvider.connect()
+
+        int personId = -1
+
+        connection.withCloseable {
+            def statement = connection.prepareStatement(query)
+            statement.setString(1, person.firstName)
+            statement.setString(2, person.lastName)
+            statement.setString(3, person.emailAddress)
+
+            ResultSet result = statement.executeQuery()
+            while (result.next()){
+                personId = result.getInt(1)
+            }
+        }
+        return personId
+    }
+
+    //todo the same method as in customerDbconnector
+    int getAffiliationId(Affiliation affiliation) {
+        String query = "SELECT id FROM affiliation WHERE organization=? " +
+                "AND address_addition=? " +
+                "AND street=? " +
+                "AND postal_code=? " +
+                "AND city=?"
+
+        Connection connection = connectionProvider.connect()
+
+        List<Integer> affiliationIds = []
+
+        def statement = connection.prepareStatement(query)
+        statement.setString(1, affiliation.organisation)
+        statement.setString(2, affiliation.addressAddition)
+        statement.setString(3, affiliation.street)
+        statement.setString(4, affiliation.postalCode)
+        statement.setString(5, affiliation.city)
+
+        ResultSet rs = statement.executeQuery()
+        while (rs.next()) {
+            affiliationIds.add(rs.getInt(1))
+        }
+
+        if(affiliationIds.size() > 1) {
+            throw new DatabaseQueryException("More than one entry found for $affiliation.")
+        }
+        if (affiliationIds.empty) {
+            throw new DatabaseQueryException("No matching affiliation found for $affiliation.")
+        }
+        return affiliationIds[0]
     }
 }
---
+++ qoffer-infrastructure/src/test/groovy/life/qbic/portal/qoffer2/database/CreateOfferDataSourceSpec.groovy
@@ -0,0 +1,164 @@
+package life.qbic.portal.qoffer2.database
+
+import groovy.sql.GroovyRowResult
+import life.qbic.datamodel.dtos.business.Affiliation
+import life.qbic.datamodel.dtos.business.Customer
+import life.qbic.datamodel.dtos.business.Offer
+import life.qbic.datamodel.dtos.business.ProductItem
+import life.qbic.datamodel.dtos.business.ProjectManager
+import life.qbic.datamodel.dtos.business.services.ProductUnit
+import life.qbic.datamodel.dtos.business.services.Sequencing
+import life.qbic.portal.portlet.offers.create.CreateOfferDataSource
+import life.qbic.portal.qoffer2.offers.OfferDbConnector
+import org.apache.groovy.sql.extensions.SqlExtensions
+import spock.lang.Specification
+
+import java.sql.Connection
+import java.sql.Date
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+
+/**
+ * <class short description - One Line!>
+ *
+ * <More detailed description - When to use, what it solves, etc.>
+ *
+ * @since: <versiontag>
+ *
+ */
+class CreateOfferDataSourceSpec extends Specification{
+
+    /**
+    def "CreateOfferDbConnector shall store the offer given the offer DTO"() {
+        given: "a predefined query template"
+        String OFFER_INSERT_QUERY = "INSERT INTO offer (modificationDate, expirationDate, customerId, projectManagerId, projectTitle, projectDescription, totalPrice, customerAffiliationId)" +
+                "VALUE(?,?,?,?,?,?,?,?)"
+
+        and: "a connection returning nothing if the the offer was created successfully"
+        // we need to stub the static SqlExtensions.toRowResult method because we do not provide an implemented RowResult
+        GroovyMock(SqlExtensions, global: true)
+        SqlExtensions.toRowResult(_ as ResultSet) >> new GroovyRowResult()
+        // our statement should only be able to fill the template with the correct values
+        PreparedStatement preparedStatement = Mock (PreparedStatement, {
+            it.setDate(1, modificationDate as Date) >> _
+            it.setDate(2, expirationDate as Date) >> _
+            it.setInt(3, customerId) >> _
+            it.setInt(4, projectManagerId) >> _
+            it.setString(5, projectTitle) >> _
+            it.setString(6, projectDescription) >> _
+            it.setDouble(7, totalPrice) >> _
+            it.setInt(8, affiliationId) >> _
+
+            it.executeQuery() >> Stub(ResultSet,{it.next() >>> [true, false]})
+        })
+        // the connection must only provide precompiled statements for the expected query template
+        Connection connection = Stub( Connection, {
+            it.prepareStatement(OFFER_INSERT_QUERY) >> preparedStatement
+        })
+
+        //and: "a ConnectionProvider providing the stubbed connection"
+        ConnectionProvider connectionProvider = Stub (ConnectionProvider, {it.connect() >> connection})
+
+        //and: "an implementation of the SearchCustomerDataSource with this connection provider"
+        CreateOfferDataSource dataSource = new OfferDbConnector(connectionProvider)
+
+        when: "the datasource is tasked with finding a customer with provided first and last name"
+        dataSource.store(new Offer.Builder(customer,projectManager,projectTitle,projectDescription,items,customerAffilation)
+                .expirationDate(expirationDate)
+                .modificationDate(modificationDate)
+                .build())
+
+        then: "No exception is thrown if the insertion was successful"
+        noExceptionThrown()
+
+        where: "offer information is as follows"
+        id | modificationDate | expirationDate | customer | projectManager | projectTitle | projectDescription | items | totalPrice | customerAffilation | customerId | projectManagerId | affiliationId
+        0 | new java.util.Date() | new java.util.Date() | new Customer.Builder("first","last","@").build() |  new ProjectManager.Builder("first","last","@").build() |
+        "title" | "description text" | [new ProductItem(2,new Sequencing("name","descr",999, ProductUnit.PER_SAMPLE))] | 2000.00 | new Affiliation.Builder("organization","street","postal_code","city").build()
+        | 0 | 0 | 0
+
+    } **/
+
+
+    def "CreateOfferDbConnector shall return the id for a given person"(){
+        given:
+        String query = "SELECT id FROM person WHERE first_name = ? AND last_name = ? AND email = ?"
+
+        and: "a connection returning the if of the person if it was found"
+        // we need to stub the static SqlExtensions.toRowResult method because we do not provide an implemented RowResult
+        GroovyMock(SqlExtensions, global: true)
+        SqlExtensions.toRowResult(_ as ResultSet) >> new GroovyRowResult(["id":id])
+        // our statement should only be able to fill the template with the correct values
+        PreparedStatement preparedStatement = Mock (PreparedStatement, {
+            it.setString(1 , firstName) >> _
+            it.setString(2, lastName) >> _
+            it.setString(3, emailAddress) >> _
+
+            it.executeQuery() >> Stub(ResultSet,{it.next() >>> [true, false]})
+        })
+        // the connection must only provide precompiled statements for the expected query template
+        Connection connection = Stub( Connection, {
+            it.prepareStatement(query) >> preparedStatement
+        })
+
+        //and: "a ConnectionProvider providing the stubbed connection"
+        ConnectionProvider connectionProvider = Stub (ConnectionProvider, {it.connect() >> connection})
+
+        //and: "an implementation of the SearchCustomerDataSource with this connection provider"
+        OfferDbConnector dataSource = new OfferDbConnector(connectionProvider)
+
+        when:
+        int resultId = dataSource.getPersonId(new Customer.Builder(firstName,lastName,emailAddress).build())
+
+        then:
+        resultId == id
+
+        where: "customer information is as follows"
+        id | firstName | lastName | academicTitle | emailAddress
+        0 | "luke" | "skywalker" | "Dr." | "sith@jedicouncil.universe"
+    }
+
+    def "CreateOfferDbConnector shall return the id for a given affiliation"(){
+        given:
+        String query = "SELECT id FROM affiliation WHERE organization=? " +
+                "AND address_addition=? " +
+                "AND street=? " +
+                "AND postal_code=? " +
+                "AND city=?"
+
+        and: "a connection returning the if of the affiliation if it was found"
+        // we need to stub the static SqlExtensions.toRowResult method because we do not provide an implemented RowResult
+        GroovyMock(SqlExtensions, global: true)
+        SqlExtensions.toRowResult(_ as ResultSet) >> new GroovyRowResult(["id":id])
+        // our statement should only be able to fill the template with the correct values
+        PreparedStatement preparedStatement = Mock (PreparedStatement, {
+            it.setString(1, organization) >> _
+            it.setString(2, address_addition) >> _
+            it.setString(3, street) >> _
+            it.setString(4, postal_code) >> _
+            it.setString(5, city) >> _
+
+            it.executeQuery() >> Stub(ResultSet,{it.next() >>> [true, false]})
+        })
+        // the connection must only provide precompiled statements for the expected query template
+        Connection connection = Stub( Connection, {
+            it.prepareStatement(query) >> preparedStatement
+        })
+
+        //and: "a ConnectionProvider providing the stubbed connection"
+        ConnectionProvider connectionProvider = Stub (ConnectionProvider, {it.connect() >> connection})
+
+        //and: "an implementation of the SearchCustomerDataSource with this connection provider"
+        OfferDbConnector dataSource = new OfferDbConnector(connectionProvider)
+
+        when:
+        int resultId = dataSource.getAffiliationId(new Affiliation.Builder(organization,street,postal_code,city).build())
+
+        then:
+        resultId == id
+
+        where: "affiliation information is as follows"
+        id | organization | address_addition | street | postal_code | city | country | category
+        0 | "QBiC" | "Quantitative Biology Center" | "Auf der Morgenstelle 10" | "72076" | "Tübingen" | "Germany" | "internal"
+    }
+}
