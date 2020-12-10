35ffd7e01182d05c52b1c1dfb2e11f0314fa4f2a
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2020-12-10 16:26:32 +0100

    Add Offer business object

---
+++ qoffer-domain/src/main/groovy/life/qbic/portal/portlet/offers/Offer.groovy
@@ -0,0 +1,162 @@
+package life.qbic.portal.portlet.offers
+
+import life.qbic.datamodel.dtos.business.Affiliation
+import life.qbic.datamodel.dtos.business.AffiliationCategory
+import life.qbic.datamodel.dtos.business.Customer
+import life.qbic.datamodel.dtos.business.OfferId
+import life.qbic.datamodel.dtos.business.ProductItem
+import life.qbic.datamodel.dtos.business.ProjectManager
+
+/**
+ * Represents the Offer business model.
+ *
+ * This class should be used in the business context of offer creation.
+ *
+ * It holds all relevant business rules to compute the final prices for
+ * offers based on the customers affiliation.
+ *
+ * @since 0.1.0
+ */
+class Offer {
+
+    /**
+     * Date on which the offer was lastly modified
+     */
+    final Date modificationDate
+    /**
+     * The date on which the offer expires
+     */
+    final Date expirationDate
+    /**
+     * The customer for which this offer was created
+     */
+    final Customer customer
+    /**
+     * The QBiC project manager who was assigned to the project
+     */
+    final ProjectManager projectManager
+    /**
+     * The title of the project
+     */
+    final String projectTitle
+    /**
+     * A short description of the project
+     */
+    final String projectDescription
+    /**
+     * A list of items for which the customer will be charged
+     */
+    final List<ProductItem> items
+    /**
+     * The identifier for the offer which makes it distinguishable from other offers
+     */
+    final OfferId identifier
+    /**
+     * The affiliation of the customer selected for this offer
+     */
+    final Affiliation selectedCustomerAffiliation
+
+    static class Builder {
+
+        Date modificationDate
+        Date expirationDate
+        Customer customer
+        ProjectManager projectManager
+        String projectTitle
+        String projectDescription
+        List<ProductItem> items
+        OfferId identifier
+        Affiliation selectedCustomerAffiliation
+
+        Builder(Customer customer, ProjectManager projectManager, String projectTitle, String projectDescription, List<ProductItem> items, Affiliation selectedCustomerAffiliation) {
+            this.customer = Objects.requireNonNull(customer, "Customer must not be null")
+            this.projectManager = Objects.requireNonNull(projectManager, "Project Manager must not be null")
+            this.projectTitle = Objects.requireNonNull(projectTitle, "Project Title must not be null")
+            this.projectDescription = Objects.requireNonNull(projectDescription, "Project Description must not be null")
+            this.items = []
+            this.selectedCustomerAffiliation = Objects.requireNonNull(selectedCustomerAffiliation, "Customer Affiliation must not be null")
+        }
+
+        Builder modificationDate(Date modificationDate) {
+            this.modificationDate = modificationDate
+            return this
+        }
+
+        Builder expirationDate(Date expirationDate) {
+            this.expirationDate = expirationDate
+            return this
+        }
+
+        Builder identifier(OfferId identifier) {
+            this.identifier = identifier
+            return this
+        }
+
+        Offer build() {
+            return new Offer(this)
+        }
+    }
+
+    private Offer(Builder builder) {
+        this.customer = builder.customer
+        this.identifier = builder.identifier
+        this.items = builder.items
+        this.expirationDate = builder.expirationDate
+        this.modificationDate = builder.modificationDate
+        this.projectManager = builder.projectManager
+        this.projectDescription = builder.projectDescription
+        this.projectTitle = builder.projectTitle
+        this.selectedCustomerAffiliation = builder.selectedCustomerAffiliation
+    }
+
+    /**
+     * The total costs for the current offer.
+     *
+     * @return The total costs in the currency of the selected items.
+     */
+    double getTotalCosts() {
+        final double netPrice = calculateNetPrice()
+        final double overhead = determineOverhead()
+        final double vat = determineVat()
+        // TODO check back with BioPM if this is correct
+        return netPrice*overhead + vat*netPrice
+    }
+
+    private double calculateNetPrice() {
+        double netSum = 0.0
+        for (item in items) {
+            netSum += item.quantity * item.product.unitPrice
+        }
+        return netSum
+    }
+
+    private double determineOverhead() {
+        double overhead = 0.0
+        switch(selectedCustomerAffiliation.category) {
+            case AffiliationCategory.INTERNAL:
+                overhead = 0.0
+                break
+            case AffiliationCategory.EXTERNAL_ACADEMIC:
+                overhead = 0.2
+                break
+            case AffiliationCategory.EXTERNAL:
+                overhead = 0.2
+                break
+            default:
+                overhead = 0.4
+        }
+        return overhead
+    }
+
+    private double determineVat() {
+        double vat = 0.0
+        switch(selectedCustomerAffiliation.category) {
+            case AffiliationCategory.INTERNAL:
+                vat = 0.0
+                break
+            default:
+                vat = 0.19
+        }
+        return vat
+    }
+}
