0852e58211f80de5fcacdb9471bc7115d6ca2eab
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2020-12-11 15:54:32 +0100

    move tests to right class, add test for finding products

--- qoffer-infrastructure/src/test/groovy/life/qbic/portal/qoffer2/database/CustomerDbConnectorSpec.groovy
+++ qoffer-infrastructure/src/test/groovy/life/qbic/portal/qoffer2/database/CustomerDbConnectorSpec.groovy
@@ -3,85 +3,23 @@ package life.qbic.portal.qoffer2.database
 import groovy.sql.GroovyRowResult
 import life.qbic.datamodel.dtos.business.Affiliation
 import life.qbic.datamodel.dtos.business.Customer
-import life.qbic.datamodel.dtos.business.Offer
-import life.qbic.datamodel.dtos.business.ProductItem
-import life.qbic.datamodel.dtos.business.ProjectManager
-import life.qbic.datamodel.dtos.business.services.ProductUnit
-import life.qbic.datamodel.dtos.business.services.Sequencing
-import life.qbic.portal.portlet.offers.create.CreateOfferDataSource
 import life.qbic.portal.qoffer2.customers.CustomerDbConnector
-import life.qbic.portal.qoffer2.offers.OfferDbConnector
 import org.apache.groovy.sql.extensions.SqlExtensions
 import spock.lang.Specification
 
 import java.sql.Connection
-import java.sql.Date
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 
 /**
- * <class short description - One Line!>
+ * Adds tests for {@link CustomerDbConnector}
  *
- * <More detailed description - When to use, what it solves, etc.>
- *
- * @since: <versiontag>
+ * @since: 0.1.0
  *
  */
-class CreateOfferDataSourceSpec extends Specification{
+class CustomerDbConnectorSpec extends Specification{
 
-    /**
-    def "CreateOfferDbConnector shall store the offer given the offer DTO"() {
-        given: "a predefined query template"
-        String OFFER_INSERT_QUERY = "INSERT INTO offer (modificationDate, expirationDate, customerId, projectManagerId, projectTitle, projectDescription, totalPrice, customerAffiliationId)" +
-                "VALUE(?,?,?,?,?,?,?,?)"
-
-        and: "a connection returning nothing if the the offer was created successfully"
-        // we need to stub the static SqlExtensions.toRowResult method because we do not provide an implemented RowResult
-        GroovyMock(SqlExtensions, global: true)
-        SqlExtensions.toRowResult(_ as ResultSet) >> new GroovyRowResult()
-        // our statement should only be able to fill the template with the correct values
-        PreparedStatement preparedStatement = Mock (PreparedStatement, {
-            it.setDate(1, modificationDate as Date) >> _
-            it.setDate(2, expirationDate as Date) >> _
-            it.setInt(3, customerId) >> _
-            it.setInt(4, projectManagerId) >> _
-            it.setString(5, projectTitle) >> _
-            it.setString(6, projectDescription) >> _
-            it.setDouble(7, totalPrice) >> _
-            it.setInt(8, affiliationId) >> _
-
-            it.executeQuery() >> Stub(ResultSet,{it.next() >>> [true, false]})
-        })
-        // the connection must only provide precompiled statements for the expected query template
-        Connection connection = Stub( Connection, {
-            it.prepareStatement(OFFER_INSERT_QUERY) >> preparedStatement
-        })
-
-        //and: "a ConnectionProvider providing the stubbed connection"
-        ConnectionProvider connectionProvider = Stub (ConnectionProvider, {it.connect() >> connection})
-
-        //and: "an implementation of the SearchCustomerDataSource with this connection provider"
-        CreateOfferDataSource dataSource = new OfferDbConnector(connectionProvider)
-
-        when: "the datasource is tasked with finding a customer with provided first and last name"
-        dataSource.store(new Offer.Builder(customer,projectManager,projectTitle,projectDescription,items,customerAffilation)
-                .expirationDate(expirationDate)
-                .modificationDate(modificationDate)
-                .build())
-
-        then: "No exception is thrown if the insertion was successful"
-        noExceptionThrown()
-
-        where: "offer information is as follows"
-        id | modificationDate | expirationDate | customer | projectManager | projectTitle | projectDescription | items | totalPrice | customerAffilation | customerId | projectManagerId | affiliationId
-        0 | new java.util.Date() | new java.util.Date() | new Customer.Builder("first","last","@").build() |  new ProjectManager.Builder("first","last","@").build() |
-        "title" | "description text" | [new ProductItem(2,new Sequencing("name","descr",999, ProductUnit.PER_SAMPLE))] | 2000.00 | new Affiliation.Builder("organization","street","postal_code","city").build()
-        | 0 | 0 | 0
-
-    } **/
-
-
-    def "CreateOfferDbConnector shall return the id for a given person"(){
+    def "CustomerDbConnector shall return the id for a given person"(){
         given:
         String query = "SELECT id FROM person WHERE first_name = ? AND last_name = ? AND email = ?"
 
@@ -119,7 +57,7 @@ class CreateOfferDataSourceSpec extends Specification{
         0 | "luke" | "skywalker" | "Dr." | "sith@jedicouncil.universe"
     }
 
-    def "CreateOfferDbConnector shall return the id for a given affiliation"(){
+    def "CustomerDbConnector shall return the id for a given affiliation"(){
         given:
         String query = "SELECT * FROM affiliation WHERE organization= ? " +
                 "AND address_addition = ? " +
@@ -127,11 +65,11 @@ class CreateOfferDataSourceSpec extends Specification{
                 "AND postal_code = ? " +
                 "AND city = ?"
 
-        and: "a connection returning the if of the affiliation if it was found"
+        and: "a connection returning the id of the affiliation if it was found"
         // we need to stub the static SqlExtensions.toRowResult method because we do not provide an implemented RowResult
         GroovyMock(SqlExtensions, global: true)
         SqlExtensions.toRowResult(_ as ResultSet) >> new GroovyRowResult(["id":id, "organization":organization, "address_addition":address_addition,"street":street,
-                                                                            "postal_code":postal_code, "city":city])
+                                                                          "postal_code":postal_code, "city":city])
         // our statement should only be able to fill the template with the correct values
         PreparedStatement preparedStatement = Mock (PreparedStatement, {
             it.setString(1, organization) >> _
