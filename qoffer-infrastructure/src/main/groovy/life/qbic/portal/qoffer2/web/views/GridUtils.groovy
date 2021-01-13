c4865c45b605f857b3611d4284edcd72a947ada3
Sven F <sven.fillinger@qbic.uni-tuebingen.de>
2021-01-13 10:49:30 +0100

    Use tiny textfields for filters (#182)

--- qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/views/GridUtils.groovy
+++ qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/views/GridUtils.groovy
@@ -5,6 +5,7 @@ import com.vaadin.shared.ui.ValueChangeMode
 import com.vaadin.ui.Grid
 import com.vaadin.ui.TextField
 import com.vaadin.ui.components.grid.HeaderRow
+import com.vaadin.ui.themes.ValoTheme
 import org.apache.commons.lang3.StringUtils
 
 /**
@@ -34,6 +35,8 @@ class GridUtils {
             )
         })
         filterTextField.setValueChangeMode(ValueChangeMode.EAGER)
+        filterTextField.addStyleName(ValoTheme.TEXTFIELD_TINY)
+        filterTextField.setPlaceholder("Filter Me")
 
         headerRow.getCell(column).setComponent(filterTextField)
         filterTextField.setSizeFull()
