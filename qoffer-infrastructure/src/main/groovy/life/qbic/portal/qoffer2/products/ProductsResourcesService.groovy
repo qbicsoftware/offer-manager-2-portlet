e8352144f1f752b4707983aa2f517e82f8bd6310
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-01-20 16:16:10 +0100

    Introduce Products Resources service

---
+++ qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/products/ProductsResourcesService.groovy
@@ -0,0 +1,57 @@
+package life.qbic.portal.qoffer2.products
+
+
+import life.qbic.datamodel.dtos.business.services.Product
+import life.qbic.portal.qoffer2.events.EventEmitter
+import life.qbic.portal.qoffer2.services.ResourcesService
+
+/**
+ * Product service that holds resources about available products
+ *
+ * This service holds a list of available products and exposes an
+ * event emitter, that can be used to subscribe to any update
+ * event of the underlying resource data.
+ *
+ * @since: 1.0.0
+ */
+class ProductsResourcesService implements ResourcesService {
+
+    private final ProductsDbConnector dbConnector
+    private final List<Product> products
+    /**
+     * EventEmitter for products. Fires every time the resources are reloaded
+     * @see #reloadResources
+     */
+    final EventEmitter<List<Product>> productEventEmitter
+
+
+    /**
+     *
+     * @param dbConnector
+     */
+    ProductsResourcesService(ProductsDbConnector dbConnector) {
+        this.dbConnector = dbConnector
+        this.products = new LinkedList<>()
+        this.productEventEmitter = new EventEmitter<>()
+        reloadResources()
+    }
+
+    private void setupProductEventEmitter() {
+        this.productEventEmitter.emit(this.products.asList())
+    }
+
+    @Override
+    void reloadResources() {
+        this.products.clear()
+        this.products.addAll(dbConnector.findAllAvailableProducts())
+        this.productEventEmitter.emit(this.products.asList().asImmutable())
+    }
+
+    /**
+     *
+     * @return currently loaded available products
+     */
+    List<Product> getProducts() {
+        return List.copyOf(this.products.asImmutable())
+    }
+}
