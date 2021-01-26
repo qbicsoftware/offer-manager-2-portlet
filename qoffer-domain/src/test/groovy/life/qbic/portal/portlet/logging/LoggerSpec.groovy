39ae70d54312080e904f23d9dc08db7ee46599d9
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-01-26 14:54:39 +0100

    Add test

---
+++ qoffer-domain/src/test/groovy/life/qbic/portal/portlet/logging/LoggerSpec.groovy
@@ -0,0 +1,26 @@
+package life.qbic.portal.portlet.logging
+
+import spock.lang.Specification
+
+/**
+ * Tests the current logging implementation
+ * and fails if changes have made to the logging API.
+ *
+ * @since 1.0.0
+ */
+class LoggerSpec extends Specification {
+
+    def "All four log levels should be callable without exception"() {
+        given: "A log instance"
+        Logging logger = Logger.getLogger(this.class)
+
+        when:
+        logger.info("Just a test info message")
+        logger.warn("Just a test warning message")
+        logger.error("Just a test error message")
+        logger.debug("Just a test debug message")
+
+        then:
+        noExceptionThrown()
+    }
+}
