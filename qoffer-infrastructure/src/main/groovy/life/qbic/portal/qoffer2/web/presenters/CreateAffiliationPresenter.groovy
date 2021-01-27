1d610402373ce46918b699e35139659c27a9b1ec
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-01-27 08:32:54 +0100

    remove successnotification

--- qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/presenters/CreateAffiliationPresenter.groovy
+++ qoffer-infrastructure/src/main/groovy/life/qbic/portal/qoffer2/web/presenters/CreateAffiliationPresenter.groovy
@@ -46,21 +46,15 @@ class CreateAffiliationPresenter implements CreateAffiliationOutput {
     }
 
     @Override
-    void successNotification(String notification) {
-        sharedViewModel.successNotifications.add(notification)
-        clearAffiliationData()
-    }
-
-    @Override
     void failNotification(String notification) {
         sharedViewModel.failureNotifications.add(notification)
     }
 
-    /**
-     * @inheritDoc
-     */
+
     @Override
     void affiliationCreated(Affiliation affiliation) {
         createAffiliationViewModel.affiliationService.reloadResources()
+        sharedViewModel.successNotifications.add("Successfully added new affiliation " + affiliation.organisation)
+        clearAffiliationData()
     }
 }
