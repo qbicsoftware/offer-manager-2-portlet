55532b3e57eef037561eb979cd3881ded9d32000
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-02-08 16:33:20 +0100

    add update person viewmodel and service

---
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/dataresources/persons/PersonUpdateService.groovy
@@ -0,0 +1,66 @@
+package life.qbic.portal.offermanager.dataresources.persons
+
+import life.qbic.datamodel.dtos.business.Customer
+import life.qbic.datamodel.dtos.general.Person
+import life.qbic.portal.offermanager.communication.EventEmitter
+import life.qbic.portal.offermanager.communication.Subscription
+import life.qbic.portal.offermanager.dataresources.ResourcesService
+
+/**
+ * Service that represents an customer to be updated.
+ *
+ * This service offers an EventEmitter property that can be
+ * used for inter component communication, when a new customer
+ * source is available for update.
+ *
+ * @since: 1.0.0
+ *
+ */
+class PersonUpdateService implements ResourcesService<Person> {
+
+    private final EventEmitter<Person> customerForUpdateEvent
+
+    PersonUpdateService(){
+        this.customerForUpdateEvent = new EventEmitter<>()
+    }
+
+    @Override
+    void reloadResources() {
+        //nothing to reload
+    }
+
+    @Override
+    void subscribe(Subscription<Person> subscription) {
+        customerForUpdateEvent.register(subscription)
+    }
+
+    @Override
+    void unsubscribe(Subscription<Person> subscription) {
+        customerForUpdateEvent.unregister(subscription)
+    }
+
+    @Override
+    void addToResource(Person resourceItem) {
+        /*
+        Since this service only is a proxy
+        of an customer update event, this service holds
+        no track of the resources.
+        */
+        customerForUpdateEvent.emit(resourceItem)
+    }
+
+    @Override
+    void removeFromResource(Person resourceItem) {
+        /*
+        Since this service only is a proxy
+        of an customer update event, this service holds
+        no track of the resources.
+        */
+        customerForUpdateEvent.emit(resourceItem)
+    }
+
+    @Override
+    Iterator<Customer> iterator() {
+        return [].iterator()
+    }
+}
