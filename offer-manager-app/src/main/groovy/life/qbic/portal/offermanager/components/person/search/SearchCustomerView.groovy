23de867fcf0f5680bf074e5dcb1ad32e75480bf6
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-02-10 10:27:58 +0100

    add search customer components

---
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/person/search/SearchCustomerView.groovy
@@ -0,0 +1,25 @@
+package life.qbic.portal.offermanager.components.person.search
+
+import com.vaadin.ui.FormLayout
+import life.qbic.portal.offermanager.components.AppViewModel
+import life.qbic.portal.offermanager.components.offer.create.CreateOfferViewModel
+import life.qbic.portal.offermanager.dataresources.offers.OfferResourcesService
+
+/**
+ * <class short description - One Line!>
+ *
+ * <More detailed description - When to use, what it solves, etc.>
+ *
+ * @since: <versiontag>
+ *
+ */
+class SearchCustomerView extends FormLayout{
+
+    private final
+
+    SearchCustomerView(AppViewModel sharedViewModel, CreateOfferViewModel createOfferViewModel, SearchCustomerController controller, OfferResourcesService offerProviderService) {
+        //sjdfl
+
+
+    }
+}
