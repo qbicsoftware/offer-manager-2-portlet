f8136f32a9370e1b0031ba4dcde56610a19475f3
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-02-15 11:14:43 +0100

    Add `@since` to public method

--- offer-manager-domain/src/main/groovy/life/qbic/business/customers/affiliation/Country.groovy
+++ offer-manager-domain/src/main/groovy/life/qbic/business/customers/affiliation/Country.groovy
@@ -14,6 +14,7 @@ class Country {
     /**
      * Provides a list of US English names of all known countries. This list contains unique entries and no empty Strings.
      * @return A list of unique country names
+     * @since 1.0.0
      */
     static List<String> availableCountryNames(){
         List<String> countryNames = Locale.US.getAvailableLocales().collect {it.getDisplayCountry()}.unique()
