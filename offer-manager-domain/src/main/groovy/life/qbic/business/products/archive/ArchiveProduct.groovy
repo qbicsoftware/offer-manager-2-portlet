a0c18d14c996860b11c92ed65bac0e18705ae89f
jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
2021-03-05 07:51:38 +0100

    describe archive products use case
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

--- offer-manager-domain/src/main/groovy/life/qbic/business/products/archive/ArchiveProduct.groovy
+++ offer-manager-domain/src/main/groovy/life/qbic/business/products/archive/ArchiveProduct.groovy
@@ -1,11 +1,12 @@
 package life.qbic.business.products.archive
 
 /**
- * <class short description - One Line!>
+ * <h1>4.3.1 Archive Service Product</h1>
+ * <br>
+ * <p> The use case archives unused or old service products. These products cannot be added to new offers but still show in old offers.</p>
+ * <br>
  *
- * <More detailed description - When to use, what it solves, etc.>
- *
- * @since: <versiontag>
+ * @since: 1.0.0
  *
  */
 class ArchiveProduct {
