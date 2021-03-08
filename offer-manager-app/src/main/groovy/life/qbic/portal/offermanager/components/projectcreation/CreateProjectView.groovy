af55cc0f5b382f06a011ec6e4e56d4108565b0ea
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-03-08 10:35:44 +0100

    Add class for the CreateProject view

---
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/projectcreation/CreateProjectView.groovy
@@ -0,0 +1,14 @@
+package life.qbic.portal.offermanager.components.projectcreation
+
+/**
+ * <h1>Enables a user to create a project based on an existing offer</h1>
+ *
+ * <p>This view provides access the the use cases <code>Create Project Space</code> and
+ * <code>Create Project</code>.</p>
+ * <p>Both use cases are part of the same scenario, when a user wants to create a project
+ * in QBiC's data management platform based on the information of an existing offer.</p>
+ *
+ * @since 1.0.0
+ */
+class CreateProjectView {
+}
