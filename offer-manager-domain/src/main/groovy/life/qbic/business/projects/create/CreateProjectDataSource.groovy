3f4301d00a916b78742f55ce68f7955f5e32b7d0
Sven F <sven.fillinger@qbic.uni-tuebingen.de>
2021-03-08 10:21:10 +0100

    Add interfaces for the Create Project and Project Space use case (#355)
    This PR provides the interfaces for the use cases Create Project and Create Project Space.
    
    Co-authored-by: jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

---
+++ offer-manager-domain/src/main/groovy/life/qbic/business/projects/create/CreateProjectDataSource.groovy
@@ -0,0 +1,34 @@
+package life.qbic.business.projects.create
+
+
+import life.qbic.business.exceptions.DatabaseQueryException
+import life.qbic.datamodel.dtos.projectmanagement.Project
+import life.qbic.datamodel.dtos.projectmanagement.ProjectApplication
+
+/**
+ * <h1>Access to the project management datasource</h1>
+ *
+ * <p>This interface collects methods to interact with the project management
+ * datasource in the context of the Create Project use case.</p>
+ *
+ * @since 1.0.0
+ */
+interface CreateProjectDataSource {
+
+    /**
+     * Creates a new QBiC project in the data management platform.
+     *
+     * @param projectApplication A project application with the information necessary for the
+     * project registration
+     *
+     * @return Information about the created project
+     *
+     * @since 1.0.0
+     * @throws ProjectExistsException If the application was denied. Reasons for denial are
+     * currently:
+     *  1. A project with the same project title already exists
+     * @throws DatabaseQueryException If any technical interaction with the data source fails
+     */
+    Project createProject(ProjectApplication projectApplication)
+            throws ProjectExistsException, DatabaseQueryException
+}
---
+++ offer-manager-domain/src/main/groovy/life/qbic/business/projects/spaces/CreateProjectSpaceOutput.groovy
@@ -0,0 +1,35 @@
+package life.qbic.business.projects.spaces
+
+import life.qbic.business.UseCaseFailure
+import life.qbic.datamodel.dtos.projectmanagement.ProjectSpace
+
+/**
+ * <h1>Describes the output interface of the Create Project Space use case.</h1>
+ *
+ * @since 1.0.0
+ */
+interface CreateProjectSpaceOutput extends UseCaseFailure {
+
+    /**
+     * <p>This method is called from the use case, after
+     * successful project space creation in QBiC's data management
+     * platform.</p>
+     *
+     * @param projectSpace The created project space
+     * @since 1.0.0
+     */
+    void projectSpaceCreated(ProjectSpace projectSpace)
+
+    /**
+     * <p>Called when a project space with a given identifier already exists.</p>
+     * <br>
+     * <p>This reflects the scenario, when a user provided a pre-defined space name
+     * which already exists in the underlying data source. In this case the space cannot be
+     * created.</p>
+     *
+     * @param projectSpace The project space that already exists
+     * @since 1.0.0
+     */
+    void projectSpaceAlreadyExists(ProjectSpace projectSpace)
+
+}
