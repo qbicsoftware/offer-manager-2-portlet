127ff3567ffadf8d7d719801b8bd0de79483ebbb
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-03-09 13:11:13 +0100

    Add log methods that accept a throwable

--- offer-manager-domain/src/main/groovy/life/qbic/business/logging/Logging.groovy
+++ offer-manager-domain/src/main/groovy/life/qbic/business/logging/Logging.groovy
@@ -12,9 +12,16 @@ interface Logging {
 
     /**
      * Logs a common information event.
-     * @param message
+     * @param message the message string to log
      */
     void info(String message)
+
+    /**
+     * Logs a message at the INFO level including the stack trace of the Throwable cause passed as parameter.
+     * @param message the message object to log.
+     * @cause the exception to log, including its stack trace
+     */
+    void info(String message, Throwable cause)
 
     /**
      * Logs a warn event, that does not indicate a runtime exception, but still might be
@@ -24,14 +31,37 @@ interface Logging {
     void warn(String message)
 
     /**
+     * Logs a warn event, that does not indicate a runtime exception, but still might be
+     * important to report.
+     * @param message the message object to log.
+     * @cause the exception to log, including its stack trace
+     */
+    void warn(String message, Throwable cause)
+
+    /**
      * Logs a error or exception during the application execution.
      * @param message
      */
     void error(String message)
 
     /**
+     * Logs a error or exception during the application execution.
+     * @param message
+     * @param message the message object to log.
+     * @cause the exception to log, including its stack trace
+     */
+    void error(String message, Throwable cause)
+
+    /**
      * Logs runtime information that are useful for debugging.
      * @param message
      */
     void debug(String message)
+
+    /**
+     * Logs runtime information that are useful for debugging.
+     * @param message the message object to log.
+     * @cause the exception to log, including its stack trace
+     */
+    void debug(String message, Throwable cause)
 }
