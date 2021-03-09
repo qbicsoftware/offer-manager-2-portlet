ae467799c1ea190348fb4c2a55e3be5149a0fbbd
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-03-09 14:51:48 +0100

    add create project space class

---
+++ offer-manager-domain/src/main/groovy/life/qbic/business/projects/spaces/CreateProjectSpace.groovy
@@ -0,0 +1,12 @@
+package life.qbic.business.projects.spaces
+
+/**
+ * <h1><short description></h1>
+ * <br>
+ * <p><detailed description></p>
+ *
+ * @since 1.0.0
+ */
+
+class CreateProjectSpace {
+}
