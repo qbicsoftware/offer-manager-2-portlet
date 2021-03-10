9f6061a86c4edee2140f434d18d8559f20bd20a0
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-03-10 13:50:10 +0100

    Remove copy tests

--- offer-manager-domain/src/test/groovy/life/qbic/business/products/copy/CopyProductSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/business/products/copy/CopyProductSpec.groovy
@@ -18,53 +18,5 @@ import spock.lang.Specification
  * @since 1.0.0
  */
 class CopyProductSpec extends Specification {
-    CopyProductDataSource dataSource
-    CopyProductOutput output
-    ProductId productId
-    Product product
-    CreateProductInput createProduct
-
-    def setup() {
-        dataSource = Stub(CopyProductDataSource)
-        output = Mock(CopyProductOutput)
-        productId = new ProductId("Test", "ABCD1234")
-        product = new AtomicProduct("Test atomic item", "This is a test item", 0.5, ProductUnit.PER_SAMPLE, productId)
-        createProduct = Mock(CreateProductInput)
-    }
-
-    def "CopyModified forwards a modified of the product to the output"() {
-        given: "a use case instance"
-        CopyProduct copyProduct = new CopyProduct(dataSource, output)
-
-        when: "the copy method is called"
-        copyProduct.copyModified(productId)
-
-        then: "the output receives the provided product and no fails"
-        1 * output.copied(product)
-        0 * output.failNotification(_)
-    }
-
-    def "Copy sends a fail notification if there is no product with the provided id"() {
-        given: "a data source that returns no entries for the given id"
-        dataSource.fetch(productId) >> Optional.empty()
-        and: "a use case instance"
-        CopyProduct copyProduct = new CopyProduct(dataSource, output)
-        when: "the copy method is called"
-        copyProduct.copy(productId)
-        then: "the use case sends a failure notification"
-        1 * output.failNotification(_ as String)
-        0 * output.copied(_)
-    }
-
-    def "Copy sends a fail notification if the database fails for technical reasons"(){
-        given: "a data source that throws a $DatabaseQueryException"
-        dataSource.fetch(productId) >> {throw new DatabaseQueryException("This is a test")}
-        and: "a use case instance"
-        CopyProduct copyProduct = new CopyProduct(dataSource, output)
-        when: "the copy method is called"
-        copyProduct.copy(productId)
-        then: "the use case sends a failure notification"
-        1 * output.failNotification(_ as String)
-        0 * output.copied(_)
-    }
+    //TODO implement
 }
