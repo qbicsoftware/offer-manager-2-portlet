8ea4793f67ef477d0a387a8d5706fb44a069a3c4
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-03-11 08:44:45 +0100

    fix tests

--- offer-manager-domain/src/test/groovy/life/qbic/portal/portlet/projects/CreateProjectSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/portal/portlet/projects/CreateProjectSpec.groovy
@@ -5,6 +5,7 @@ import life.qbic.business.projects.create.CreateProject
 import life.qbic.business.projects.create.CreateProjectDataSource
 import life.qbic.business.projects.create.CreateProjectOutput
 import life.qbic.business.projects.create.ProjectExistsException
+import life.qbic.business.projects.spaces.CreateProjectSpaceDataSource
 import life.qbic.datamodel.dtos.business.Customer
 import life.qbic.datamodel.dtos.business.OfferId
 import life.qbic.datamodel.dtos.business.ProjectManager
@@ -13,6 +14,7 @@ import life.qbic.datamodel.dtos.projectmanagement.ProjectApplication
 import life.qbic.datamodel.dtos.projectmanagement.ProjectCode
 import life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier
 import life.qbic.datamodel.dtos.projectmanagement.ProjectSpace
+import spock.lang.Shared
 import spock.lang.Specification
 
 /**
@@ -24,23 +26,25 @@ import spock.lang.Specification
 
 class CreateProjectSpec extends Specification{
 
-    CreateProject createProject
-    CreateProjectOutput output
-    CreateProjectDataSource dataSource
+    @Shared CreateProject createProject
+    @Shared CreateProjectOutput output
+    @Shared CreateProjectDataSource dataSource
+    @Shared CreateProjectSpaceDataSource spaceDataSource
 
-    ProjectManager projectManager
-    Customer customer
-    OfferId offerId
+    @Shared ProjectManager projectManager
+    @Shared Customer customer
+    @Shared OfferId offerId
 
-    ProjectApplication projectApplication
-    ProjectSpace space
-    ProjectIdentifier projectIdentifier
-    ProjectCode projectCode
+    @Shared ProjectApplication projectApplication
+    @Shared ProjectSpace space
+    @Shared ProjectIdentifier projectIdentifier
+    @Shared ProjectCode projectCode
 
     def setup(){
         dataSource = Stub(CreateProjectDataSource)
+        spaceDataSource = Stub(CreateProjectSpaceDataSource)
         output = Mock(CreateProjectOutput)
-        createProject = new CreateProject(output,dataSource)
+        createProject = new CreateProject(output,dataSource,spaceDataSource)
 
         offerId = new OfferId("my-project","abab","1")
         projectManager = new ProjectManager.Builder("Max","Mustermann","a.b@c.d").build()
@@ -97,6 +101,24 @@ class CreateProjectSpec extends Specification{
         then: "a project is successfully created"
         0 * output.projectCreated(_)
         1 * output.failNotification("The project application for ${projectApplication.projectCode} was not successful. The project can not be stored in the database.")
+        0 * output.projectAlreadyExists(_,_)
+    }
+
+    def "If a new space is provided it shall be created before the project is created"(){
+        given: "a project application is provided"
+        projectApplication = new ProjectApplication(offerId,"Title","objective","exp. design",projectManager,space, customer, projectCode)
+        Project project = new Project(projectIdentifier,projectApplication.projectTitle,offerId)
+
+        and: "the data source is able to create the project"
+        dataSource.createProject(projectApplication) >> project
+        spaceDataSource.createProjectSpace(space) >> {}
+
+        when: "the creation of the project is triggered"
+        createProject.createProjectWithSpace(projectApplication)
+
+        then: "a space and project are successfully created"
+        1 * output.projectCreated(project)
+        0 * output.failNotification(_)
         0 * output.projectAlreadyExists(_,_)
     }
 }
