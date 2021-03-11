cc744b33f38dfcaeaa4b984d12fca1fd935d834c
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-03-11 18:31:34 +0100

    basics

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/dataresources/projects/ProjectMainConnector.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/dataresources/projects/ProjectMainConnector.groovy
@@ -56,6 +56,10 @@ class ProjectMainConnector implements CreateProjectDataSource, CreateProjectSpac
     this.projectDbConnector = projectDbConnector
     this.openbisClient = openbisClient
   }
+  
+  List<ProjectSpace> listSpaces() {
+    
+  }
 
   @Override
   void createProjectSpace(ProjectSpace projectSpace) throws ProjectSpaceExistsException, DatabaseQueryException {
