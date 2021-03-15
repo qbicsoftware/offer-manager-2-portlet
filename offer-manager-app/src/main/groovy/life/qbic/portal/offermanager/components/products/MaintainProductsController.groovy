94c1dad3555a17cfbc8437edf62be406b79f5384
Tobias Koch <KochTobi@users.noreply.github.com>
2021-03-15 17:59:36 +0100

    Fix typo
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/products/MaintainProductsController.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/products/MaintainProductsController.groovy
@@ -75,7 +75,7 @@ class MaintainProductsController {
          * @param category The products category which determines what kind of products is created
          * @param description The description of the product
          * @param name The name of the product
-         * @param unitPrice The unit price of the products
+         * @param unitPrice The unit price of the product
          * @param unit The unit in which the products is measured
          * @return
          */
