a1cdbe8a57c0ff020854d033527ad331fefbc47f
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-03-16 08:46:13 +0100

    add description to CreateProductView

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/product/create/CreateProductView.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/product/create/CreateProductView.groovy
@@ -3,9 +3,9 @@ package life.qbic.portal.offermanager.components.product.create
 import com.vaadin.ui.HorizontalLayout
 
 /**
- * <h1><short description></h1>
+ * <h1>This view serves the user to create a new service product</h1>
  * <br>
- * <p><detailed description></p>
+ * <p>The view contains several text input fields and combo boxes in order to fully describe the new service products</p>
  *
  * @since 1.0.0
  *
