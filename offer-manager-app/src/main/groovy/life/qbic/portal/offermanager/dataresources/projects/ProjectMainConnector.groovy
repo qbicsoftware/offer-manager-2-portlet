6babe2ff00c674cee920fab4390621f0a79a1521
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-03-22 18:00:14 +0100

    first part of fix: create empty experimental design objects

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/dataresources/projects/ProjectMainConnector.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/dataresources/projects/ProjectMainConnector.groovy
@@ -14,6 +14,8 @@ import life.qbic.datamodel.dtos.business.*
 import life.qbic.datamodel.dtos.projectmanagement.*
 import life.qbic.business.exceptions.DatabaseQueryException
 
+import life.qbic.datamodel.identifiers.ExperimentCodeFunctions
+
 import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.ResultSet
@@ -21,13 +23,24 @@ import java.sql.Statement
 
 import life.qbic.openbis.openbisclient.OpenBisClient
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import life.qbic.xml.manager.StudyXMLParser
+import life.qbic.xml.study.Qexperiment
+
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.operation.SynchronousOperationExecutionOptions
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.common.operation.IOperation
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.project.create.CreateProjectsOperation
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.project.create.ProjectCreation
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.experiment.create.ExperimentCreation
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.experiment.id.ExperimentIdentifier
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.experiment.create.CreateExperimentsOperation
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.create.SampleCreation
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.sample.create.CreateSamplesOperation
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.space.create.CreateSpacesOperation
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.space.create.SpaceCreation
 import ch.ethz.sis.openbis.generic.asapi.v3.dto.space.id.SpacePermId
+import ch.ethz.sis.openbis.generic.asapi.v3.dto.entitytype.id.EntityTypePermId;
 
 
 /**
@@ -103,15 +116,62 @@ class ProjectMainConnector implements CreateProjectDataSource, CreateProjectSpac
   }
 
   private void createOpenbisProject(ProjectSpace space, ProjectCode projectCode, String description) {
-    ProjectCreation project = new ProjectCreation();
-    project.setCode(projectCode.toString());
-    project.setSpaceId(new SpacePermId(space.toString()));
-    project.setDescription(description);
+    ProjectCreation project = new ProjectCreation()
+    project.setCode(projectCode.toString())
+    project.setSpaceId(new SpacePermId(space.toString()))
+    project.setDescription(description)
 
-    IOperation operation = new CreateProjectsOperation(project);
+    IOperation operation = new CreateProjectsOperation(project)
+    handleOperations(operation)
+  }
+  
+  private void setupEmptyExperimentalDesign(ProjectSpace space, ProjectCode projectCodeObj)
+      throws JAXBException {
+    StudyXMLParser xmlParser = new StudyXMLParser()
+    JAXBElement<Qexperiment> res =
+        xmlParser.createNewDesign(new HashSet<>(), new ArrayList<>(), new HashMap<>(), new HashMap<>())
+    String emptyStudyXML = xmlParser.toString(res)
+
+    String spaceCode = space.toString()
+    String projectCode = projectCodeObj.toString()
+    
+    String experimentCode = projectCode + "_INFO"
+    String sampleCode = projectCode + "000"
+    String experimentIdentifier = ExperimentCodeFunctions.getInfoExperimentID(spaceCode, projectCode)
+
+    Map<String, String> properties = new HashMap<>()
+    properties.put("Q_EXPERIMENTAL_SETUP", emptyStudyXML)
+
+    createOpenbisExperiment(spaceCode, projectCode, experimentCode, "Q_PROJECT_DETAILS", properties)
+    
+    createOpenbisSample(spaceCode, experimentIdentifier, sampleCode, "Q_ATTACHMENT_SAMPLE", new HashMap<>())
+  }
+  
+  private void createOpenbisExperiment(String spaceCode, String projectCode, String experimentCode, String experimentType, Map<String,String> properties) {
+    ExperimentCreation experiment = new ExperimentCreation()
+    experiment.setCode(experimentCode)
+    experiment.setProjectId(new ch.ethz.sis.openbis.generic.asapi.v3.dto.project.id.ProjectIdentifier(spaceCode, projectCode))
+    experiment.setTypeId(new EntityTypePermId(experimentType))
+    experiment.setProperties(properties)
+    
+    IOperation operation = new CreateExperimentsOperation(experiment)
+    handleOperations(operation)
+  }
+  
+  private void createOpenbisSample(String spaceCode, String experimentIdentifier, String sampleCode, String sampleType, Map<String,String> properties) {
+    SampleCreation sampleCreation = new SampleCreation()
+    sampleCreation.setTypeId(new EntityTypePermId(sampleType))
+    sampleCreation.setSpaceId(new SpacePermId(spaceCode))
+
+    sampleCreation.setExperimentId(new ExperimentIdentifier(experimentIdentifier));
+    sampleCreation.setCode(sampleCode);
+
+    sampleCreation.setProperties(properties);
+
+    IOperation operation = new CreateSamplesOperation(sampleCreation);
     handleOperations(operation);
   }
-
+  
   /**
    * Returns a copied list of existing projects fetched upon creation of this class
    */
@@ -159,12 +219,12 @@ class ProjectMainConnector implements CreateProjectDataSource, CreateProjectSpac
       }
       try {
         createOpenbisProject(space, projectCode, description)
+        setupEmptyExperimentalDesign(space, projectCode)
       } catch (Exception e) {
         log.error(e.message)
         log.error(e.stackTrace.join("\n"))
         throw new DatabaseQueryException("Could not create project.")
       }
-  
       return projectDbConnector.addProjectAndConnectPersonsInUserDB(projectIdentifier, projectApplication)
     }
     
