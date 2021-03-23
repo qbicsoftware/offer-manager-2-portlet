32ecaf4a9f7c1655ae47d02636a42e25d9e7ad5f
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-03-23 11:19:06 +0100

    refine javadoc comment for checksum calculation

--- offer-manager-domain/src/main/groovy/life/qbic/business/products/Product.groovy
+++ offer-manager-domain/src/main/groovy/life/qbic/business/products/Product.groovy
@@ -58,7 +58,7 @@ class Product {
     }
 
     /**
-     * Calculates the checksum for the product
+     * Calculates the SHA checksum for the product
      * @return a string containing the checksum for this product
      */
     String checksum(){
@@ -67,7 +67,9 @@ class Product {
     }
 
     /**
-     * Compute the checksum for a product
+     * Compute the checksum for a product based on the encryption method provided
+     * The checksum is computed based on the product name, description, unit, unit price and the category     *
+     *
      * @param digest The digestor will digest the message that needs to be encrypted
      * @param product Contains the product information
      * @return a string that encrypts the product object
