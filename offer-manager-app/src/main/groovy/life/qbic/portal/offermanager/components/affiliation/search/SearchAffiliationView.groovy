f889156658c52ddbfe16e940b5df3bf42152b76f
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-04-23 18:14:16 +0200

    Add classes

---
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/affiliation/search/SearchAffiliationView.groovy
@@ -0,0 +1,15 @@
+package life.qbic.portal.offermanager.components.affiliation.search
+
+import com.vaadin.ui.FormLayout
+
+/**
+ * <h1>View allowing the user to search for an affiliation</h1>
+ *
+ * <p>This view provides functionality for searching for affiliations.</p>
+ *
+ * @since 1.0.0
+ */
+class SearchAffiliationView extends FormLayout{
+
+    private final SearchAffiliationViewModel
+}
---
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/affiliation/search/SearchAffiliationViewModel.groovy
@@ -0,0 +1,11 @@
+package life.qbic.portal.offermanager.components.affiliation.search
+
+/**
+ * <h1>ViewModel for SearchAffiliationView</h1>
+ *
+ * <p>Determines state of the view. Listens to data events.</p>
+ *
+ * @since 1.0.0
+ */
+class SearchAffiliationViewModel {
+}
