cfdef74fc94e54c69b485f487616b08e208c723b
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-04-26 11:09:41 +0200

    Implement view model

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/affiliation/search/SearchAffiliationView.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/affiliation/search/SearchAffiliationView.groovy
@@ -1,6 +1,7 @@
 package life.qbic.portal.offermanager.components.affiliation.search
 
 import com.vaadin.ui.FormLayout
+import life.qbic.portal.offermanager.dataresources.persons.AffiliationResourcesService
 
 /**
  * <h1>View allowing the user to search for an affiliation</h1>
@@ -12,4 +13,9 @@ import com.vaadin.ui.FormLayout
 class SearchAffiliationView extends FormLayout{
 
     private final SearchAffiliationViewModel
+
+    SearchAffiliationView(SearchAffiliationViewModel) {
+        this.SearchAffiliationViewModel = SearchAffiliationViewModel
+    }
+
 }
--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/affiliation/search/SearchAffiliationViewModel.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/affiliation/search/SearchAffiliationViewModel.groovy
@@ -1,5 +1,8 @@
 package life.qbic.portal.offermanager.components.affiliation.search
 
+import life.qbic.datamodel.dtos.business.Affiliation
+import life.qbic.portal.offermanager.dataresources.persons.AffiliationResourcesService
+
 /**
  * <h1>ViewModel for SearchAffiliationView</h1>
  *
@@ -8,4 +11,40 @@ package life.qbic.portal.offermanager.components.affiliation.search
  * @since 1.0.0
  */
 class SearchAffiliationViewModel {
+
+    /**
+     * A list of available affiliations. All items are of class {@link Affiliation}
+     */
+    ObservableList affiliations
+
+    private final AffiliationResourcesService affiliationResourcesService
+
+    SearchAffiliationViewModel(AffiliationResourcesService affiliationResourcesService) {
+        this.affiliationResourcesService = affiliationResourcesService
+        this.affiliations = new ObservableList(new ArrayList<Affiliation>())
+        resetAffiliations()
+        subscribeToResources()
+    }
+
+    /**
+     * Resets the list of affiliations to the state of the affiliation
+     * resource service
+     * @since 1.0.0
+     * @see #affiliations
+     */
+    void resetAffiliations() {
+        affiliations.clear()
+        affiliations.addAll(affiliationResourcesService.iterator())
+    }
+
+    /**
+     * As we can not distinguish on the type of event.
+     * Therefore, we reset the whole list.
+     * @see #resetAffiliations
+     */
+    private void subscribeToResources() {
+        this.affiliationResourcesService.subscribe({
+            resetAffiliations()
+        })
+    }
 }
