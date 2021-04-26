a2ee92bc3d16150a74e765cf5c986701b2c01ed3
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-04-26 08:25:28 +0200

    add confirmation dialog class

---
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/ConfirmationDialog.groovy
@@ -0,0 +1,20 @@
+package life.qbic.portal.offermanager.components
+
+import com.vaadin.server.VaadinRequest
+import com.vaadin.ui.UI
+
+/**
+ * <h1>Dialog to ask for user confirmation</h1>
+ *
+ * <p>The confirmation dialog requests the users confirmation about specific processes that will follow.
+ * E.g before deleting the user should be asked if he really wants to do that</p>
+ *
+ * @since 1.0.0
+ *
+*/
+class ConfirmationDialog extends UI{
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+}
