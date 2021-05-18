75f04f9012c203489afe55a7cee32d669888d0b0
Tobias Koch <KochTobi@users.noreply.github.com>
2021-05-18 15:56:04 +0200

    Remove vat from generated html
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/OfferToPDFConverter.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/OfferToPDFConverter.groovy
@@ -263,7 +263,6 @@ class OfferToPDFConverter implements OfferExporter {
     void setTaxationStatement() {
         if(!offer.getSelectedCustomerAffiliation().country.equals(countryWithVAT)) {
             htmlContent.getElementById("vat-cost-applicable").text("Taxation is not applied to offers outside of ${countryWithVAT}")
-            htmlContent.getElementById("total-taxes-ratio").text("VAT (0%)")
         }
 
     }
