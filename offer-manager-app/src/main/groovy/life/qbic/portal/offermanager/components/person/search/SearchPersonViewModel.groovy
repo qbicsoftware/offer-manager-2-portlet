a45b12ed808b0bbc1efd036a968920bfb40f0147
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-05-27 22:56:54 +0200

    Replace concrete classes with interfaces
    Introduce dependency injection to SearchPersonViewModel.groovy

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/person/search/SearchPersonViewModel.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/person/search/SearchPersonViewModel.groovy
@@ -1,29 +1,27 @@
 package life.qbic.portal.offermanager.components.person.search
 
-
 import life.qbic.datamodel.dtos.general.Person
-import life.qbic.portal.offermanager.dataresources.persons.PersonResourceService
 import life.qbic.portal.offermanager.communication.EventEmitter
+import life.qbic.portal.offermanager.dataresources.ResourcesService
 
 /**
  * View model of the SearchPerson use case
  *
  * This model holds all the data that is displayed in the respective view {@link SearchPersonView}
  *
- * @since: 1.0.0
- *
+ * @since: 1.0.0*
  */
 class SearchPersonViewModel {
 
     ObservableList availablePersons
 
-    private final PersonResourceService personService
+    private final ResourcesService<Person> personService
 
     Optional<Person> selectedPerson
     EventEmitter<Person> personEvent
 
-    SearchPersonViewModel(PersonResourceService personService,
-            EventEmitter<Person> personEvent) {
+    SearchPersonViewModel(ResourcesService<Person> personService,
+                          EventEmitter<Person> personEvent) {
         this.personService = personService
         this.personEvent = personEvent
         this.availablePersons = new ObservableList(new ArrayList<Person>())
@@ -43,7 +41,7 @@ class SearchPersonViewModel {
     }
 
     Person getSelectedPerson() {
-        if(selectedPerson.isPresent()) {
+        if (selectedPerson.isPresent()) {
             return selectedPerson.get()
         } else {
             throw new RuntimeException("No person is currently selected.")
