qbicsoftware/offer-manager-2-portlet

commit 32350b4fc5fa03e618ea3a167b659491d3db3edd

Fix/#612 refactor dependency manager by context (#624)
This PR restructures the DependencyManager. The components are now created in separate methods based on their context. A separate creation method allows for the creation of each component accessible from the AppView.

* Use ResourcesService interface instead of concrete classes

* Use DataSource interfaces instead of concrete classes outside the DB connection method

* Introduce missing interfaces between service and database connectors 

This does not introduce interfaces between database connectors but only between the service part and the database part

* Extract component construction into helper methods

* Update Changelog

* Change OfferOverviewView creation

For the OfferOverviewView to work the way the CreateProjectView was constructed
was changed. This change was necessary because the CreateProjectView is only
relevant for the OfferOverviewView. There must be a new CreateProjectView for every
OfferOverviewView to ensure that the component is only added there.

* Improve JavaDoc

Co-authored-by: jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>

* Add missing since tag

Co-authored-by: jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>

offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/offer/overview/OfferOverviewView.groovy
@@ -1,6 +1,5 @@
 package life.qbic.portal.offermanager.components.offer.overview
 
-
 import com.vaadin.data.provider.DataProvider
 import com.vaadin.data.provider.ListDataProvider
 import com.vaadin.icons.VaadinIcons
@@ -15,7 +14,6 @@ import com.vaadin.ui.renderers.TextRenderer
 import com.vaadin.ui.themes.ValoTheme
 import groovy.util.logging.Log4j2
 import life.qbic.business.offers.Currency
-import life.qbic.datamodel.dtos.business.Offer
 import life.qbic.portal.offermanager.OfferFileNameFormatter
 import life.qbic.portal.offermanager.components.GridUtils
 import life.qbic.portal.offermanager.components.offer.overview.projectcreation.CreateProjectView
@@ -51,15 +49,17 @@ class OfferOverviewView extends FormLayout {
 
     private Button createProjectButton
 
+    private FormLayout defaultContent
+
     OfferOverviewView(OfferOverviewModel model,
                       OfferOverviewController offerOverviewController,
                       CreateProjectView createProjectView) {
         this.model = model
         this.offerOverviewController = offerOverviewController
         this.overviewGrid = new Grid<>()
-        this.downloadBtn = new Button("Download Offer",VaadinIcons.DOWNLOAD)
-        this.updateOfferBtn = new Button("Update Offer",VaadinIcons.EDIT)
-        this.createProjectButton =  new Button("Create Project", VaadinIcons.PLUS_CIRCLE)
+        this.downloadBtn = new Button("Download Offer", VaadinIcons.DOWNLOAD)
+        this.updateOfferBtn = new Button("Update Offer", VaadinIcons.EDIT)
+        this.createProjectButton = new Button("Create Project", VaadinIcons.PLUS_CIRCLE)
         this.downloadSpinner = new ProgressBar()
         this.createProjectView = createProjectView
 
@@ -69,6 +69,17 @@ class OfferOverviewView extends FormLayout {
     }
 
     private void initLayout() {
+        this.setMargin(false)
+        this.setSpacing(false)
+        this.addComponent(createProjectView)
+        createProjectView.setVisible(false)
+        defaultContent = generateDefaultLayout()
+        this.addComponent(defaultContent)
+        defaultContent.setVisible(true)
+    }
+
+    private FormLayout generateDefaultLayout() {
+        FormLayout defaultContent = new FormLayout()
         /*
         We start with the header, that contains a descriptive
         title of what the view is about.
@@ -79,7 +90,7 @@ class OfferOverviewView extends FormLayout {
         label.addStyleName(ValoTheme.LABEL_HUGE)
         headerRow.addComponent(label)
         headerRow.setMargin(false)
-        this.addComponent(headerRow)
+        defaultContent.addComponent(headerRow)
 
         /*
         Below the header, we create content row with two components.
@@ -106,14 +117,16 @@ class OfferOverviewView extends FormLayout {
                 downloadSpinner)
 
         activityContainer.setMargin(false)
-        headerRow.addComponents(activityContainer,overviewGrid)
+        headerRow.addComponents(activityContainer, overviewGrid)
         headerRow.setSizeFull()
 
-        this.setMargin(false)
-        this.setSpacing(false)
+        defaultContent.setMargin(false)
+        defaultContent.setSpacing(false)
 
-        this.setWidthFull()
+        defaultContent.setWidthFull()
+        return defaultContent
     }
+
 
     private DataProvider setupDataProvider() {
         def dataProvider = new ListDataProvider(model.offerOverviewList)
@@ -122,23 +135,25 @@ class OfferOverviewView extends FormLayout {
     }
 
     private void setupGrid() {
-        Column<Offer, Date> dateColumn = overviewGrid.addColumn({ overview -> overview
-                .getModificationDate() }).setCaption("Creation Date").setId("CreationDate")
-        dateColumn.setRenderer(date -> date,  new DateRenderer('%1$tY-%1$tm-%1$td'))
-        overviewGrid.addColumn({overview -> overview.offerId.toString()})
+        Column<OfferOverview, Date> dateColumn = overviewGrid.addColumn({ overview ->
+            overview
+                    .getModificationDate()
+        }).setCaption("Creation Date").setId("CreationDate")
+        dateColumn.setRenderer(date -> date, new DateRenderer('%1$tY-%1$tm-%1$td'))
+        overviewGrid.addColumn({ overview -> overview.offerId.toString() })
                 .setCaption("Offer ID").setId("OfferId")
-        overviewGrid.addColumn({overview -> overview.getProjectTitle()})
+        overviewGrid.addColumn({ overview -> overview.getProjectTitle() })
                 .setCaption("Project Title").setId("ProjectTitle")
-        overviewGrid.addColumn({overview -> overview.getCustomer()})
+        overviewGrid.addColumn({ overview -> overview.getCustomer() })
                 .setCaption("Customer").setId("Customer")
-        overviewGrid.addColumn({overview -> overview.getProjectManager()})
+        overviewGrid.addColumn({ overview -> overview.getProjectManager() })
                 .setCaption("ProjectManager").setId("ProjectManager")
-        overviewGrid.addColumn({overview -> overview.getAssociatedProject()})
+        overviewGrid.addColumn({ overview -> overview.getAssociatedProject() })
                 .setCaption("Project ID").setId("ProjectID")
-                .setRenderer({maybeIdentifier -> maybeIdentifier.isPresent()? maybeIdentifier.get().toString() : "-"}, new TextRenderer())
+                .setRenderer({ maybeIdentifier -> maybeIdentifier.isPresent() ? maybeIdentifier.get().toString() : "-" }, new TextRenderer())
 
         // Format price by using a column renderer. This way the sorting will happen on the underlying double values, leading to expected behaviour.
-        Column<Offer, Double> priceColumn = overviewGrid.addColumn({overview -> overview.getTotalPrice()}).setCaption("Total Price")
+        Column<OfferOverview, Double> priceColumn = overviewGrid.addColumn({ overview -> overview.getTotalPrice() }).setCaption("Total Price")
         priceColumn.setRenderer(price -> Currency.getFormatterWithSymbol().format(price), new TextRenderer())
 
         overviewGrid.sort(dateColumn, SortDirection.DESCENDING)
@@ -178,9 +193,9 @@ class OfferOverviewView extends FormLayout {
             model.offerEventEmitter.emit(model.getSelectedOffer())
         })
         createProjectButton.addClickListener({
-            this.setVisible(false)
+            defaultContent.setVisible(false)
             createProjectView.setVisible(true)
-            createProjectView.model.startedFromView = Optional.of(this)
+            createProjectView.model.startedFromView = Optional.of(defaultContent)
             createProjectView.model.selectedOffer = Optional.of(model.selectedOffer)
         })
     }
@@ -188,7 +203,7 @@ class OfferOverviewView extends FormLayout {
     private void setupGridListeners() {
         overviewGrid.addSelectionListener(
                 { selection ->
-                    selection.firstSelectedItem.ifPresent({overview ->
+                    selection.firstSelectedItem.ifPresent({ overview ->
                         UI.getCurrent().setPollInterval(50)
                         downloadSpinner.setVisible(true)
                         new LoadOfferInfoThread(UI.getCurrent(), overview).start()
@@ -245,24 +260,24 @@ class OfferOverviewView extends FormLayout {
                 updateOfferBtn.setEnabled(false)
                 createProjectButton.setEnabled(false)
             })
-                offerOverviewController.fetchOffer(offerOverview.offerId)
-                createResourceForDownload()
+            offerOverviewController.fetchOffer(offerOverview.offerId)
+            createResourceForDownload()
 
-                ui.access(() -> {
-                    downloadSpinner.setVisible(false)
-                    overviewGrid.setSelectionMode(Grid.SelectionMode.SINGLE)
-                    // After we have set the single mode to NONE, the listeners seem to be gone
-                    // So we set them again
-                    // IMPORTANT: the selection must be set before we attach the listener,
-                    // otherwise the selection listener gets triggered (LOOP!)
-                    overviewGrid.select(selectedOffer.get())
-                    setupGridListeners()
-                    overviewGrid.setEnabled(true)
-                    downloadBtn.setEnabled(true)
-                    updateOfferBtn.setEnabled(true)
-                    checkProjectCreationAllowed(offerOverview)
-                    ui.setPollInterval(-1)
-                })
+            ui.access(() -> {
+                downloadSpinner.setVisible(false)
+                overviewGrid.setSelectionMode(Grid.SelectionMode.SINGLE)
+                // After we have set the single mode to NONE, the listeners seem to be gone
+                // So we set them again
+                // IMPORTANT: the selection must be set before we attach the listener,
+                // otherwise the selection listener gets triggered (LOOP!)
+                overviewGrid.select(selectedOffer.get())
+                setupGridListeners()
+                overviewGrid.setEnabled(true)
+                downloadBtn.setEnabled(true)
+                updateOfferBtn.setEnabled(true)
+                checkProjectCreationAllowed(offerOverview)
+                ui.setPollInterval(-1)
+            })
         }
     }
 

offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/offer/create/OfferOverviewView.groovy
@@ -3,23 +3,14 @@ package life.qbic.portal.offermanager.components.offer.create
 import com.vaadin.data.provider.ListDataProvider
 import com.vaadin.icons.VaadinIcons
 import com.vaadin.shared.ui.grid.HeightMode
-import com.vaadin.ui.Alignment
-import com.vaadin.ui.Button
-import com.vaadin.ui.Component
-import com.vaadin.ui.Grid
-import com.vaadin.ui.HorizontalLayout
-import com.vaadin.ui.Label
-import com.vaadin.ui.Panel
-import com.vaadin.ui.TextArea
-import com.vaadin.ui.VerticalLayout
+import com.vaadin.ui.*
 import com.vaadin.ui.components.grid.HeaderRow
 import com.vaadin.ui.renderers.NumberRenderer
 import com.vaadin.ui.themes.ValoTheme
 import groovy.util.logging.Log4j2
-import life.qbic.datamodel.dtos.business.services.Product
 import life.qbic.business.offers.Currency
+import life.qbic.datamodel.dtos.business.services.Product
 import life.qbic.portal.offermanager.components.GridUtils
-import life.qbic.portal.offermanager.dataresources.offers.OfferResourcesService
 
 /**
  * This class generates a Layout in which the user
@@ -29,11 +20,10 @@ import life.qbic.portal.offermanager.dataresources.offers.OfferResourcesService
  * with the intention of allowing the user to get an overview over all information he has entered before he creates and stores an
  * offer in the QBiC database.
  *
- * @since: 0.1.0
- *
+ * @since 0.1.0
  */
 @Log4j2
-class OfferOverviewView extends VerticalLayout{
+class OfferOverviewView extends VerticalLayout {
 
     private final CreateOfferViewModel createOfferViewModel
 
@@ -43,7 +33,7 @@ class OfferOverviewView extends VerticalLayout{
     Button save
     CreateOfferController createOfferController
 
-    OfferOverviewView(CreateOfferViewModel viewModel, CreateOfferController controller, OfferResourcesService service){
+    OfferOverviewView(CreateOfferViewModel viewModel, CreateOfferController controller) {
         this.createOfferViewModel = viewModel
         this.createOfferController = controller
         initLayout()
@@ -72,7 +62,7 @@ class OfferOverviewView extends VerticalLayout{
     /**
      * Initializes the start layout for this view
      */
-    private void initLayout(){
+    private void initLayout() {
         this.previous = new Button(VaadinIcons.CHEVRON_CIRCLE_LEFT)
         previous.addStyleName(ValoTheme.LABEL_LARGE)
 
@@ -90,7 +80,7 @@ class OfferOverviewView extends VerticalLayout{
 
         this.itemGrid = new Grid<>("Selected Items:")
 
-        this.addComponents(offerOverview,buttonLayout)
+        this.addComponents(offerOverview, buttonLayout)
         this.setMargin(false)
     }
 
@@ -123,7 +113,7 @@ class OfferOverviewView extends VerticalLayout{
     /**
      * Fills the information of the offer into the panel
      */
-    void fillPanel(){
+    void fillPanel() {
         /*
         Container for the complete overview
          */
@@ -163,12 +153,10 @@ class OfferOverviewView extends VerticalLayout{
         TextArea experimentInfo = new TextArea("Experimental design")
         experimentInfo.setIcon(VaadinIcons.NOTEBOOK)
         experimentInfo.setValue(
-                createOfferViewModel.experimentalDesign ? createOfferViewModel.experimentalDesign: "No design defined.")
+                createOfferViewModel.experimentalDesign ? createOfferViewModel.experimentalDesign : "No design defined.")
         experimentInfo.setEnabled(false)
         experimentInfo.setWidth("100%")
         projectInfoForm.addComponent(experimentInfo)
-
-
 
 
         /*
@@ -206,8 +194,9 @@ class OfferOverviewView extends VerticalLayout{
                 new PriceField("Total Price", createOfferViewModel.totalPrice)
         ])
         gridLayout.addColumn(PriceField::getName)
-        gridLayout.addColumn(  {
-            costs -> costs.value},
+        gridLayout.addColumn({
+            costs -> costs.value
+        },
                 new NumberRenderer(Currency.getFormatterWithSymbol()))
 
         gridLayout.headerVisible = false
@@ -220,6 +209,7 @@ class OfferOverviewView extends VerticalLayout{
     Small helper object, that will display information
     about individual price positions for offer overviews.
      */
+
     private class PriceField {
 
         String name

offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/OfferToPDFConverter.groovy
@@ -1,22 +1,10 @@
 package life.qbic.portal.offermanager
 
 import groovy.util.logging.Log4j2
-import life.qbic.datamodel.dtos.business.AcademicTitle
-import life.qbic.datamodel.dtos.business.Affiliation
-import life.qbic.datamodel.dtos.business.AffiliationCategory
-import life.qbic.datamodel.dtos.business.Customer
-import life.qbic.datamodel.dtos.business.Offer
-import life.qbic.datamodel.dtos.business.ProductItem
-import life.qbic.datamodel.dtos.business.ProjectManager
 import life.qbic.business.offers.Currency
 import life.qbic.business.offers.OfferExporter
-import life.qbic.datamodel.dtos.business.services.DataStorage
-import life.qbic.datamodel.dtos.business.services.MetabolomicAnalysis
-import life.qbic.datamodel.dtos.business.services.PrimaryAnalysis
-import life.qbic.datamodel.dtos.business.services.ProjectManagement
-import life.qbic.datamodel.dtos.business.services.ProteomicAnalysis
-import life.qbic.datamodel.dtos.business.services.SecondaryAnalysis
-import life.qbic.datamodel.dtos.business.services.Sequencing
+import life.qbic.datamodel.dtos.business.*
+import life.qbic.datamodel.dtos.business.services.*
 import org.jsoup.nodes.Document
 import org.jsoup.parser.Parser
 
@@ -33,8 +21,8 @@ import java.util.concurrent.TimeUnit
  *
  * Implements {@link OfferExporter} and is responsible for converting an offer into PDF Format
  *
- * @since: 1.0.0
- * @author: Jennifer Bödker
+ * @since 1.0.0 
+ * @author Jennifer Bödker
  *
  */
 @Log4j2
@@ -117,11 +105,11 @@ class OfferToPDFConverter implements OfferExporter {
         private String name
 
         ProductGroups(String name) {
-            this.name = name;
+            this.name = name
         }
 
         String getName() {
-            return this.name;
+            return this.name
         }
     }
     /**
@@ -183,7 +171,7 @@ class OfferToPDFConverter implements OfferExporter {
     private void setProjectInformation() {
         htmlContent.getElementById("project-title").text(offer.projectTitle)
         htmlContent.getElementById("project-description").text(offer.projectDescription)
-        if(offer.experimentalDesign.isPresent()) htmlContent.getElementById("experimental-design").text(offer.experimentalDesign.get())
+        if (offer.experimentalDesign.isPresent()) htmlContent.getElementById("experimental-design").text(offer.experimentalDesign.get())
     }
 
     private void setCustomerInformation() {
@@ -261,7 +249,7 @@ class OfferToPDFConverter implements OfferExporter {
     }
 
     void setTaxationStatement() {
-        if(!offer.getSelectedCustomerAffiliation().country.equals(countryWithVAT)) {
+        if (!offer.getSelectedCustomerAffiliation().country.equals(countryWithVAT)) {
             htmlContent.getElementById("vat-cost-applicable").text("Taxation is not applied to offers outside of ${countryWithVAT}")
         }
 
@@ -278,7 +266,7 @@ class OfferToPDFConverter implements OfferExporter {
 
     // Apply VAT only if the offer originated from Germany and it's affilation category is non-internal
     static double determineTaxCost(String country, AffiliationCategory category) {
-        if(country.equals(countryWithVAT) && !category.equals(noVatCategory)) {
+        if (country.equals(countryWithVAT) && !category.equals(noVatCategory)) {
             return VAT
         }
         return 0
@@ -332,10 +320,10 @@ class OfferToPDFConverter implements OfferExporter {
         htmlContent.getElementById("offer-date").text(dateFormat.format(offer.modificationDate))
     }
 
-    double calculateNetSum(List<ProductItem> productItems){
+    double calculateNetSum(List<ProductItem> productItems) {
         double netSum = 0
         productItems.each {
-                netSum += it.quantity * it.product.unitPrice
+            netSum += it.quantity * it.product.unitPrice
         }
         return netSum
     }
@@ -345,8 +333,7 @@ class OfferToPDFConverter implements OfferExporter {
         productItems.each {
             if (it.product.class in productGroupClasses[ProductGroups.DATA_MANAGEMENT]) {
                 overheadSum = 0
-        }
-            else {
+            } else {
                 overheadSum += it.quantity * it.product.unitPrice * offer.overheadRatio
             }
         }
@@ -375,24 +362,24 @@ class OfferToPDFConverter implements OfferExporter {
             }
         }
 
-            //Map Lists to the "DataGeneration", "DataAnalysis" and "Project and Data Management"
-            productItemsMap[ProductGroups.DATA_GENERATION] = dataGenerationItems
-            productItemsMap[ProductGroups.DATA_ANALYSIS] = dataAnalysisItems
-            productItemsMap[ProductGroups.DATA_MANAGEMENT] = dataManagementItems
+        //Map Lists to the "DataGeneration", "DataAnalysis" and "Project and Data Management"
+        productItemsMap[ProductGroups.DATA_GENERATION] = dataGenerationItems
+        productItemsMap[ProductGroups.DATA_ANALYSIS] = dataAnalysisItems
+        productItemsMap[ProductGroups.DATA_MANAGEMENT] = dataManagementItems
 
-            return productItemsMap
-        }
+        return productItemsMap
+    }
 
     void generateProductTable(Map productItemsMap, int maxTableItems) {
         // Create the items in html in the overview table
         int itemNumber = 0
-        productItemsMap.each {ProductGroups productGroup, List<ProductItem> items ->
+        productItemsMap.each { ProductGroups productGroup, List<ProductItem> items ->
             //Check if there are ProductItems stored in map entry
-            if(items){
+            if (items) {
                 def elementId = "product-items" + "-" + tableCount
                 //Append Table Title
                 htmlContent.getElementById(elementId).append(ItemPrintout.tableTitle(productGroup))
-                items.each{ProductItem item ->
+                items.each { ProductItem item ->
                     itemNumber++
                     //start (next) table and add Product to it
                     if (tableItemsCount >= maxTableItems) {
@@ -451,8 +438,8 @@ class OfferToPDFConverter implements OfferExporter {
             builder.redirectErrorStream(true)
             Process process = builder.start()
             process.waitFor(10, TimeUnit.SECONDS)
-            process.getInputStream().eachLine {log.info(it)}
-            if (! new File(output.toString()).exists()) {
+            process.getInputStream().eachLine { log.info(it) }
+            if (!new File(output.toString()).exists()) {
                 throw new RuntimeException("Offer PDF has not been generated.")
             }
         }
@@ -495,9 +482,9 @@ class OfferToPDFConverter implements OfferExporter {
                              """
         }
 
-        static String tableTitle(ProductGroups productGroup){
+        static String tableTitle(ProductGroups productGroup) {
 
-            String tableTitle= productGroup.getName()
+            String tableTitle = productGroup.getName()
 
 
             return """<div class = "small-spacer"</div>
@@ -505,7 +492,7 @@ class OfferToPDFConverter implements OfferExporter {
                    """
         }
 
-        static String subTableFooter(ProductGroups productGroup){
+        static String subTableFooter(ProductGroups productGroup) {
 
             String footerTitle = productGroup.getName()
 
@@ -527,7 +514,7 @@ class OfferToPDFConverter implements OfferExporter {
                                  """
         }
 
-        static String tableFooter(double overheadRatio, Affiliation affiliation){
+        static String tableFooter(double overheadRatio, Affiliation affiliation) {
 
             DecimalFormat decimalFormat = new DecimalFormat("#%")
             String overheadPercentage = decimalFormat.format(overheadRatio)
@@ -556,5 +543,5 @@ class OfferToPDFConverter implements OfferExporter {
                                  """
         }
 
-        }
+    }
 }

offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/AppView.groovy
@@ -5,18 +5,14 @@ import com.vaadin.server.Page
 import com.vaadin.ui.*
 import com.vaadin.ui.MenuBar.MenuItem
 import com.vaadin.ui.themes.ValoTheme
-import groovy.transform.CompileStatic
 import groovy.util.logging.Log4j2
 import life.qbic.portal.offermanager.components.affiliation.create.CreateAffiliationView
 import life.qbic.portal.offermanager.components.affiliation.search.SearchAffiliationView
-import life.qbic.portal.offermanager.components.affiliation.search.SearchAffiliationViewModel
 import life.qbic.portal.offermanager.components.offer.create.CreateOfferView
-import life.qbic.portal.offermanager.components.offer.overview.projectcreation.CreateProjectView
-import life.qbic.portal.offermanager.components.person.create.CreatePersonView
 import life.qbic.portal.offermanager.components.offer.overview.OfferOverviewView
+import life.qbic.portal.offermanager.components.person.create.CreatePersonView
 import life.qbic.portal.offermanager.components.person.search.SearchPersonView
 import life.qbic.portal.offermanager.components.product.MaintainProductsView
-
 
 /**
  * Class which connects the view elements with the ViewModel and the Controller
@@ -25,7 +21,6 @@ import life.qbic.portal.offermanager.components.product.MaintainProductsView
  * and layout upon which the views are presented
  *
  * @since 1.0.0
- *
  */
 @Log4j2
 class AppView extends VerticalLayout {
@@ -40,7 +35,6 @@ class AppView extends VerticalLayout {
     private final OfferOverviewView overviewView
     private final SearchPersonView searchPersonView
     private final MaintainProductsView maintainProductsView
-    private final CreateProjectView createProjectView
     private final CreateOfferView updateOfferView
 
     AppView(AppViewModel portletViewModel,
@@ -51,8 +45,7 @@ class AppView extends VerticalLayout {
             OfferOverviewView overviewView,
             CreateOfferView updateOfferView,
             SearchPersonView searchPersonView,
-            MaintainProductsView maintainProductsView,
-            CreateProjectView createProjectView) {
+            MaintainProductsView maintainProductsView) {
         super()
         this.portletViewModel = portletViewModel
         this.createPersonView = createPersonView
@@ -64,7 +57,6 @@ class AppView extends VerticalLayout {
         this.updateOfferView = updateOfferView
         this.searchPersonView = searchPersonView
         this.maintainProductsView = maintainProductsView
-        this.createProjectView = createProjectView
 
         initLayout()
         registerListeners()
@@ -87,13 +79,12 @@ class AppView extends VerticalLayout {
                 overviewView,
                 updateOfferView,
                 searchPersonView,
-                maintainProductsView,
-                createProjectView
+                maintainProductsView
         ])
     }
 
     private void hideAllFeatureViews() {
-        featureViews.each {it.setVisible(false)}
+        featureViews.each { it.setVisible(false) }
     }
 
     /**
@@ -117,7 +108,6 @@ class AppView extends VerticalLayout {
         verticalLayout.addComponent(this.updateOfferView)
         verticalLayout.addComponent(this.searchPersonView)
         verticalLayout.addComponent(this.maintainProductsView)
-        verticalLayout.addComponent(this.createProjectView)
 
         this.setSizeFull()
         this.addComponent(verticalLayout)
@@ -151,14 +141,14 @@ class AppView extends VerticalLayout {
         })
 
         this.updateOfferView.viewModel.addPropertyChangeListener("offerCreatedSuccessfully", {
-            if(it.newValue as Boolean){
+            if (it.newValue as Boolean) {
                 this.updateOfferView.setVisible(false)
                 this.overviewView.setVisible(true)
             }
         })
 
         this.createOfferView.viewModel.addPropertyChangeListener("offerCreatedSuccessfully", {
-            if(it.newValue as Boolean){
+            if (it.newValue as Boolean) {
                 this.createOfferView.setVisible(false)
                 this.overviewView.setVisible(true)
             }
@@ -184,18 +174,18 @@ class AppView extends VerticalLayout {
         private void createMenuBar() {
             this.addComponent(dropDownButton("Offers",
                     [
-                            "New Offer": {toggleView(createOfferView)},
-                            "Offer Overview": {toggleView(overviewView)}
+                            "New Offer"     : { toggleView(createOfferView) },
+                            "Offer Overview": { toggleView(overviewView) }
                     ]))
             this.addComponent(dropDownButton("Persons",
                     [
-                            "New Person": {toggleView(createPersonView)},
-                            "Search": {toggleView(searchPersonView)}
+                            "New Person": { toggleView(createPersonView) },
+                            "Search"    : { toggleView(searchPersonView) }
                     ]))
             this.addComponent(dropDownButton("Affiliations",
                     [
-                            "New Affiliation": {toggleView(createAffiliationView)},
-                            "Search": {toggleView(searchAffiliationView)}
+                            "New Affiliation": { toggleView(createAffiliationView) },
+                            "Search"         : { toggleView(searchAffiliationView) }
                     ]))
             Button maintainProducts = new Button("Service Products", VaadinIcons.GRID_BIG)
             maintainProducts.setStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED)

commit 95681077d5ea632c14320197d3034869a0dc698c

Remove FormLayout for layouts with children (#636)
This is a fix for an issue introduced in this version by #624 
The issue was that the FormLayout is now also provided with layouts as child components. This confuses the layout of the FormLayout vaadin class.

offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/offer/overview/OfferOverviewView.groovy
@@ -29,7 +29,7 @@ import life.qbic.portal.offermanager.dataresources.offers.OfferOverview
  * @since 1.0.0
  */
 @Log4j2
-class OfferOverviewView extends FormLayout {
+class OfferOverviewView extends VerticalLayout {
 
     final private OfferOverviewModel model
 

offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/person/create/CreatePersonView.groovy
@@ -30,7 +30,7 @@ import life.qbic.portal.offermanager.components.affiliation.create.CreateAffilia
  */
 
 @Log4j2
-class CreatePersonView extends FormLayout implements Resettable{
+class CreatePersonView extends VerticalLayout implements Resettable{
     protected final AppViewModel sharedViewModel
     protected final CreatePersonViewModel createPersonViewModel
     final CreatePersonController controller