435fe152c81fd68b38afa1f7f93a998434bcd0ca
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-06-01 11:49:00 +0200

    Rework view model binding
    Reset now resets the underlying model triggering events that clear the fields.
    ViewModels are not bound bny value change and property change events.
    Reset of the valid properties of the view model now leads to removal of component errors.

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/offer/create/ProjectInformationView.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/offer/create/ProjectInformationView.groovy
@@ -1,17 +1,12 @@
 package life.qbic.portal.offermanager.components.offer.create
 
-import com.vaadin.data.Binder
+
 import com.vaadin.data.ValidationResult
 import com.vaadin.data.Validator
 import com.vaadin.data.ValueContext
 import com.vaadin.icons.VaadinIcons
 import com.vaadin.server.UserError
-import com.vaadin.ui.Alignment
-import com.vaadin.ui.Button
-import com.vaadin.ui.HorizontalLayout
-import com.vaadin.ui.TextArea
-import com.vaadin.ui.TextField
-import com.vaadin.ui.VerticalLayout
+import com.vaadin.ui.*
 import com.vaadin.ui.themes.ValoTheme
 import life.qbic.portal.offermanager.components.Resettable
 
@@ -23,8 +18,7 @@ import life.qbic.portal.offermanager.components.Resettable
  * with the intention of enabling a user the adding the project information for which an offer will be created in the
  * QBiC database.
  *
- * @since: 0.1.0
- *
+ * @since 0.1.0
  */
 class ProjectInformationView extends VerticalLayout implements Resettable {
 
@@ -80,9 +74,9 @@ class ProjectInformationView extends VerticalLayout implements Resettable {
     }
 
     private void resetContent() {
-        this.projectTitle.setValue("")
-        this.projectObjective.setValue("")
-        this.experimentalDesign.setValue("")
+        this.createOfferViewModel.projectTitle = null
+        this.createOfferViewModel.projectObjective = null
+        this.createOfferViewModel.experimentalDesign = null
     }
 
 
@@ -90,65 +84,51 @@ class ProjectInformationView extends VerticalLayout implements Resettable {
      * This method connects the form fields to the corresponding values in the view model
      */
     private void bindViewModel() {
-        Binder<CreateOfferViewModel> binder = new Binder<>()
 
-        // by binding the fields to the view model, the model is updated when the user input changed
-        binder.setBean(createOfferViewModel)
+        // bind projectTitle
+        createOfferViewModel.addPropertyChangeListener("projectTitle") {
+            String newValue = it.newValue as String
+            projectTitle.value = newValue ?: projectTitle.emptyValue
+        }
+        projectTitle.addValueChangeListener({
+            createOfferViewModel.projectTitle = it.value
+        })
 
-        binder.forField(projectTitle)
-                .bind({ it.projectTitle }, { it, updatedValue -> it.setProjectTitle(updatedValue) })
-        binder.forField(projectObjective)
-                .bind({ it.projectObjective }, { it, updatedValue -> it.setProjectObjective(updatedValue) })
-        binder.forField(experimentalDesign)
-                .bind({ it.experimentalDesign }, { it, updatedValue -> it.setExperimentalDesign(updatedValue) })
+        // bind projectObjective
+        createOfferViewModel.addPropertyChangeListener("projectObjective", {
+            String newValue = it.newValue as String
+            projectObjective.value = newValue ?: projectObjective.emptyValue
+        })
+        projectObjective.addValueChangeListener({
+            createOfferViewModel.projectObjective = it.value
+        })
 
-
-        /*
-        Here we setup a listener to the viewModel that hold displayed information.
-        The listener is needed since Vaadin bindings only work one-way
-
-        Please NOTE: we cannot use the binder.readBean(binder.getBean) refresh here since it would
-        overwrite all validators attached to the fields. We furthermore cannot use the
-        BinderBuilder#withValidator method since this would prevent the form from showing invalid
-        information that is stored within the viewModel. We want the view to reflect the view model
-        at all times!
-         */
-        createOfferViewModel.addPropertyChangeListener({it ->
-            switch (it.propertyName) {
-                case "projectTitle":
-                    String newValue = it.newValue as String
-                    projectTitle.value = newValue ?: projectTitle.emptyValue
-                    break
-                case "projectObjective":
-                    String newValue = it.newValue as String
-                    projectObjective.value = newValue ?: projectObjective.emptyValue
-                    break
-                case "experimentalDesign":
-                    String newValue = it.newValue as String
-                    experimentalDesign.value = newValue ?: experimentalDesign.emptyValue
-                    break
-                default:
-                    break
-            }
+        // bind experimentalDesign
+        createOfferViewModel.addPropertyChangeListener("experimentalDesign", {
+            String newValue = it.newValue as String
+            experimentalDesign.value = newValue ?: experimentalDesign.emptyValue
+        })
+        experimentalDesign.addValueChangeListener({
+            createOfferViewModel.experimentalDesign = it.value
         })
 
         /*
         we listen to the valid properties. whenever the presenter resets values in the viewmodel
         and resets the valid properties the component error on the respective component is removed
         */
-        createOfferViewModel.addPropertyChangeListener({it ->
+        createOfferViewModel.addPropertyChangeListener({ it ->
             switch (it.propertyName) {
-                case "projectTitle":
+                case "projectTitleValid":
                     if (it.newValue || it.newValue == null) {
                         projectTitle.componentError = null
                     }
                     break
-                case "projectObjective":
+                case "projectObjectiveValid":
                     if (it.newValue || it.newValue == null) {
                         projectObjective.componentError = null
                     }
                     break
-                case "experimentalDesign":
+                case "experimentalDesignValid":
                     if (it.newValue || it.newValue == null) {
                         experimentalDesign.componentError = null
                     }
@@ -156,21 +136,20 @@ class ProjectInformationView extends VerticalLayout implements Resettable {
                 default:
                     break
             }
-            //todo should not always be enabled
             next.enabled = allValuesValid()
         })
     }
 
-    private boolean allValuesValid(){
+    private boolean allValuesValid() {
 
         return createOfferViewModel.projectTitleValid &&
                 createOfferViewModel.projectObjectiveValid &&
                 createOfferViewModel.experimentalDesignValid
     }
 
-    private void setupValidators(){
-        Validator<String> notNullValidator =  Validator.from({ String value -> (value != null)}, "Please provide a valid description.")
-        Validator<String> notEmptyValidator =  Validator.from({ String value -> (value && !value.trim().empty)}, "Please provide a valid description.")
+    private void setupValidators() {
+        Validator<String> notNullValidator = Validator.from({ String value -> (value != null) }, "Please provide a valid description.")
+        Validator<String> notEmptyValidator = Validator.from({ String value -> (value && !value.trim().empty) }, "Please provide a valid description.")
 
 
         //Add Listeners to all Fields in the Form layout
