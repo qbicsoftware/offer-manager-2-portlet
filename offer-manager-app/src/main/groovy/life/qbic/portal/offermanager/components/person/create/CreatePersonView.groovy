7b35be8cd22622fc56df05fa948807191feb5903
Steffengreiner <Steffen.Greiner@gmx.de>
2021-06-04 15:33:32 +0200

    Check and validate changes in academicTitle during person creation and update (#637)
    * Make titlefield mandatory during create and update person view
    
    * Update Changelog
    
    * Set default value to none
    
    * Remove unnecessary line from initDefaultValue
    
    * Overwrite and extend the allValuesValid method
    
    * Set title field required in CreatePersonView.groovy
    
    UpdatePersonView.groovy now inherits those properties and does not set them itself
    
    Co-authored-by: Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/person/create/CreatePersonView.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/person/create/CreatePersonView.groovy
@@ -14,6 +14,7 @@ import com.vaadin.shared.ui.ContentMode
 import com.vaadin.ui.*
 import com.vaadin.ui.themes.ValoTheme
 import groovy.util.logging.Log4j2
+import life.qbic.datamodel.dtos.business.AcademicTitle
 import life.qbic.datamodel.dtos.business.Affiliation
 import life.qbic.portal.offermanager.components.AppViewModel
 import life.qbic.portal.offermanager.components.Resettable
@@ -26,11 +27,11 @@ import life.qbic.portal.offermanager.components.affiliation.create.CreateAffilia
  * CreatePersonViewModel will be integrated into the qOffer 2.0 Portlet and provides an User Interface
  * with the intention of enabling a user the creation of a new person in the QBiC Database
  *
- * @since: 1.0.0
+ * @since 1.0.0
  */
 
 @Log4j2
-class CreatePersonView extends VerticalLayout implements Resettable{
+class CreatePersonView extends VerticalLayout implements Resettable {
     protected final AppViewModel sharedViewModel
     protected final CreatePersonViewModel createPersonViewModel
     final CreatePersonController controller
@@ -64,6 +65,7 @@ class CreatePersonView extends VerticalLayout implements Resettable{
         bindViewModel()
         setupFieldValidators()
         registerListeners()
+        initDefaultValues()
     }
 
     private AbstractOrderedLayout generateViewContent() {
@@ -74,6 +76,8 @@ class CreatePersonView extends VerticalLayout implements Resettable{
         defaultContent.addComponent(viewCaption)
 
         this.titleField = generateTitleSelector(createPersonViewModel.academicTitles)
+        titleField.setRequiredIndicatorVisible(true)
+        titleField.setEmptySelectionAllowed(false)
 
         this.firstNameField = new TextField("First Name")
         firstNameField.setPlaceholder("First name")
@@ -104,7 +108,7 @@ class CreatePersonView extends VerticalLayout implements Resettable{
 
         this.addressAdditionComboBox = new ComboBox<>("Address Addition")
         addressAdditionComboBox.setRequiredIndicatorVisible(false)
-        addressAdditionComboBox.setItemCaptionGenerator({(it.addressAddition == ""|| it.addressAddition == " ")? "no address addition" : it.addressAddition})
+        addressAdditionComboBox.setItemCaptionGenerator({ (it.addressAddition == "" || it.addressAddition == " ") ? "no address addition" : it.addressAddition })
         addressAdditionComboBox.setEnabled(false)
         addressAdditionComboBox.setEmptySelectionAllowed(false)
 
@@ -183,25 +187,25 @@ class CreatePersonView extends VerticalLayout implements Resettable{
      */
     private void bindViewModel() {
 
-        this.titleField.addValueChangeListener({this.createPersonViewModel.academicTitle = it.value })
+        this.titleField.addValueChangeListener({ this.createPersonViewModel.academicTitle = it.value })
         createPersonViewModel.addPropertyChangeListener("academicTitle", {
             String newValue = it.newValue as String
             titleField.value = newValue ?: titleField.emptyValue
         })
 
-        this.firstNameField.addValueChangeListener({this.createPersonViewModel.firstName = it.value })
+        this.firstNameField.addValueChangeListener({ this.createPersonViewModel.firstName = it.value })
         createPersonViewModel.addPropertyChangeListener("firstName", {
             String newValue = it.newValue as String
             firstNameField.value = newValue ?: firstNameField.emptyValue
         })
 
-        this.lastNameField.addValueChangeListener({this.createPersonViewModel.lastName = it.value })
+        this.lastNameField.addValueChangeListener({ this.createPersonViewModel.lastName = it.value })
         createPersonViewModel.addPropertyChangeListener("lastName", {
             String newValue = it.newValue as String
             lastNameField.value = newValue ?: lastNameField.emptyValue
         })
 
-        this.emailField.addValueChangeListener({this.createPersonViewModel.email = it.value })
+        this.emailField.addValueChangeListener({ this.createPersonViewModel.email = it.value })
         createPersonViewModel.addPropertyChangeListener("email", {
             String newValue = it.newValue as String
             emailField.value = newValue ?: emailField.emptyValue
@@ -218,7 +222,8 @@ class CreatePersonView extends VerticalLayout implements Resettable{
                 addressAdditionComboBox.value = newValue
             } else {
                 addressAdditionComboBox.value = addressAdditionComboBox.emptyValue
-                organisationComboBox.value = organisationComboBox.emptyValue            }
+                organisationComboBox.value = organisationComboBox.emptyValue
+            }
         })
 
         createPersonViewModel.addPropertyChangeListener("affiliationViewVisible", {
@@ -229,7 +234,7 @@ class CreatePersonView extends VerticalLayout implements Resettable{
         we listen to the valid properties. whenever the presenter resets values in the viewmodel
         and resets the valid properties the component error on the respective component is removed
         */
-        createPersonViewModel.addPropertyChangeListener({it ->
+        createPersonViewModel.addPropertyChangeListener({ it ->
             switch (it.propertyName) {
                 case "academicTitleValid":
                     if (it.newValue || it.newValue == null) {
@@ -275,7 +280,7 @@ class CreatePersonView extends VerticalLayout implements Resettable{
 
         ListDataProvider<Affiliation> dataProvider = organisation.affiliations
         this.addressAdditionComboBox.setDataProvider(dataProvider)
-        dataProvider.setSortOrder({it.addressAddition}, SortDirection.ASCENDING)
+        dataProvider.setSortOrder({ it.addressAddition }, SortDirection.ASCENDING)
 
         this.addressAdditionComboBox.setSelectedItem(dataProvider.getItems().getAt(0))
     }
@@ -285,11 +290,21 @@ class CreatePersonView extends VerticalLayout implements Resettable{
      */
     private void setupFieldValidators() {
 
-        Validator<String> nameValidator =  Validator.from({String value -> (value && !value.trim().empty)}, "Please provide a valid name.")
+        Validator<String> nameValidator = Validator.from({ String value -> (value && !value.trim().empty) }, "Please provide a valid name.")
         Validator<String> emailValidator = new EmailValidator("Please provide a valid email address.")
-        Validator<? extends Object> selectionValidator = Validator.from({o -> o != null}, "Please make a selection.")
+        Validator<? extends Object> selectionValidator = Validator.from({ o -> o != null }, "Please make a selection.")
 
         //Add Listeners to all Fields in the Form layout
+        this.titleField.addSelectionListener({ selection ->
+            ValidationResult result = selectionValidator.apply(selection.getValue(), new ValueContext(this.titleField))
+            if (result.isError()) {
+                createPersonViewModel.academicTitleValid = false
+                UserError error = new UserError(result.getErrorMessage())
+                titleField.setComponentError(error)
+            } else {
+                createPersonViewModel.academicTitleValid = true
+            }
+        })
         this.firstNameField.addValueChangeListener({ event ->
             ValidationResult result = nameValidator.apply(event.getValue(), new ValueContext(this.firstNameField))
             if (result.isError()) {
@@ -341,7 +356,7 @@ class CreatePersonView extends VerticalLayout implements Resettable{
         ComboBox<Organisation> organisationComboBox =
                 new ComboBox<>("Organisation")
         organisationComboBox.setPlaceholder("Select person affiliation organisation")
-        organisationComboBox.setItemCaptionGenerator({it.name})
+        organisationComboBox.setItemCaptionGenerator({ it.name })
         ListDataProvider<Organisation> dataProvider = new ListDataProvider<>(organisations)
         organisationComboBox.setDataProvider(dataProvider)
         organisationComboBox.setEmptySelectionAllowed(false)
@@ -357,8 +372,16 @@ class CreatePersonView extends VerticalLayout implements Resettable{
                 new ComboBox<>("Academic Title")
         titleCombobox.setPlaceholder("Select academic title")
         titleCombobox.setItems(academicTitles)
-        titleCombobox.setEmptySelectionAllowed(true)
+        titleCombobox.setEmptySelectionAllowed(false)
         return titleCombobox
+    }
+
+    /**
+     * Initializes the default values to be set in the components of the CreatePersonVieW
+     * @return
+     */
+    private void initDefaultValues() {
+        createPersonViewModel.academicTitle = AcademicTitle.NONE.toString()
     }
 
     /**
@@ -367,10 +390,11 @@ class CreatePersonView extends VerticalLayout implements Resettable{
      * @return
      */
     protected boolean allValuesValid() {
-        return createPersonViewModel.firstNameValid \
-            && createPersonViewModel.lastNameValid \
-            && createPersonViewModel.emailValid \
-            && createPersonViewModel.affiliationValid
+        return createPersonViewModel.academicTitleValid \
+             && createPersonViewModel.firstNameValid \
+             && createPersonViewModel.lastNameValid \
+             && createPersonViewModel.emailValid \
+             && createPersonViewModel.affiliationValid
     }
 
     private void registerListeners() {
@@ -384,7 +408,7 @@ class CreatePersonView extends VerticalLayout implements Resettable{
                 List<Affiliation> affiliations = new ArrayList()
                 affiliations.add(createPersonViewModel.affiliation)
 
-                if(!createPersonViewModel.outdatedPerson){
+                if (!createPersonViewModel.outdatedPerson) {
                     controller.createNewPerson(firstName, lastName, title, email, affiliations)
                 }
 
@@ -399,7 +423,7 @@ class CreatePersonView extends VerticalLayout implements Resettable{
         })
 
         this.organisationComboBox.addSelectionListener({
-            if(it.selectedItem.isPresent()){
+            if (it.selectedItem.isPresent()) {
                 refreshAddressAdditions(it.selectedItem.get())
             }
         })
@@ -475,7 +499,7 @@ class CreatePersonView extends VerticalLayout implements Resettable{
     protected Optional<Organisation> findOrganisation(Affiliation affiliation) {
         Optional<Organisation> foundOrganisation = Optional.empty()
         createPersonViewModel.availableOrganisations.each {
-            if(affiliation in (it as Organisation).affiliations) foundOrganisation = Optional.of((it as Organisation))
+            if (affiliation in (it as Organisation).affiliations) foundOrganisation = Optional.of((it as Organisation))
         }
 
         return foundOrganisation
@@ -498,5 +522,6 @@ class CreatePersonView extends VerticalLayout implements Resettable{
     void reset() {
         createPersonViewModel.reset()
         clearAllFields()
+        initDefaultValues()
     }
 }
