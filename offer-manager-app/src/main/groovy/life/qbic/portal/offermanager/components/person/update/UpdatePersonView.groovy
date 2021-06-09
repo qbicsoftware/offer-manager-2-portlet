354bbe72915b3e5b0e842a76554f6f8bbbb7c2b8
Tobias Koch <KochTobi@users.noreply.github.com>
2021-06-09 15:13:02 +0200

    Listen to affiliations of person instead of selected affiliation (#655)
    * Listen to affiliations of the person instead of selected affiliation
    
    * Update Changelog

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/person/update/UpdatePersonView.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/person/update/UpdatePersonView.groovy
@@ -44,6 +44,11 @@ class UpdatePersonView extends CreatePersonView {
         submitButton.caption = "Update Person"
         abortButton.caption = "Abort Person Update"
 
+        // remove required indicator since affiliationValid is no longer required to update
+        // it is replaced by affiliationsValid
+        organisationComboBox.setRequiredIndicatorVisible(false)
+        addressAdditionComboBox.setRequiredIndicatorVisible(false)
+
         //be careful when adding new components to the view
         //it is based on the CreatePersonView, you might disrupt the view when adding new components to the wrong position
 
@@ -198,6 +203,20 @@ class UpdatePersonView extends CreatePersonView {
                 }
                 submitButton.enabled = allValuesValid()
             }
+
+            updatePersonViewModel.affiliationList.addPropertyChangeListener({
+                //validation
+                updatePersonViewModel.affiliationsValid = ! updatePersonViewModel.affiliationList.isEmpty()
+                if(updatePersonViewModel.outdatedPerson) {
+                    List<Affiliation> originalList = updatePersonViewModel.outdatedPerson.affiliations
+                    List<Affiliation> viewModelList = updatePersonViewModel.affiliationList
+                    boolean affiliationsChanged = ! (originalList == viewModelList)
+                    // set the changed needs to be triggered after the validity update
+                    updatePersonViewModel.personUpdated = updatePersonViewModel.affiliationsValid && affiliationsChanged
+                } else {
+                    log.warn("Tried to check for person affiliation changes. There is no person to update.")
+                }
+            })
         })
     }
 
@@ -217,7 +236,11 @@ class UpdatePersonView extends CreatePersonView {
      */
     @Override
     protected boolean allValuesValid() {
-        return super.allValuesValid()
-                && updatePersonViewModel.personUpdated
+        return createPersonViewModel.academicTitleValid \
+             && createPersonViewModel.firstNameValid \
+             && createPersonViewModel.lastNameValid \
+             && createPersonViewModel.emailValid \
+             && updatePersonViewModel.personUpdated \
+             && updatePersonViewModel.affiliationsValid
     }
 }
