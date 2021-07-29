407da9262c2272501c251b55eb748319f511eb9e
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-07-29 10:36:23 +0200

    add quantity discounts

--- offer-manager-domain/src/main/groovy/life/qbic/business/offers/OfferItem.groovy
+++ offer-manager-domain/src/main/groovy/life/qbic/business/offers/OfferItem.groovy
@@ -1,12 +1,6 @@
 package life.qbic.business.offers
 
 import groovy.transform.EqualsAndHashCode
-import life.qbic.business.offers.identifier.OfferId
-import life.qbic.datamodel.dtos.business.Affiliation
-import life.qbic.datamodel.dtos.business.Customer
-import life.qbic.datamodel.dtos.business.ProductItem
-import life.qbic.datamodel.dtos.business.ProjectManager
-import life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier
 
 /**
  * <h1>Item that contains the information about a product item on the offer</h1>
@@ -22,6 +16,7 @@ class OfferItem {
     private String productDescription
     private String productName
     private double unitPrice
+    private double quantityDiscount
     private String serviceProvider
     private String unit
     private double itemTotal
@@ -31,15 +26,17 @@ class OfferItem {
         String productDescription
         String productName
         double unitPrice
+        double quantityDiscount
         String serviceProvider
         String unit
         double itemTotal
 
-        Builder(double quantity, String productDescription, String productName, double unitPrice, String serviceProvider, String unit, double itemTotal) {
+        Builder(double quantity, String productDescription, String productName, double unitPrice, double quantityDiscount, String serviceProvider, String unit, double itemTotal) {
             this.quantity =  Objects.requireNonNull(quantity, "Customer must not be null")
             this.productDescription =  Objects.requireNonNull(productDescription, "Customer must not be null")
             this.productName =  Objects.requireNonNull(productName, "Customer must not be null")
             this.unitPrice =  Objects.requireNonNull(unitPrice, "Customer must not be null")
+            this.quantityDiscount =  Objects.requireNonNull(quantityDiscount, "Customer must not be null")
             this.serviceProvider =  Objects.requireNonNull(serviceProvider, "Customer must not be null")
             this.unit =  Objects.requireNonNull(unit, "Customer must not be null")
             this.itemTotal =  Objects.requireNonNull(itemTotal, "Customer must not be null")
@@ -56,6 +53,7 @@ class OfferItem {
         this.productDescription = builder.productDescription
         this.productName = builder.productName
         this.unitPrice = builder.unitPrice
+        this.quantityDiscount = quantityDiscount
         this.serviceProvider = builder.serviceProvider
         this.unit = builder.unit
         this.itemTotal = builder.itemTotal
