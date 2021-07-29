ec4244afe2e0ce2e7ba9149f0495e66df7b4497a
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2021-07-29 10:39:22 +0200

    add equality test

--- offer-manager-domain/src/test/groovy/life/qbic/business/offers/OfferItemSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/business/offers/OfferItemSpec.groovy
@@ -12,8 +12,17 @@ class OfferItemSpec extends Specification
 {
     def "OfferItem is created successfully"() {
         when:"a offerItem is created"
-        new OfferItem.Builder(2,"description","name",2.0,"Quantitative Biology Center","Sample",30.00).build()
+        new OfferItem.Builder(2,"description","name",2.0,1.0,"Quantitative Biology Center","Sample",30.00).build()
         then: "no error is thrown"
         noExceptionThrown()
+    }
+
+    def "OfferItems with the same content are equal"() {
+        when:"two offerItem are created with the same content"
+        OfferItem offerItem = new OfferItem.Builder(2,"description","name",2.0,1.0,"Quantitative Biology Center","Sample",30.00).build()
+        OfferItem theSame = new OfferItem.Builder(2,"description","name",2.0,1.0,"Quantitative Biology Center","Sample",30.00).build()
+
+        then: "offeritems are the same"
+        offerItem.equals(theSame)
     }
 }
