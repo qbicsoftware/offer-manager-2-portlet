ff2a279029318c5b80e4a23dcb366649272a13df
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-07-29 11:27:17 +0200

    Add difference tests

--- offer-manager-domain/src/test/groovy/life/qbic/business/offers/OfferItemSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/business/offers/OfferItemSpec.groovy
@@ -25,4 +25,26 @@ class OfferItemSpec extends Specification
         then: "offeritems are the same"
         offerItem.equals(theSame)
     }
+
+    def "OfferItems with the different content are different"() {
+        when:"two offerItem are created with different content"
+        OfferItem offerItem = new OfferItem.Builder(quantity,productDescription,productName,unitPrice,quantityDiscount,serviceProvider,unit,itemTotal).build()
+        OfferItem offerItem2 = new OfferItem.Builder(quantity2, productDescription2, productName2, unitPrice2, quantityDiscount2,serviceProvider2,unit2,itemTotal2).build()
+
+        then: "offeritems are the different"
+        ! offerItem.equals(offerItem2)
+
+        where: "the values are as follows"
+        quantity | productDescription | productName | unitPrice | quantityDiscount | serviceProvider | unit | itemTotal | quantity2 | productDescription2 | productName2 | unitPrice2 | quantityDiscount2 | serviceProvider2 | unit2 | itemTotal2
+        3|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00 | 2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00
+        2|"description2"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00 | 2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00
+        2|"description"|"name2"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00 | 2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00
+        2|"description"|"name"|3.0|1.0|"Quantitative Biology Center"|"Sample"|30.00 | 2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00
+        2|"description"|"name"|2.0|3.0|"Quantitative Biology Center"|"Sample"|30.00 | 2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00
+        2|"description"|"name"|2.0|1.0|"Some place"|"Sample"|30.00 | 2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00
+        2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Unit"|30.00 | 2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00
+        2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|42.00 | 2|"description"|"name"|2.0|1.0|"Quantitative Biology Center"|"Sample"|30.00
+
+
+    }
 }
