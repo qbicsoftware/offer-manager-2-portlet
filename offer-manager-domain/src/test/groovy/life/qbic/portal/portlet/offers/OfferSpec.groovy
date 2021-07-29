68f3db8eb3c107ae2f7f0af76704f795b1597bdf
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-07-29 15:36:00 +0200

    Remove outdated test

--- offer-manager-domain/src/test/groovy/life/qbic/portal/portlet/offers/OfferSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/portal/portlet/offers/OfferSpec.groovy
@@ -275,44 +275,6 @@ class OfferSpec extends Specification {
         !res
     }
 
-    //TODO do we still need/want this??
-    def "An Offer will provide methods to distinct between ProductItems associated with overhead costs and calculate their net sum"() {
-        given:
-        ProductItem primaryAnalysis = new ProductItem(2, new PrimaryAnalysis("Basic RNAsq", "Just an" +
-                " example primary analysis", 1.0, 1.0, ProductUnit.PER_SAMPLE, 1, Facility.QBIC), 2.0, 0.0)
-        ProductItem secondaryAnalysis = new ProductItem(1, new SecondaryAnalysis("Basic RNAsq", "Just an" +
-                " example secondary analysis", 2.0, 2.0, ProductUnit.PER_SAMPLE, 1, Facility.QBIC), 2.0, 0.0)
-        ProductItem sequencing = new ProductItem(3, new Sequencing("Basic Sequencing", "Just an" +
-                "example sequencing", 3.0, 3.0, ProductUnit.PER_SAMPLE, 1, Facility.QBIC), 2.0, 0.0)
-        ProductItem projectManagement = new ProductItem(1, new ProjectManagement("Basic Management",
-                "Just an example", 10.0, 10.0, ProductUnit.PER_DATASET, 1, Facility.QBIC), 2.0, 0.0)
-        ProductItem dataStorage = new ProductItem(2, new DataStorage("Data Storage",
-                "Just an example", 20.0, 20.0, ProductUnit.PER_DATASET, 1, Facility.QBIC), 2.0, 0.0)
-
-        List<ProductItem> items = [primaryAnalysis, projectManagement, sequencing, dataStorage, secondaryAnalysis]
-        Offer offer = new Offer.Builder(customerWithAllAffiliations, projectManager, "Awesome Project", "An " +
-                "awesome project", items, externalAffiliation).build()
-
-
-        when:
-        List<ProductItem> itemsWithoutOverhead = offer.getNoOverheadItems()
-        List<ProductItem> itemsWithOverhead = offer.getOverheadItems()
-        double itemsWithoutOverheadNetPrice = offer.getNoOverheadItemsNet()
-        double itemsWithOverheadNetPrice = offer.getOverheadItemsNet()
-
-        then:
-
-        double expectedItemsWithOverheadNetPrice = 2 * 1.0 + 1 * 2.0 + 3 * 3.0
-        double expectedItemsWithoutOverheadNetPrice = 1 * 10 + 2 * 20
-        List<ProductItem> expectedItemsWithOverhead = [primaryAnalysis, sequencing, secondaryAnalysis]
-        List<ProductItem> expectedItemsWithoutOverhead = [projectManagement, dataStorage]
-        itemsWithOverhead == expectedItemsWithOverhead
-        itemsWithoutOverhead == expectedItemsWithoutOverhead
-        expectedItemsWithOverheadNetPrice == itemsWithOverheadNetPrice
-        expectedItemsWithoutOverheadNetPrice == itemsWithoutOverheadNetPrice
-
-    }
-
     /**
      * @since 1.1.0
      */
