3965734512210766ee981b01526cf6935bb6c589
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2021-08-02 16:37:12 +0200

    write tests

---
+++ offer-manager-domain/src/test/groovy/life/qbic/business/ProductFactory.groovy
@@ -0,0 +1,164 @@
+package life.qbic.business
+
+import life.qbic.datamodel.dtos.business.facilities.Facility
+import life.qbic.datamodel.dtos.business.services.DataStorage
+import life.qbic.datamodel.dtos.business.services.MetabolomicAnalysis
+import life.qbic.datamodel.dtos.business.services.PrimaryAnalysis
+import life.qbic.datamodel.dtos.business.services.Product
+import life.qbic.datamodel.dtos.business.services.ProductUnit
+import life.qbic.datamodel.dtos.business.services.ProjectManagement
+import life.qbic.datamodel.dtos.business.services.ProteomicAnalysis
+import life.qbic.datamodel.dtos.business.services.SecondaryAnalysis
+import life.qbic.datamodel.dtos.business.services.Sequencing
+
+/**
+ * <b>Creates Products</b>
+ *
+ * <p>Creates products for testing purposes</p>
+ *
+ * @since 1.1.0
+ */
+class ProductFactory {
+    static <T extends Product> T createProduct(Class clazz, String description, String name, double internalPrice, double externalPrice, Facility serviceProvider) {
+        int runningNumber = 1
+        ProductUnit productUnit = ProductUnit.PER_SAMPLE
+        switch (clazz) {
+            case DataStorage:
+                return new DataStorage(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case MetabolomicAnalysis:
+                return new MetabolomicAnalysis(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case PrimaryAnalysis:
+                return new PrimaryAnalysis(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case ProjectManagement:
+                return new ProjectManagement(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case ProteomicAnalysis:
+                return new ProteomicAnalysis(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case SecondaryAnalysis:
+                return new SecondaryAnalysis(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case Sequencing:
+                return new Sequencing(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+        }
+    }
+
+    static <T extends Product> T createProduct(Class clazz, double internalPrice, double externalPrice) {
+        int runningNumber = 1
+        String name = "Test product"
+        String description = "Product for testing purposes"
+        Facility serviceProvider = Facility.QBIC
+        ProductUnit productUnit = ProductUnit.PER_SAMPLE
+        switch (clazz) {
+            case DataStorage:
+                return new DataStorage(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case MetabolomicAnalysis:
+                return new MetabolomicAnalysis(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case PrimaryAnalysis:
+                return new PrimaryAnalysis(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case ProjectManagement:
+                return new ProjectManagement(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case ProteomicAnalysis:
+                return new ProteomicAnalysis(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case SecondaryAnalysis:
+                return new SecondaryAnalysis(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+            case Sequencing:
+                return new Sequencing(name,
+                        description,
+                        internalPrice,
+                        externalPrice,
+                        productUnit,
+                        runningNumber,
+                        serviceProvider) as T
+                break
+        }
+    }
+}
--- offer-manager-domain/src/test/groovy/life/qbic/business/offers/CreateOfferContentSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/business/offers/CreateOfferContentSpec.groovy
@@ -1,5 +1,6 @@
 package life.qbic.business.offers
 
+import life.qbic.business.ProductFactory
 import life.qbic.business.offers.content.CreateOfferContent
 import life.qbic.business.offers.content.CreateOfferContentOutput
 import life.qbic.business.offers.fetch.FetchOffer
@@ -13,9 +14,12 @@ import life.qbic.datamodel.dtos.business.OfferId
 import life.qbic.datamodel.dtos.business.ProductItem
 import life.qbic.datamodel.dtos.business.ProjectManager
 import life.qbic.datamodel.dtos.business.facilities.Facility
+import life.qbic.datamodel.dtos.business.services.DataStorage
 import life.qbic.datamodel.dtos.business.services.PrimaryAnalysis
+import life.qbic.datamodel.dtos.business.services.Product
 import life.qbic.datamodel.dtos.business.services.ProductUnit
 import life.qbic.datamodel.dtos.business.services.ProjectManagement
+import life.qbic.datamodel.dtos.business.services.Sequencing
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -65,16 +69,27 @@ class CreateOfferContentSpec extends Specification{
         offerId = new OfferId("Conserved", "abcd", "2")
     }
 
-    def "Overhead costs for product groups are correctly set"(){
+    def "Overhead costs of #overheadPercent for product groups are correctly set for #categoryString affiliation"(){
         given: "the create offer content use case"
+        Product product1 = ProductFactory.createProduct(Sequencing, internalPrice1, externalPrice1)
+        Product product2 = ProductFactory.createProduct(Sequencing, internalPrice2, externalPrice2)
+        Product product3 = ProductFactory.createProduct(Sequencing, internalPrice3, externalPrice3)
+        List<ProductItem> productItems = [
+                new ProductItem(1, product1),
+                new ProductItem(2, product2),
+                new ProductItem(3, product3)
+        ]
+        Affiliation affiliation = new Affiliation.Builder("Test orga", "Test street", "1234", "Tübingen").category(affiliationCategory).build()
+
+        and: "a data stubbed data source"
         FetchOfferDataSource ds = Stub(FetchOfferDataSource.class)
 
         CreateOfferContentOutput output = Stub()
         CreateOfferContent createOfferContent = new CreateOfferContent(output,ds)
 
         and: "a mocked offer, that is returned upon the FetchOffer use case"
-        ds.getOffer(_ as OfferId) >> Optional.of(new life.qbic.datamodel.dtos.business.Offer.Builder(customer, projectManager, projectTitle, projectDescription, selectedAffiliation)
-                .modificationDate(date).expirationDate(date).items(items).identifier(offerId)
+        ds.getOffer(_ as OfferId) >> Optional.of(new life.qbic.datamodel.dtos.business.Offer.Builder(customer, projectManager, projectTitle, projectDescription, affiliation)
+                .modificationDate(date).expirationDate(date).items(productItems).identifier(offerId)
                 .build())
 
         when: "the offer content creation is triggered"
@@ -83,23 +98,59 @@ class CreateOfferContentSpec extends Specification{
         then: "the overheads for the product groups are set correctly"
         output.createdOfferContent(_ as OfferContent) >> { arguments ->
             final OfferContent offerContent = arguments.get(0)
-            assert offerContent.overheadsDataAnalysis  == (2 * 1 * 0.4) as double
-            assert offerContent.overheadsDataGeneration == 0
-            assert offerContent.overheadsProjectManagementAndDataStorage == (1 * 10 * 0.4) as double
+            assert offerContent.overheadsDataAnalysis  == offerContent.getNetDataAnalysis() * overheadRatio
+            assert offerContent.overheadsDataGeneration == offerContent.getNetDataGeneration() * overheadRatio
+            assert offerContent.overheadsProjectManagementAndDataStorage == offerContent.getNetPMandDS() * overheadRatio
         }
+
+        where:
+        affiliationCategory | overheadRatio
+        AffiliationCategory.INTERNAL | 0.0
+        AffiliationCategory.EXTERNAL_ACADEMIC | 0.2
+        AffiliationCategory.EXTERNAL | 0.4
+
+        and:
+        overheadPercent = "${overheadRatio * 100}%"
+        categoryString = affiliationCategory.toString()
+
+        and:
+        internalPrice1 = 4.0
+        internalPrice2 = 5.9
+        internalPrice3 = 3.3
+
+        and:
+        externalPrice1 = internalPrice1 + 0.5
+        externalPrice2 = internalPrice2 + 0.5
+        externalPrice3 = internalPrice3 + 0.5
     }
 
     def "Net costs for product groups are correctly set"(){
         given: "the create offer content use case"
+        ProductItem productItem1 = new ProductItem(2, ProductFactory.createProduct(PrimaryAnalysis, internalPrice, externalPrice))
+        ProductItem productItem2 = new ProductItem(3, ProductFactory.createProduct(Sequencing, internalPrice, externalPrice))
+        ProductItem productItem3 = new ProductItem(4, ProductFactory.createProduct(DataStorage, internalPrice, externalPrice))
+        List<ProductItem> productItems = [
+                productItem1,
+                productItem2,
+                productItem3
+        ]
+        Affiliation affiliation = new Affiliation.Builder("Test orga", "Test street", "1234", "Tübingen").category(affiliationCategory).build()
+
+        and: "a data stubbed data source"
         FetchOfferDataSource ds = Stub(FetchOfferDataSource.class)
 
         CreateOfferContentOutput output = Stub()
         CreateOfferContent createOfferContent = new CreateOfferContent(output,ds)
+        and:
+        QuantityDiscount quantityDiscount = new QuantityDiscount()
+
+        and: "an offer to be returned"
+        life.qbic.datamodel.dtos.business.Offer offerDto = new life.qbic.datamodel.dtos.business.Offer.Builder(customer, projectManager, projectTitle, projectDescription, affiliation)
+                .modificationDate(date).expirationDate(date).items(productItems).identifier(offerId)
+                .build()
 
         and: "a mocked offer, that is returned upon the FetchOffer use case"
-        ds.getOffer(_ as OfferId) >> Optional.of(new life.qbic.datamodel.dtos.business.Offer.Builder(customer, projectManager, projectTitle, projectDescription, selectedAffiliation)
-                .modificationDate(date).expirationDate(date).items(items).identifier(offerId)
-                .build())
+        ds.getOffer(_ as OfferId) >> Optional.of(offerDto)
 
         when: "the offer content creation is triggered"
         createOfferContent.createOfferContent(offerId)
@@ -107,10 +158,20 @@ class CreateOfferContentSpec extends Specification{
         then: "the overheads for the product groups are set correctly"
         output.createdOfferContent(_ as OfferContent) >> { arguments ->
             final OfferContent offerContent = arguments.get(0)
-            assert offerContent.netDataAnalysis  == (2 * 1) as double
-            assert offerContent.netDataGeneration == 0
-            assert offerContent.netPMandDS == (1 * 10) as double
+            assert offerContent.netDataAnalysis  == 2 * unitPrice - quantityDiscount.apply(2, 2*unitPrice)
+            assert offerContent.netDataGeneration == 3 * unitPrice - quantityDiscount.apply(2, 3*unitPrice)
+            assert offerContent.netPMandDS == 4 * unitPrice - quantityDiscount.apply(2, 4*unitPrice)
         }
+
+        where:
+        internalPrice = 4.0
+        externalPrice = 4.5
+
+        and:
+        affiliationCategory << [AffiliationCategory.INTERNAL, AffiliationCategory.EXTERNAL_ACADEMIC, AffiliationCategory.EXTERNAL]
+
+        and:
+        unitPrice = affiliationCategory == AffiliationCategory.INTERNAL ? internalPrice : externalPrice
     }
 
     def "Unknown OfferId triggers the failedNotification() method"(){
