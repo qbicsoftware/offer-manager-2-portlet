014ae2ba490822addb6361f2849ab3230298e310
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-08-06 10:46:24 +0200

    fix builder error messages

--- offer-manager-domain/src/main/groovy/life/qbic/business/offers/OfferItem.groovy
+++ offer-manager-domain/src/main/groovy/life/qbic/business/offers/OfferItem.groovy
@@ -32,14 +32,14 @@ class OfferItem {
         double itemTotal
 
         Builder(double quantity, String productDescription, String productName, double unitPrice, double quantityDiscount, String serviceProvider, String unit, double itemTotal) {
-            this.quantity =  Objects.requireNonNull(quantity, "Customer must not be null")
-            this.productDescription =  Objects.requireNonNull(productDescription, "Customer must not be null")
-            this.productName =  Objects.requireNonNull(productName, "Customer must not be null")
-            this.unitPrice =  Objects.requireNonNull(unitPrice, "Customer must not be null")
-            this.quantityDiscount =  Objects.requireNonNull(quantityDiscount, "Customer must not be null")
-            this.serviceProvider =  Objects.requireNonNull(serviceProvider, "Customer must not be null")
-            this.unit =  Objects.requireNonNull(unit, "Customer must not be null")
-            this.itemTotal =  Objects.requireNonNull(itemTotal, "Customer must not be null")
+            this.quantity =  Objects.requireNonNull(quantity, "Quantity must not be null")
+            this.productDescription =  Objects.requireNonNull(productDescription, "Product description must not be null")
+            this.productName =  Objects.requireNonNull(productName, "Product name must not be null")
+            this.unitPrice =  Objects.requireNonNull(unitPrice, "Unit price must not be null")
+            this.quantityDiscount =  Objects.requireNonNull(quantityDiscount, "Quantity discount must not be null")
+            this.serviceProvider =  Objects.requireNonNull(serviceProvider, "Service provider must not be null")
+            this.unit =  Objects.requireNonNull(unit, "Unit must not be null")
+            this.itemTotal =  Objects.requireNonNull(itemTotal, "Item total must not be null")
 
         }
 
