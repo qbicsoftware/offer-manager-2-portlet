425021535336d44211989761b5769c645ba3a9d0
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2021-10-04 16:16:45 +0200

    Update business rules to account for external services

---
+++ offer-manager-domain/src/main/groovy/life/qbic/business/offers/TaxOffice.groovy
@@ -0,0 +1,68 @@
+package life.qbic.business.offers
+
+import life.qbic.datamodel.dtos.business.Affiliation
+import life.qbic.datamodel.dtos.business.AffiliationCategory
+import life.qbic.datamodel.dtos.business.services.ExternalServiceProduct
+import life.qbic.datamodel.dtos.business.services.Product
+
+
+/**
+ * <b>Tax office that is responsible for offer tax calculation.</b>
+ *
+ * <p>Calculates the taxes that need to be considered for a given price value, affiliation and product type</p>
+ *
+ * <p>The current implementation takes a VAT ratio of 19%, default for Germany. Taxes only apply for customers within Germany
+ * and if they are not part of the University or UKT. Taxes are not forwarded internally.</p>
+ *
+ * <p>The only exception are products (services) external services, for which taxes need to
+ * be applied regardless of the affiliation status.</p>
+ *
+ * @since 1.2.0
+ */
+class TaxOffice {
+
+    private static final BigDecimal VAT_RATIO = BigDecimal.valueOf(0.19)
+
+    private final Affiliation affiliation
+
+    /**
+     * Creates a tax office that is configured by the affiliation.
+     *
+     * @param affiliation the affiliation the tax office instance needs to consider for calculations
+     */
+    TaxOffice(Affiliation affiliation) {
+        this.affiliation = affiliation
+    }
+
+    /**
+     * Calculates the absolute tax value that applies to a given service cost value and product type.
+     *
+     * @param serviceCosts the service cost value the office has to consider
+     * @param product the service product
+     * @return the absolute resulting tax value
+     */
+    BigDecimal applyTaxes(BigDecimal serviceCosts, Class clazz) {
+        switch (affiliation) {
+            case ({it.country != "Germany"}):
+                return BigDecimal.valueOf(0)
+                break
+            case ({it.category.equals(AffiliationCategory.INTERNAL)}):
+                return applyTaxesForInternals(serviceCosts, clazz)
+                break
+            default:
+                return applyTaxesForExternals(serviceCosts)
+                break
+        }
+    }
+
+    private static BigDecimal applyTaxesForInternals(BigDecimal serviceCosts, Class clazz) {
+        if (clazz.equals(ExternalServiceProduct)) {
+            return serviceCosts * VAT_RATIO
+        }
+        return BigDecimal.valueOf(0)
+    }
+
+    private static BigDecimal applyTaxesForExternals(BigDecimal serviceCosts) {
+        return serviceCosts * VAT_RATIO
+    }
+}
---
+++ offer-manager-domain/src/test/groovy/life/qbic/business/offers/TaxOfficeSpec.groovy
@@ -0,0 +1,111 @@
+package life.qbic.business.offers
+
+import life.qbic.business.products.Product
+import life.qbic.datamodel.dtos.business.Affiliation
+import life.qbic.datamodel.dtos.business.AffiliationCategory
+import life.qbic.datamodel.dtos.business.ProductItem
+import life.qbic.datamodel.dtos.business.facilities.Facility
+import life.qbic.datamodel.dtos.business.services.DataStorage
+import life.qbic.datamodel.dtos.business.services.ExternalServiceProduct
+import life.qbic.datamodel.dtos.business.services.MetabolomicAnalysis
+import life.qbic.datamodel.dtos.business.services.PrimaryAnalysis
+import life.qbic.datamodel.dtos.business.services.ProductUnit
+import life.qbic.datamodel.dtos.business.services.ProjectManagement
+import life.qbic.datamodel.dtos.business.services.ProteomicAnalysis
+import life.qbic.datamodel.dtos.business.services.SecondaryAnalysis
+import life.qbic.datamodel.dtos.business.services.Sequencing
+import spock.lang.Shared
+import spock.lang.Specification
+
+/**
+ * <class short description - 1 Line!>
+ *
+ * <More detailed description - When to use, what it solves, etc.>
+ *
+ * @since <version tag>
+ */
+class TaxOfficeSpec extends Specification{
+    @Shared
+    Affiliation internalAffiliation
+    @Shared
+    Affiliation externalAcademicAffiliation
+    @Shared
+    Affiliation externalAffiliation
+
+    def setup() {
+        internalAffiliation = new Affiliation.Builder("Uni Tübingen", "Auf der " +
+                "Morgenstelle 10", "72076", "Tuebingen").category(AffiliationCategory.INTERNAL).build()
+        externalAcademicAffiliation = new Affiliation.Builder("Uni Frankfurt",
+                "Irgendwo im Nirgendwo 20", "12345", "Frankfurt").category(AffiliationCategory
+                .EXTERNAL_ACADEMIC)
+                .build()
+        externalAffiliation = new Affiliation.Builder("Company Frankfurt",
+                "Irgendwo " +
+                        "im Nirgendwo 20", "12345", "Frankfurt").category(AffiliationCategory.EXTERNAL)
+                .build()
+    }
+
+    def "An internal customer has to pay no taxes on internal services"() {
+        given:
+        BigDecimal serviceCosts = BigDecimal.valueOf(20)
+        TaxOffice taxOffice = new TaxOffice(internalAffiliation)
+
+        when:
+        BigDecimal taxesAmount = taxOffice.applyTaxes(serviceCosts, clazz)
+
+        then:
+        taxesAmount == BigDecimal.valueOf(0)
+
+        where:
+        clazz << [Sequencing,
+                  PrimaryAnalysis,
+                  SecondaryAnalysis,
+                  DataStorage,
+                  ProjectManagement,
+                  ProteomicAnalysis,
+                  MetabolomicAnalysis]
+    }
+
+    def "An external customer has to pay taxes on every service"() {
+        given:
+        BigDecimal serviceCosts = BigDecimal.valueOf(20)
+        TaxOffice taxOffice = new TaxOffice(externalAcademicAffiliation)
+
+        when:
+        BigDecimal taxesAmount = taxOffice.applyTaxes(serviceCosts, clazz)
+
+        then:
+        taxesAmount == BigDecimal.valueOf(0.19 * 20)
+
+        where:
+        clazz << [Sequencing,
+                  PrimaryAnalysis,
+                  SecondaryAnalysis,
+                  DataStorage,
+                  ProjectManagement,
+                  ProteomicAnalysis,
+                  MetabolomicAnalysis]
+    }
+
+    def "An external academic customer has to pay taxes on every service"() {
+        given:
+        BigDecimal serviceCosts = BigDecimal.valueOf(20)
+        TaxOffice taxOffice = new TaxOffice(externalAffiliation)
+
+        when:
+        BigDecimal taxesAmount = taxOffice.applyTaxes(serviceCosts, clazz)
+
+        then:
+        taxesAmount == BigDecimal.valueOf(0.19 * 20)
+
+        where:
+        clazz << [Sequencing,
+                  PrimaryAnalysis,
+                  SecondaryAnalysis,
+                  DataStorage,
+                  ProjectManagement,
+                  ProteomicAnalysis,
+                  MetabolomicAnalysis]
+    }
+
+}
