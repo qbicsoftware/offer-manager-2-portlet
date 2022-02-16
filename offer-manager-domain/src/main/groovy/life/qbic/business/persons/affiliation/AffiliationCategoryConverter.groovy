c7f7aed1114ccbb7931df1e663f443a04649837f
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2022-02-16 15:58:11 +0100

    Add category converter class

---
+++ offer-manager-domain/src/main/groovy/life/qbic/business/persons/affiliation/AffiliationCategoryConverter.groovy
@@ -0,0 +1,13 @@
+package life.qbic.business.persons.affiliation
+
+import javax.persistence.AttributeConverter
+
+/**
+ * <b><class short description - 1 Line!></b>
+ *
+ * <p><More detailed description - When to use, what it solves, etc.></p>
+ *
+ * @since <version tag>
+ */
+class AffiliationCategoryConverter implements AttributeConverter<Category, String>{
+}
