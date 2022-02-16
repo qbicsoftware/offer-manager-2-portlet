8e07d84426fef1a67971ace3e69b17b26fdeda4e
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-02-16 14:40:37 +0100

    Add static factory method to AffiliationCategory

--- offer-manager-domain/src/main/groovy/life/qbic/business/persons/affiliation/AffiliationCategory.java
+++ offer-manager-domain/src/main/groovy/life/qbic/business/persons/affiliation/AffiliationCategory.java
@@ -1,22 +1,37 @@
 package life.qbic.business.persons.affiliation;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 /**
- * We define three major business affiliation categories here, which
- * means that we decide between
+ * We define three major business affiliation categories here, which means that we decide between
+ *
  * <ul>
- *  <li><b>internal</b>: An affiliation we consider as within the University of Tübingen or University
- *  Hospital of Tübingen</li>
- *  <li><b>external academic</b>: An outside affiliation but an academic institution (public research
- *  institutions)</li>
- *  <li><b>external</b>: An outside affiliation but not academic (i.e. private sector, companies, etc)</li>
+ *   <li><b>internal</b>: An affiliation we consider as within the University of Tübingen or
+ *       University Hospital of Tübingen
+ *   <li><b>external academic</b>: An outside affiliation but an academic institution (public
+ *       research institutions)
+ *   <li><b>external</b>: An outside affiliation but not academic (i.e. private sector, companies,
+ *       etc)
  * </ul>
+ *
  * @since 1.3.0
  */
-enum AffiliationCategory {
-
+public enum AffiliationCategory {
   INTERNAL("internal"),
   EXTERNAL_ACADEMIC("external academic"),
   EXTERNAL("external");
+
+  public static AffiliationCategory from(String label) {
+    Optional<AffiliationCategory> affiliationCategory =
+        Arrays.stream(AffiliationCategory.values())
+            .filter(it -> it.getLabel().equals(label))
+            .findAny();
+    return affiliationCategory.orElseThrow(
+        () ->
+            new IllegalArgumentException(
+                String.format("No AffiliationCategory with label %s", label)));
+  }
 
   /**
    * Holds the value of the enum
