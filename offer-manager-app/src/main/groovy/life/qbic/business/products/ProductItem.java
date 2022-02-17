2c161301ee0ee94f11a318e6b7a9217edad768f8
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-02-17 10:39:36 +0100

    Add ProductItem

---
+++ offer-manager-app/src/main/groovy/life/qbic/business/products/ProductItem.java
@@ -0,0 +1,66 @@
+package life.qbic.business.products;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "productitem", uniqueConstraints = {
+    @UniqueConstraint(name = "id", columnNames = {"id"})
+})
+public class ProductItem {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Integer id;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "productId", nullable = false)
+  private Product product;
+
+  @Column(name = "quantity", nullable = false)
+  private Double quantity;
+
+  @Column(name = "offerId", nullable = false)
+  private Integer offerId;
+
+  public Integer getOfferId() {
+    return offerId;
+  }
+
+  public void setOfferId(Integer offerId) {
+    this.offerId = offerId;
+  }
+
+  public Double getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Double quantity) {
+    this.quantity = quantity;
+  }
+
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+}
