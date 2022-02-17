ed7317ad61b8e71fbd4f005aa3735ff32f351c87
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2022-02-17 14:08:00 +0100

    PRovide Offer calc

---
+++ offer-manager-domain/src/main/groovy/life/qbic/business/offers/OfferCalculus.groovy
@@ -0,0 +1,13 @@
+package life.qbic.business.offers
+
+/**
+ * <b><class short description - 1 Line!></b>
+ *
+ * <p><More detailed description - When to use, what it solves, etc.></p>
+ *
+ * @since <version tag>
+ */
+class OfferCalculus {
+
+
+}
