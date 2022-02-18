82ff70a9f77c2a2e38dc58288fbc10d013ed9566
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2022-02-18 15:41:35 +0100

    Finish offer item creation

--- offer-manager-domain/src/main/groovy/life/qbic/business/offers/OfferCalculus.java
+++ offer-manager-domain/src/main/groovy/life/qbic/business/offers/OfferCalculus.java
@@ -3,9 +3,10 @@ package life.qbic.business.offers;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import life.qbic.business.persons.affiliation.Affiliation;
 import life.qbic.business.persons.affiliation.AffiliationCategory;
 import life.qbic.business.products.ProductItem;
 
@@ -38,6 +39,11 @@ class OfferCalculus {
   private BigDecimal totalCost;
   private BigDecimal totalDiscountAmount;
 
+  private static final List<String> DATA_GENERATION = Arrays.asList("Sequencing", "Proteomics", "Metabolomics");
+  private static final List<String> DATA_ANALYSIS = Arrays.asList("Primary Bioinformatics", "Secondary Bioinformatics");
+  private static final List<String> PROJECT_AND_DATA_MANAGEMENT = Arrays.asList("Project Management", "Data Storage");
+  private static final List<String> EXTERNAL_SERVICES = Collections.singletonList("External Service");
+
    /**
    * Determines the overhead ratio for this offer based on the customer affiliation.
    * @param offer the offer for which the overhead ratio should be determined
@@ -66,15 +72,51 @@ class OfferCalculus {
    * @param offer
    * @return
    */
-  OfferV2 groupItems(OfferV2 offer) {
+
+  List<OfferItem> createOfferItems(OfferV2 offer) {
+    AffiliationCategory affiliationCategory = offer.getSelectedCustomerAffiliation().getCategory();
+    List<OfferItem> offerItems;
+    // Discounting for internal customer is different, so we check the affiliation first
+    if (affiliationCategory == AffiliationCategory.INTERNAL) {
+      offerItems = convertProductItemsForInternals(offer.getItems());
+    } else {
+      offerItems = convertProductItemsForExternals(offer.getItems());
+    }
+    return offerItems;
+  }
+
+  OfferV2 groupItems(OfferV2 offer, List<OfferItem> offerItems) {
+    if (offerItems.isEmpty()) {
+      offerItems = createOfferItems(offer);
+    }
     OfferV2 offerCopy = OfferV2.copy(offer);
-    // Create OfferItems from ProductItems first
-    List<OfferItem> offerItems = convertProductItems(offerCopy.getItems(), offerCopy.getSelectedCustomerAffiliation());
+    List<OfferItem> ungroupedItems = new ArrayList<>();
     // And then sort them into the correct categories
     for (OfferItem item : offerItems) {
-      if (item.)
+      String category = item.getCategory();
+      if (DATA_GENERATION.contains(category)) {
+        offerCopy.addDataGenerationItem(item);
+        continue;
+      }
+      if (DATA_ANALYSIS.contains(category)) {
+        offerCopy.addDataAnalysisItem(item);
+        continue;
+      }
+      if (PROJECT_AND_DATA_MANAGEMENT.contains(category)) {
+        offerCopy.addDataManagementItem(item);
+        continue;
+      }
+      if (EXTERNAL_SERVICES.contains(category)) {
+        offerCopy.addExternalServiceItem(item);
+      } else {
+        ungroupedItems.add(item);
+      }
     }
-    return null;
+    if (ungroupedItems.size() > 0) {
+      throw new OfferCalculusException("Some offer items could not be sorted, because their category"
+          + " was unknown. " + ungroupedItems);
+    }
+    return offerCopy;
   }
 
   public static List<OfferItem> convertProductItemsForExternals(List<ProductItem> productItems) {
@@ -122,5 +164,19 @@ class OfferCalculus {
     return affiliationCategory == AffiliationCategory.INTERNAL ?
         BigDecimal.valueOf(item.getProduct().getInternalUnitPrice())
         : BigDecimal.valueOf(item.getProduct().getExternalUnitPrice());
+  }
+
+  static class OfferCalculusException extends RuntimeException {
+    OfferCalculusException() {
+      super();
+    }
+
+    OfferCalculusException(String message) {
+      super(message);
+    }
+
+    OfferCalculusException(String message, Throwable t) {
+      super(message, t);
+    }
   }
 }
