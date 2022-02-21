21edc67ba992b08b66b6066139fed35b71de3822
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-02-21 16:10:00 +0100

    Add Test

--- offer-manager-domain/src/test/groovy/life/qbic/business/offers/OfferCalculusSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/business/offers/OfferCalculusSpec.groovy
@@ -2,6 +2,8 @@ package life.qbic.business.offers
 
 import life.qbic.business.persons.affiliation.Affiliation
 import life.qbic.business.persons.affiliation.AffiliationCategory
+import life.qbic.business.products.Product
+import life.qbic.business.products.ProductItem
 import spock.lang.Specification
 
 /**
@@ -12,12 +14,14 @@ import spock.lang.Specification
  * @since <version tag>
  */
 class OfferCalculusSpec extends Specification {
+  OfferCalculus offerCalculus = new OfferCalculus()
+
   def "overhead ratio is #expectedRatio for customer affiliation with category #category"() {
     given: "an offer with the respective customer affiliation"
     OfferV2 offer = new OfferV2()
     offer.setSelectedCustomerAffiliation(new Affiliation("organization", "addressAddition", "street", "postalCode", "city", "country", category))
     when: "the calculus determines the overhead ratio for that offer"
-    OfferV2 filledOffer = new OfferCalculus().overheadRatio(offer)
+    OfferV2 filledOffer = offerCalculus.overheadRatio(offer)
     then: "the overhead ratio is #expectedRatio"
     filledOffer.getOverheadRatio() == expectedRatio.doubleValue()
     where:
@@ -27,4 +31,57 @@ class OfferCalculusSpec extends Specification {
     AffiliationCategory.EXTERNAL          | 0.4
   }
 
+  def "when the offers product items are converted to offer items, then every product item is represented in an offer item"() {
+    given:
+    def item1 = new ProductItem(createDataAnalysisProduct(), 4.5, 0.0, 0.0)
+    def productItems = [item1]
+    OfferV2 offer = new OfferV2()
+    offer.setItems(productItems)
+    when: "the offers product items are converted to offer items"
+    def offerItems = offerCalculus.createOfferItems(offer)
+    then: "every product item is represented in an offer item"
+    offerItems.stream()
+            .filter(offerItem ->
+                    productItems.stream()
+                            .anyMatch(it -> equals(it, offerItem))
+            )
+            .findAny()
+            .isPresent()
+
+  }
+
+  boolean equals(ProductItem productItem, OfferItem offerItem) {
+    // check for product properties
+    if (!(productItem.getProduct().getCategory().equals(offerItem.getCategory()))) {
+      return false
+    }
+    if (!productItem.getProduct().getDescription().equals(offerItem.getProductDescription())) {
+      return false
+    }
+    if (!productItem.getProduct().getProductName().equals(offerItem.getProductName())) {
+      return false
+    }
+    if (!productItem.getProduct().serviceProvider.equals(offerItem.getServiceProvider())) {
+      return false
+    }
+    if (!productItem.getProduct().getUnit().equals(offerItem.getUnit())) {
+      return false
+    }
+
+    return productItem.getQuantity().equals(offerItem.getQuantity())
+  }
+
+  Product createDataAnalysisProduct() {
+    def daProduct = new Product()
+    daProduct.setCategory("Primary Bioinformatics")
+    daProduct.setActive(true)
+    daProduct.setDescription("This is a primary bioinformatics product")
+    daProduct.setExternalUnitPrice(0.25)
+    daProduct.setInternalUnitPrice(0.5)
+    daProduct.setProductId("PB_1")
+    daProduct.setProductName("Awesome Bioinformatics")
+    daProduct.setUnit("Brain Cell")
+    daProduct.setServiceProvider("Bioinformatiker Model Z")
+    return daProduct
+  }
 }
