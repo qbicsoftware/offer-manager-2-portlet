4ed72bfab0372d1df06f4c1f5add1ae5810db22d
Sven Fillinger <sven.fillinger@qbic.uni-tuebingen.de>
2022-03-03 16:25:12 +0100

    Implement converter methods

--- offer-manager-domain/src/main/groovy/life/qbic/business/RefactorConverter.java
+++ offer-manager-domain/src/main/groovy/life/qbic/business/RefactorConverter.java
@@ -1,17 +1,19 @@
 package life.qbic.business;
 
+import java.util.function.Function;
 import life.qbic.business.offers.Offer;
 import life.qbic.business.products.Product;
 import life.qbic.business.products.dtos.ProductDraft;
-import life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier;
+import life.qbic.datamodel.dtos.business.ProductId;
 import life.qbic.datamodel.dtos.projectmanagement.ProjectSpace;
 
 /**
  * <b>ATTENTION: Only for refactor purposes. Meant to be removed.</b>
+ *
  * <ol>
- *   <li>use cases dürfen nur die neuen klassen verwenden (keine data-model-lib)</li>
- *   <li>apdapter (controller und presenter) benutzen diesen converter</li>
- *   <li>sobald alles umgestellt ist kann man den offer manager laufen lassen</li>
+ *   <li>use cases dürfen nur die neuen klassen verwenden (keine data-model-lib)
+ *   <li>apdapter (controller und presenter) benutzen diesen converter
+ *   <li>sobald alles umgestellt ist kann man den offer manager laufen lassen
  * </ol>
  */
 public class RefactorConverter {
@@ -41,24 +43,125 @@ public class RefactorConverter {
     return null;
   }
 
-
-  String toProductId(life.qbic.datamodel.dtos.business.ProductId productId) {
-    return null; // todo implement conversion to String representation
+  /**
+   * Converts a {@link life.qbic.datamodel.dtos.business.ProductId} object to a String
+   * representation of type <code>#type_#id</code>, for example <code>ME_22</code>.
+   *
+   * @param productId the product id object
+   * @return the String representation of the product id
+   */
+  public String toProductId(life.qbic.datamodel.dtos.business.ProductId productId) {
+    // we do not want to be dependent on the objects toString() implementation
+    // therefore we define the String format explicitly to have control
+    return String.format("%s_%d", productId.getType().toUpperCase(), productId.getUniqueId());
   }
 
-  life.qbic.datamodel.dtos.business.ProductId toProductIdDto(String productId) {
-    return null; // todo implement parser for DTO conversion
+  /**
+   * Converts a String representation of a product id into the DTO form
+   *
+   * @param productId the product id String
+   * @return the converted product id DTO
+   */
+  public life.qbic.datamodel.dtos.business.ProductId toProductIdDto(String productId)
+      throws IllegalArgumentException {
+    return new ProductIdParser().apply(productId);
   }
 
-  // Todo: create ProjectSpace business entity
-  ProjectSpace toProjectSpace(ProjectSpace projectSpace) {
-    return null;
+  /**
+   * Converts a {@link life.qbic.datamodel.dtos.projectmanagement.ProjectSpace} object to its
+   * uppercase String representation.
+   *
+   * @param projectSpace the project space object
+   * @return the uppercase String representation
+   */
+  public String toProjectSpace(
+      life.qbic.datamodel.dtos.projectmanagement.ProjectSpace projectSpace) {
+    return projectSpace.getName().toUpperCase();
   }
 
-  // Todo: Create ProjectIdentifier business entity
-  ProjectIdentifier toProjectIdentifier(ProjectIdentifier projectIdentifier) {
-    return null; // Todo: implement
+  /**
+   * Converts a project space String representation into a {@link
+   * life.qbic.datamodel.dtos.projectmanagement.ProjectSpace} object.
+   *
+   * @param projectSpace the project space String representation
+   * @return its dto representation
+   * @throws IllegalArgumentException if the String cannot be converted, for example due to format
+   *     rule violations
+   */
+  public life.qbic.datamodel.dtos.projectmanagement.ProjectSpace toProjectSpaceDTO(
+      String projectSpace) throws IllegalArgumentException {
+    ProjectSpace projectSpaceDTO;
+    try {
+      projectSpaceDTO = new ProjectSpace(projectSpace);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Could not convert project space", e);
+    }
+    return projectSpaceDTO;
   }
 
+  /**
+   * Converts a {@link life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier} object
+   * to its String representation.
+   *
+   * The resulting format is upper case: <code>/#projectspace/#projectcode</code>
+   * @param projectIdentifier the project identifier to convert
+   * @return its String representation
+   */
+  public String toProjectIdentifier(
+      life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier projectIdentifier) {
+    // we do not want to  depend on the class String format implementation, so we are explicit
+    return String.format("/%s/%s", projectIdentifier.getProjectSpace().getName().toUpperCase(),
+        projectIdentifier.getProjectCode().getCode().toUpperCase());
+  }
 
+  public life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier toProjectIdentifierDTO(
+      String projectIdentifier) throws IllegalArgumentException {
+    // todo implement
+    return new ProjectIdentifierParser().apply(projectIdentifier);
+  }
+
+  /**
+   * Small helper class to provide project identifier parsing functionality. Since we control the String
+   * representation of the project identifier, we know that we can expect it
+   * to be in the format <code>/#projectspace/#projectcode</code>.
+   */
+  protected static class ProjectIdentifierParser
+      implements Function<String, life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier> {
+
+    @Override
+    public life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier apply(String s) {
+      return null; // todo implement
+    }
+  }
+
+  /**
+   * Small helper class to provide product id parsing functionality. Since we control the String
+   * format representation of the product id, we know that we expect it to be of format <code>
+   * #type_#id</code>, which is a String for the type and a long value for the id.
+   */
+  protected static class ProductIdParser
+      implements Function<String, life.qbic.datamodel.dtos.business.ProductId> {
+
+    @Override
+    public life.qbic.datamodel.dtos.business.ProductId apply(String s) {
+      return parseString(s);
+    }
+
+    private static life.qbic.datamodel.dtos.business.ProductId parseString(String s) {
+      String[] splitString = s.split("_");
+      if (splitString.length != 2) {
+        throw new IllegalArgumentException(String.format("Unknown product id format: %s", s));
+      }
+      String productCategory = splitString[0].toUpperCase();
+      long productId;
+
+      try {
+        productId = Long.parseLong(splitString[1]);
+      } catch (NumberFormatException ignored) {
+        throw new IllegalArgumentException(String.format("Cannot determine product id: %s", s));
+      }
+
+      return new ProductId.Builder(productCategory, productId).build();
+    }
+  }
 }
