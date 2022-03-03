8d011f8fe1c3bffd25cdbba6433d9d8731fffa80
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-03-03 15:41:01 +0100

    Add refactor converter method signatures

--- offer-manager-domain/src/main/groovy/life/qbic/business/RefactorConverter.java
+++ offer-manager-domain/src/main/groovy/life/qbic/business/RefactorConverter.java
@@ -1,5 +1,11 @@
 package life.qbic.business;
 
+import life.qbic.business.offers.Offer;
+import life.qbic.business.products.Product;
+import life.qbic.business.products.dtos.ProductDraft;
+import life.qbic.datamodel.dtos.projectmanagement.ProjectIdentifier;
+import life.qbic.datamodel.dtos.projectmanagement.ProjectSpace;
+
 /**
  * <b>ATTENTION: Only for refactor purposes. Meant to be removed.</b>
  * <ol>
@@ -14,6 +20,45 @@ public class RefactorConverter {
   // vat calculation logic in the offer calculus can be redirected to the Tax office /Tax office can
   // be removed
   // ProductDraft and ProductEntity can be replaced
+
+  Offer toOffer(life.qbic.datamodel.dtos.business.OfferId offerId) {
+    return null;
+  }
+
+  Offer toOffer(life.qbic.datamodel.dtos.business.Offer offerDto) {
+    return null;
+  }
+
+  life.qbic.datamodel.dtos.business.Offer toOfferDto(Offer offer) {
+    return null;
+  }
+
+  Product toProduct(ProductDraft productDraft) {
+    return null;
+  }
+
+  life.qbic.datamodel.dtos.business.services.Product toProductDto(Product product) {
+    return null;
+  }
+
+
+  String toProductId(life.qbic.datamodel.dtos.business.ProductId productId) {
+    return null; // todo implement conversion to String representation
+  }
+
+  life.qbic.datamodel.dtos.business.ProductId toProductIdDto(String productId) {
+    return null; // todo implement parser for DTO conversion
+  }
+
+  // Todo: create ProjectSpace business entity
+  ProjectSpace toProjectSpace(ProjectSpace projectSpace) {
+    return null;
+  }
+
+  // Todo: Create ProjectIdentifier business entity
+  ProjectIdentifier toProjectIdentifier(ProjectIdentifier projectIdentifier) {
+    return null; // Todo: implement
+  }
 
 
 }
