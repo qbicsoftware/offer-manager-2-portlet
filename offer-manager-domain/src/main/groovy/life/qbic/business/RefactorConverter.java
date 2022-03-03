2eae06d55ecabb5021d6c6d924ad351e76e9f3ed
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-03-03 10:34:41 +0100

    empty converter

---
+++ offer-manager-domain/src/main/groovy/life/qbic/business/RefactorConverter.java
@@ -0,0 +1,14 @@
+package life.qbic.business;
+
+/**
+ * <b>ATTENTION: Only for refactor purposes. Meant to be removed.</b>
+ * <ol>
+ *   <li>use cases dürfen nur die neuen klassen verwenden (keine data-model-lib)</li>
+ *   <li>apdapter (controller und presenter) benutzen diesen converter</li>
+ *   <li>sobald alles umgestellt ist kann man den offer manager laufen lassen</li>
+ * </ol>
+ */
+public class RefactorConverter {
+
+
+}
