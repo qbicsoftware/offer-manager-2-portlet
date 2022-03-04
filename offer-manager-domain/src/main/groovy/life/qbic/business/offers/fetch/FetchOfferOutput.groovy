ef65f6ac523e02ba9c5054bf365e0afb9bad327c
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-03-04 17:07:33 +0100

    Rollback view classes to working version

--- offer-manager-domain/src/main/groovy/life/qbic/business/offers/fetch/FetchOfferOutput.groovy
+++ offer-manager-domain/src/main/groovy/life/qbic/business/offers/fetch/FetchOfferOutput.groovy
@@ -1,7 +1,7 @@
 package life.qbic.business.offers.fetch
 
 import life.qbic.business.UseCaseFailure
-import life.qbic.business.offers.Offer
+import life.qbic.business.offers.OfferV2
 
 /**
  * Output interface for the {@link life.qbic.business.offers.fetch.FetchOffer} use case
@@ -17,5 +17,5 @@ interface FetchOfferOutput extends UseCaseFailure {
    * @param fetchedOffer {@link life.qbic.datamodel.dtos.business.Offer}
    * @since 1.0.0
    */
-  void fetchedOffer(Offer fetchedOffer)
+  void fetchedOffer(OfferV2 fetchedOffer)
 }
