82b9df20673ebc8ccbba09ca28a342ff1777583d
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-03-07 13:10:08 +0100

    Fix CreatePersonSpec

--- offer-manager-domain/src/test/groovy/life/qbic/portal/portlet/persons/create/CreatePersonSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/portal/portlet/persons/create/CreatePersonSpec.groovy
@@ -1,11 +1,10 @@
 package life.qbic.portal.portlet.persons.create
 
+import life.qbic.business.persons.Person
+import life.qbic.business.persons.PersonExistsException
 import life.qbic.business.persons.create.CreatePerson
 import life.qbic.business.persons.create.CreatePersonDataSource
 import life.qbic.business.persons.create.CreatePersonOutput
-import life.qbic.datamodel.dtos.business.AcademicTitle
-import life.qbic.datamodel.dtos.business.Customer
-import life.qbic.datamodel.dtos.general.Person
 import spock.lang.Specification
 
 /**
@@ -17,43 +16,61 @@ import spock.lang.Specification
  * @author: Tobias Koch
  */
 class CreatePersonSpec extends Specification {
-    CreatePersonOutput output
-    CreatePersonDataSource dataSource
+  CreatePersonOutput output
+  CreatePersonDataSource dataSource
 
 
-    def setup() {
-        output = Mock()
-        dataSource = Mock()
-    }
+  def setup() {
+    output = Mock()
+    dataSource = Mock()
+  }
 
-    def "given full information add the customer using a mocked data source"(){
-        given: "A new create customer use case instance"
-        CreatePerson useCase = new CreatePerson(output, dataSource)
+  def "given full information add the customer using a mocked data source"() {
+    given: "A new create customer use case instance"
+    CreatePerson useCase = new CreatePerson(output, dataSource)
 
-        when: "The use case method is called"
-        useCase.createPerson(customer)
+    when: "The use case method is called"
+    useCase.createPerson(customer)
 
-        then: "The customer is added using the data source"
-        1 * dataSource.addPerson(customer)
+    then: "The customer is added using the data source"
+    1 * dataSource.addPerson(customer)
 
-        where:
-        customer = new Customer.Builder("Test", "user", "test").title(AcademicTitle.NONE).build()
-    }
+    where:
+    customer = new Person("my.user@id.de", "FirstName", "LastName", "Title", "email", [])
+  }
 
-    def "datasource throwing an exception leads to fail notification on output"() {
-        given: "a data source that throws an exception"
-        dataSource.addPerson(_ as Customer) >> { throw new Exception("Something went wrong.") }
-        CreatePerson useCase = new CreatePerson(output, dataSource)
 
-        when: "the use case is executed"
-        useCase.createPerson(customer)
+  def "datasource throwing an exception leads to fail notification on output"() {
+    given: "a data source that throws an exception"
+    dataSource.addPerson(_ as Person) >> { throw new RuntimeException("Some unexpected runtime exception.") }
+    CreatePerson useCase = new CreatePerson(output, dataSource)
 
-        then: "the output receives a failure notification"
-        1 * output.failNotification(_ as String)
-        0 * output.customerCreated(_ as Person)
+    when: "the use case is executed"
+    useCase.createPerson(customer)
 
-        where:
-        customer = new Customer.Builder("Test", "user", "test").title(AcademicTitle.NONE).build()
-    }
+    then: "the output receives a failure notification"
+    1 * output.failNotification(_ as String)
+    0 * output.personCreated(_ as Person)
+
+    where:
+    customer = new Person("my.user@id.de", "FirstName", "LastName", "Title", "email", [])
+  }
+
+  def "when the person already exists, then a failure notification is send"() {
+    when: "the person already exists"
+    dataSource.addPerson(_ as Person) >> { throw new PersonExistsException("The person already exists.") }
+    output.failNotification(_ as String) >> { println it}
+    CreatePerson useCase = new CreatePerson(output, dataSource)
+
+    useCase.createPerson(customer)
+    then: "a failure notification is send"
+    0 * output.personCreated(_)
+    1 * output.failNotification(_)
+
+    where:
+    customer = new Person("my.user@id.de", "FirstName", "LastName", "Title", "email", [])
+
+  }
+
 
 }
