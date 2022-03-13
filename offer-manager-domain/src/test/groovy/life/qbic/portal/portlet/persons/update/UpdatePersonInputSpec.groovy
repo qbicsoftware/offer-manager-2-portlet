d14ad3ae0b8958217f04cca8214441a0cf9c7a58
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-03-13 13:31:01 +0100

    Update UpdatePersonInputSpec.groovy

--- offer-manager-domain/src/test/groovy/life/qbic/portal/portlet/persons/update/UpdatePersonInputSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/portal/portlet/persons/update/UpdatePersonInputSpec.groovy
@@ -1,12 +1,11 @@
 package life.qbic.portal.portlet.persons.update
 
+import life.qbic.business.persons.Person
+import life.qbic.business.persons.affiliation.Affiliation
+import life.qbic.business.persons.affiliation.AffiliationCategory
 import life.qbic.business.persons.create.CreatePersonDataSource
-import life.qbic.business.persons.update.*
-import life.qbic.datamodel.dtos.business.AcademicTitle
-import life.qbic.datamodel.dtos.business.Customer
-import life.qbic.datamodel.dtos.business.Affiliation
-import life.qbic.datamodel.dtos.general.CommonPerson
-import life.qbic.datamodel.dtos.general.Person
+import life.qbic.business.persons.update.UpdatePerson
+import life.qbic.business.persons.update.UpdatePersonOutput
 import spock.lang.Specification
 
 /**
@@ -17,68 +16,100 @@ import spock.lang.Specification
  * @since: 1.0.0
  */
 class UpdatePersonInputSpec extends Specification {
-    UpdatePersonOutput output
-    CreatePersonDataSource dataSource
+  UpdatePersonOutput output
+  CreatePersonDataSource dataSource
+  private static Person newPerson = new Person(null, "first", "last", "", "first.last@n.ame", [])
+  private static Person existingPerson =  new Person("my-old-user-id", "test", "Mustermann", "Prof. Dr.", "abc@def.ge", [])
+  private static Person existingPersonPlusAffiliations = new Person(existingPerson.getUserId(), existingPerson.getFirstName(), existingPerson.getLastName(), existingPerson.getTitle(), existingPerson.getEmail(), [new Affiliation("orga", "addressAddition", "street", "1234", "city", "country", AffiliationCategory.EXTERNAL)])
 
 
   def setup() {
-      output = Mock()
-      dataSource = Mock()
+    output = Mock()
+    dataSource = Mock()
   }
 
-  def "given customer changes, update the customer using a mocked data source"(){
-      given: "A new update customer use case instance"
-      UpdatePerson useCase = new UpdatePerson(output, dataSource)
-      dataSource.getPerson(42) >> new CommonPerson.Builder("Test", "user", "oldmail").title(AcademicTitle.NONE).build()
+  def "given customer changes, update the customer using a mocked data source"() {
+    given: "A new update customer use case instance"
+    UpdatePerson useCase = new UpdatePerson(output, dataSource)
+    dataSource.updatePerson(oldPerson, updatedPerson) >> UpdatePersonInputSpec::updatedPersonWithPreservedUserId
 
-      when: "The use case method is called"
-      useCase.updatePerson(customerId, customer)
+    when: "The use case method is called"
+    useCase.updatePerson(oldPerson, updatedPerson)
 
-      then: "The customer is updated using the data source"
-      1 * dataSource.updatePerson(customerId, customer)
-      0 * dataSource.updatePersonAffiliations(_ as String, _ as List<Affiliation>)
+    then: "The customer is updated using the data source"
+    with(dataSource) {
+      1 * updatePerson(oldPerson, updatedPerson)
+      0 * updatePersonAffiliations(_)
+    }
+    1 * output.personUpdated({ Person it -> hasSamePersonInformation(it, expectedPerson) })
+    expectedPerson.isActive
 
-      where:
-      customer = new CommonPerson.Builder("Test", "user", "newmail").title(AcademicTitle.NONE).build()
-      customerId = 42
+    where:
+    updatedPerson = newPerson
+    oldPerson = existingPerson
+    expectedPerson = updatedPersonWithPreservedUserId(oldPerson, updatedPerson)
   }
-  
-  def "given no customer changes, update the affiliations using a mocked data source"(){
-      given: "A new update customer use case instance"
-      UpdatePerson useCase = new UpdatePerson(output, dataSource)
-      dataSource.getPerson(42) >> new CommonPerson.Builder("Test", "user", "oldmail").title(AcademicTitle.NONE).affiliation(affiliation1).build()
 
-      when: "The use case method is called"
-      useCase.updatePerson(customerId, customer)
+  def "given no person specific data changes, update the affiliations using a mocked data source"() {
+    given: "A new update customer use case instance"
+    UpdatePerson useCase = new UpdatePerson(output, dataSource)
+    dataSource.updatePerson(oldPerson, updatedPerson as Person) >> UpdatePersonInputSpec::updatedPersonWithPreservedUserId
 
-      then: "The customer affiliations are updated using the data source"
-      0 * dataSource.updatePerson(_ as String, _ as Customer)
-      1 * dataSource.updatePersonAffiliations(customerId, twoAffiliations)
+    when: "The use case method is called"
+    useCase.updatePerson(oldPerson, updatedPerson as Person)
 
-      where:
-      affiliation1 = new Affiliation.Builder(
-        "org", "street", "zip", "city").build()
-      twoAffiliations = new ArrayList<Affiliation>(Arrays.asList(new Affiliation.Builder(
-        "other org", "other street", "zip", "city").build(), affiliation1))
-      customer = new CommonPerson.Builder("Test", "user", "oldmail").title(AcademicTitle.NONE).affiliations(twoAffiliations).build()
-      customerId = 42
+    then: "Only customer affiliations are updated using the data source"
+    0 * dataSource.updatePerson(oldPerson, updatedPerson)
+    1 * dataSource.updatePersonAffiliations(updatedPerson)
+    1 * output.personUpdated(expectedPerson)
+    expectedPerson.isActive
+    println "expectedPerson.affiliations = $expectedPerson.affiliations"
+
+    where:
+    oldPerson = existingPerson
+    updatedPerson = existingPersonPlusAffiliations
+    expectedPerson = existingPersonPlusAffiliations
   }
-  
+
   def "datasource throwing an exception leads to fail notification on output"() {
       given: "a data source that throws an exception"
-      dataSource.getPerson(_ as Integer) >> new CommonPerson.Builder("Test", "user", "oldmail").title(AcademicTitle.NONE).build()
-      dataSource.updatePerson(_ as Integer, _ as Person) >> { throw new Exception("Something went wrong.") }
+      dataSource._ >> { throw new RuntimeException("Something went wrong.") }
       UpdatePerson useCase = new UpdatePerson(output, dataSource)
 
       when: "the use case is executed"
-      useCase.updatePerson(customerId, customer)
+      useCase.updatePerson(oldPerson, updatedPerson)
 
       then: "the output receives a failure notification"
       1 * output.failNotification(_ as String)
       0 * output.personUpdated(_ as Person)
 
       where:
-      customer = new CommonPerson.Builder("Test", "user", "newmail").title(AcademicTitle.NONE).build()
-      customerId = 420
+      oldPerson = existingPerson
+      updatedPerson << [
+              newPerson,
+              existingPersonPlusAffiliations
+      ]
+  }
+
+  private static boolean hasSamePersonInformation(Person p1, Person p2) {
+    if (p1.is(p2)) return true
+    if (!(p2 instanceof Person)) return false
+
+    Person person = (Person) p2
+
+    if (p1.isActive != person.isActive) return false
+    if (p1.affiliations != person.affiliations) return false
+    if (p1.email != person.email) return false
+    if (p1.firstName != person.firstName) return false
+    if (p1.id != person.id) return false
+    if (p1.lastName != person.lastName) return false
+    if (p1.title != person.title) return false
+    if (p1.userId != person.userId) return false
+    return true
+  }
+  private static Person updatedPersonWithPreservedUserId(Person oldPerson, Person updatedPerson) {
+    Person person = new Person(oldPerson.getUserId(), updatedPerson.getFirstName(), updatedPerson.getLastName(), updatedPerson.getTitle(), updatedPerson.getEmail(), updatedPerson.getAffiliations())
+    person.setIsActive(true)
+    return person
   }
 }
