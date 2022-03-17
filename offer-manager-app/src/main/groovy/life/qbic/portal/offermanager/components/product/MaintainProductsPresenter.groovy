27486fea12949074623f089986dcfd265c9e1793
Sven Fillinger <sven.fillinger@mailbox.org>
2022-03-17 08:23:43 +0100

    Implement methods for MaintainProductPresenter

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/product/MaintainProductsPresenter.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/product/MaintainProductsPresenter.groovy
@@ -1,5 +1,6 @@
 package life.qbic.portal.offermanager.components.product
 
+import life.qbic.business.RefactorConverter
 import life.qbic.business.products.archive.ArchiveProductOutput
 import life.qbic.business.products.create.CreateProductOutput
 import life.qbic.datamodel.dtos.business.ProductId
@@ -14,38 +15,16 @@ import life.qbic.portal.offermanager.components.AppViewModel
  * @since 1.0.0
  *
  */
-class MaintainProductsPresenter implements CreateProductOutput, ArchiveProductOutput{
+class MaintainProductsPresenter implements CreateProductOutput, ArchiveProductOutput {
 
     private final MaintainProductsViewModel productsViewModel
     private final AppViewModel mainViewModel
 
-    MaintainProductsPresenter(MaintainProductsViewModel productsViewModel, AppViewModel mainViewModel){
+    private static final RefactorConverter refactorConverter = new RefactorConverter()
+
+    MaintainProductsPresenter(MaintainProductsViewModel productsViewModel, AppViewModel mainViewModel) {
         this.productsViewModel = productsViewModel
         this.mainViewModel = mainViewModel
-    }
-
-    @Override
-    void archived(Product product) {
-        mainViewModel.successNotifications << "Successfully archived product $product.productId - $product.productName."
-        productsViewModel.productsResourcesService.removeFromResource(product)
-    }
-
-    @Override
-    void created(Product product) {
-        mainViewModel.successNotifications << "Successfully added new product $product.productId - $product.productName."
-        productsViewModel.productsResourcesService.addToResource(product)
-        productsViewModel.productCreatedSuccessfully = true
-    }
-
-    @Override
-    void foundDuplicates(List<Product> duplicateProducts) {
-        Product duplicateProduct = duplicateProducts.first()
-        List<ProductId> duplicateProductIds = []
-        duplicateProducts.forEach { Product product ->
-            duplicateProductIds << product.getProductId()
-        }
-        String productIds = duplicateProductIds.join(", ")
-        mainViewModel.failureNotifications << "Found multiple products for ${duplicateProduct.productName} : ${productIds}"
     }
 
     @Override
@@ -53,4 +32,27 @@ class MaintainProductsPresenter implements CreateProductOutput, ArchiveProductOu
         mainViewModel.failureNotifications << notification
     }
 
+    @Override
+    void archived(life.qbic.business.products.Product product) {
+        mainViewModel.successNotifications << "Successfully archived product $product.productId - $product.productName."
+        productsViewModel.productsResourcesService.removeFromResource(refactorConverter.toProductDto(product))
+    }
+
+    @Override
+    void created(life.qbic.business.products.Product product) {
+        mainViewModel.successNotifications << "Successfully added new product $product.productId - $product.productName."
+        productsViewModel.productsResourcesService.addToResource(refactorConverter.toProductDto(product))
+        productsViewModel.productCreatedSuccessfully = true
+    }
+
+    @Override
+    void foundDuplicates(List<life.qbic.business.products.Product> products) {
+        life.qbic.business.products.Product duplicateProduct = products.first()
+        List<ProductId> duplicateProductIds = []
+        products.forEach { Product product ->
+            duplicateProductIds << product.getProductId()
+        }
+        String productIds = duplicateProductIds.join(", ")
+        mainViewModel.failureNotifications << "Found multiple products for ${duplicateProduct.productName} : ${productIds}"
+    }
 }
