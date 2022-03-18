f808be95d8b8d9d6964eb7244ccf1d4cd33a8f2e
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-03-18 09:36:10 +0100

    add discount tests from offer calculus

--- offer-manager-domain/src/test/groovy/life/qbic/business/products/ProductItemSpec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/business/products/ProductItemSpec.groovy
@@ -48,10 +48,127 @@ class ProductItemSpec extends Specification {
 
     }
 
+    def "Given a product is a data analysis service, apply no quantity discount to the unit price" () {
+
+    }
+
     def "Given a product is a data storage service and the customer is internal, apply 100% discount to the unit price"() {
 
     }
 
+    def "Given a product is a data storage service and the customer is external or external academic, apply 0% discount to the unit price"() {
+
+    }
+
+    // DATA STORAGE DISCOUNT
+    /*def "expect the data storage discount is 0 for external and external academic affiliations"() {
+        expect: "the data storage discount is 0 for external and external academic"
+        calculateDataStorageDiscount(AffiliationCategory.EXTERNAL_ACADEMIC,
+                "Data Storage",
+                unitPrice as BigDecimal) == BigDecimal.ZERO
+
+        calculateDataStorageDiscount(AffiliationCategory.EXTERNAL,
+                "Data Storage",
+                unitPrice as BigDecimal) == BigDecimal.ZERO
+
+        where: "the product category, unit price and quantity are as follows"
+        unitPrice | quantity
+        10        | 10
+        20        | 30.33
+        0         | 0
+        0.33      | 100
+    }
+
+    def "expect the data storage discount is 100% for internal affiliation and data storage product"() {
+        expect: "the data storage discount is 0 for external and external academic"
+        calculateDataStorageDiscount(AffiliationCategory.INTERNAL,
+                "Data Storage",
+                unitPrice as BigDecimal) == expectedPrice
+
+        where: "the product category, unit price and quantity are as follows"
+        unitPrice | quantity | expectedPrice
+        10        | 10       | 10.0
+        20        | 30.33    | 20.0
+        0         | 0        | 0.00
+        0.33      | 100      | 0.33
+    }
+
+    def "expect the data storage discount is 0 for #productCategory products"() {
+        expect:
+        calculateDataStorageDiscount(AffiliationCategory.INTERNAL,
+                productCategory,
+                unitPrice as BigDecimal) == BigDecimal.ZERO
+
+        where: "the unit price and quantity are as follows"
+        unitPrice = 30.33
+        quantity = 94
+        and: "the product category is one of"
+        productCategory << ["Sequencing",
+                            "Proteomics",
+                            "Metabolomics",
+                            "Primary Bioinformatics",
+                            "Secondary Bioinformatics",
+                            "External Service"]
+    }*/
+    // QUANTITY DISCOUNT
+    /*def "expect the quantity discount is 0 for #productCategory products"() {
+        expect:
+        calculateQuantityDiscount(unitPrice, quantity, productCategory) == BigDecimal.ZERO
+        where:
+        unitPrice = 33.34
+        quantity = 44.0
+
+        and:
+        productCategory << ["Sequencing",
+                            "Proteomics",
+                            "Metabolomics",
+                            "Data Storage",
+                            "External Service"]
+    }
+
+    def "expect the quantity discount is #expectedResult for primary and secondary bioinformatics products"() {
+        expect:
+        calculateQuantityDiscount(unitPrice, quantity, "Primary Bioinformatics") == expectedResult
+        calculateQuantityDiscount(unitPrice, quantity, "Secondary Bioinformatics") == expectedResult
+        where:
+        unitPrice | quantity | expectedResult
+        100.33    | 33.0     | 57.19 //57.1881
+        33.34     | 44.0     | 20.34 // 20.3374
+        10.0      | 10000.0  | 8.20 // 8.20
+    }
+
+    def "expect the quantity discount to be rounded up 2 digits after the comma"() {
+        expect: "the quantity discount is computed"
+        calculateQuantityDiscount(unitPrice, quantity, "Primary Bioinformatics")
+                == expectedResult
+        where:
+        unitPrice | quantity | expectedResult
+        100.33    | 33.0     | 57.19 //57.1881
+        33.34     | 44.0     | 20.34 // 20.3374
+        10.0      | 10000.0  | 8.20 // 8.20
+    }*/
+    // ITEM DISCOUNT
+    /*def "expect the item discount is the maximum of quantity and data storage discount for data storage products"() {
+        expect:
+        calculateUnitDiscount(productCategory as String,
+                affiliationCategory as AffiliationCategory,
+                unitPrice as BigDecimal,
+                quantity as BigDecimal) == expectedResult
+        where:
+        [productCategory, affiliationCategory, unitPrice, quantity] << GroovyCollections.combinations(
+                ["Data Storage", "Primary Bioinformatics", "Project Management"],
+                AffiliationCategory.values(),
+                [0, 33.33, 44, 100],
+                [0.1, 0.5, 1, 7, 33, 100, 1000])
+        and:
+        dataStorageDiscount = calculateDataStorageDiscount(affiliationCategory as AffiliationCategory,
+                productCategory as String,
+                unitPrice as BigDecimal)
+        quantityDiscount = calculateQuantityDiscount(unitPrice as BigDecimal,
+                quantity as BigDecimal,
+                productCategory as String)
+        expectedResult = dataStorageDiscount.max(quantityDiscount)
+    }*/
 
     void createTestAffiliations() {
         internalAffiliation = new Affiliation("Test orga", "", "Funnystreet", "72070", "Tuebingen", "Germany", AffiliationCategory.INTERNAL)
@@ -64,5 +181,3 @@ class ProductItemSpec extends Specification {
         testItem = new ProductItem(product, 10)
     }
 }
-
-
