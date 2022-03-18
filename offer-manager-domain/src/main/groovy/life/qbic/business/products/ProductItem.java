0c034f145d2f56b93bda168bae643d400118db11
Sven Fillinger <sven.fillinger@mailbox.org>
2022-03-18 15:14:37 +0100

    Cleanup

--- offer-manager-domain/src/main/groovy/life/qbic/business/products/ProductItem.java
+++ offer-manager-domain/src/main/groovy/life/qbic/business/products/ProductItem.java
@@ -4,7 +4,6 @@ import groovy.transform.CompileStatic;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -27,10 +26,8 @@ import life.qbic.business.persons.affiliation.AffiliationCategory;
 @CompileStatic
 public class ProductItem {
 
-  private static final List<String> DATA_GENERATION = Arrays.asList("Sequencing", "Proteomics", "Metabolomics");
-  private static final List<String> DATA_ANALYSIS = Arrays.asList("Primary Bioinformatics", "Secondary Bioinformatics");
-  private static final List<String> PROJECT_AND_DATA_MANAGEMENT = Arrays.asList("Project Management", "Data Storage");
-  private static final List<String> EXTERNAL_SERVICES = Collections.singletonList("External Service");
+  private static final List<String> DATA_ANALYSIS = Arrays.asList("Primary Bioinformatics",
+      "Secondary Bioinformatics");
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -62,7 +59,8 @@ public class ProductItem {
     this.quantity = quantity;
   }
 
-  protected ProductItem() {}
+  protected ProductItem() {
+  }
 
   public void setOffer(OfferV2 offer) {
     this.offer = offer;
@@ -103,7 +101,8 @@ public class ProductItem {
     unitListPrice = determineUnitPrice(affiliationCategory, product);
 
     BigDecimal quantityDiscount =
-        calculateQuantityDiscount(unitListPrice, BigDecimal.valueOf(quantity), product.getCategory());
+        calculateQuantityDiscount(unitListPrice, BigDecimal.valueOf(quantity),
+            product.getCategory());
     BigDecimal storageDiscount =
         calculateDataStorageDiscount(affiliationCategory, product.getCategory(), unitListPrice);
     unitDiscountPrice = quantityDiscount.max(storageDiscount);
@@ -113,14 +112,16 @@ public class ProductItem {
 
   /**
    * <p>The relevant unit price for a product in an offer depends on the customer affiliation.
-   * The current business policy is to use provide to base prices for internal and external customers,
-   * two cover the cases where VAT of i.e. purchased consumables needs to be reflected in the
-   * internal base price.</p>
+   * The current business policy is to use provide to base prices for internal and external
+   * customers, two cover the cases where VAT of i.e. purchased consumables needs to be reflected in
+   * the internal base price.</p>
+   *
    * @param affiliationCategory internal, external or external academic
-   * @param product the product of interest
+   * @param product             the product of interest
    * @return the numeric value of the determined base price
    */
-  protected static BigDecimal determineUnitPrice(AffiliationCategory affiliationCategory, Product product) {
+  protected static BigDecimal determineUnitPrice(AffiliationCategory affiliationCategory,
+      Product product) {
     if (affiliationCategory.getLabel().equalsIgnoreCase("internal")) {
       return BigDecimal.valueOf(product.getInternalUnitPrice()).setScale(2,
           RoundingMode.HALF_UP);
@@ -135,8 +136,8 @@ public class ProductItem {
    * data storage products.
    *
    * @param affiliationCategory the considered affiliation category for discount determination
-   * @param productCategory the product category to be discounted
-   * @param unitPrice the unit price to be discounted
+   * @param productCategory     the product category to be discounted
+   * @param unitPrice           the unit price to be discounted
    * @return the amount of discount per unit for the provided unit price
    */
   protected static BigDecimal calculateDataStorageDiscount(
@@ -152,7 +153,7 @@ public class ProductItem {
    * Calculates the discounted unit price based on the quantity.
    *
    * @param unitPrice the pre-selected uni price
-   * @param quantity the quantity amount of the product
+   * @param quantity  the quantity amount of the product
    * @return the discounted final price
    */
   protected static BigDecimal calculateQuantityDiscount(
@@ -167,9 +168,10 @@ public class ProductItem {
 
   /**
    * <p>In some cases it is of interest to the client to quickly check
-   * if a given product item has a discount.</p>
-   * This method provides the information.
-   * @return <code>true</code>, if there is discount applied to the product item, else <code>false</code>
+   * if a given product item has a discount.</p> This method provides the information.
+   *
+   * @return <code>true</code>, if there is discount applied to the product item, else
+   * <code>false</code>
    */
   public boolean hasDiscount() {
     return discountApplied;
@@ -177,9 +179,11 @@ public class ProductItem {
 
   /**
    * <p>If there is some discount applied to the product item,
-   * clients might want to know the unit price discount value.</p>
-   * This method returns the discount unit price value
-   * @return between 0 for no discount or the value of {@link ProductItem#getUnitListPrice()} for 100% discount
+   * clients might want to know the unit price discount value.</p> This method returns the discount
+   * unit price value
+   *
+   * @return between 0 for no discount or the value of {@link ProductItem#getUnitListPrice()} for
+   * 100% discount
    */
   public BigDecimal getUnitPriceDiscount() {
     return this.unitDiscountPrice;
@@ -187,6 +191,7 @@ public class ProductItem {
 
   /**
    * <p>Provides the relevant product unit price for the current offer.</p>
+   *
    * @return the numerical value of the unit list price
    */
   public BigDecimal getUnitListPrice() {
@@ -196,6 +201,7 @@ public class ProductItem {
   /**
    * <p>Provides the total item net price of the product item, which
    * includes discounts.</p>
+   *
    * @return the numerical value of the item's quantity multiplied with the discounted unit price.
    */
   public BigDecimal getItemTotalPrice() {
@@ -207,7 +213,9 @@ public class ProductItem {
   /**
    * <p>Provides the relative discount in percentage.</p>
    *
-   * <p>In case no discount has been applied, this will return 0, in case of full discount it will return 100.</p>
+   * <p>In case no discount has been applied, this will return 0, in case of full discount it will
+   * return 100.</p>
+   *
    * @return 0, 100, or in between, for no discount, full discount or partial discount applied
    */
   public BigDecimal getRelativeDiscount() {
