f4c41560e0b7e822f73e2cd7fb2168d0ac857403
Steffengreiner <Steffen.Greiner@gmx.de>
2022-03-23 16:42:00 +0100

    Introduce tests for pricing calculation in OfferV2
    Co-authored-by: KochTobi <tobias.koch@qbic.uni-tuebingen.de>

--- offer-manager-domain/src/test/groovy/life/qbic/business/offers/OfferV2Spec.groovy
+++ offer-manager-domain/src/test/groovy/life/qbic/business/offers/OfferV2Spec.groovy
@@ -53,22 +53,12 @@ class OfferV2Spec extends Specification {
 
   def "the offer's sale price is the sum of the group sale prices"() {
     given:
-    OfferV2 offer = new OfferV2()
-    offer.setSelectedCustomerAffiliation(new Affiliation("", "", "", "", "", "Germany", AffiliationCategory.INTERNAL))
-    and: "some one product item of each category once"
-    def dataGeneration = createDataGenerationItem(offer)
-    def dataAnalysis = createDataAnalysisItem(offer)
-    def dataManagement = createDataManagementItem(offer)
-    def externalServices = createExternalServiceItem(offer)
-    when:
-    offer.addItems([dataGeneration, dataAnalysis, dataManagement, externalServices])
-    then: "the groups overheads are the sum of the items overheads"
+    OfferV2 offer = createOfferWithItemsOfEachCategory(AffiliationCategory.INTERNAL, "Germany")
+    expect: "the groups overheads are the sum of the items overheads"
     offer.getSalePrice() == offer.getDataGenerationSalePrice()
             .add(offer.getDataAnalysisSalePrice())
             .add(offer.getDataManagementSalePrice())
             .add(offer.getExternalServiceSalePrice())
-
-
   }
 
   def "overhead ratio is #overheadPercentage% for customer affiliation with category #category"() {
@@ -123,6 +113,15 @@ class OfferV2Spec extends Specification {
             .add(offer.externalServiceOverhead).doubleValue()
   }
 
+  def "expect the price before vat being the sale price + the overhead"() {
+    given: "an offer for external in Germany"
+    OfferV2 offer = createOfferWithItemsOfEachCategory(affiliationCategory as AffiliationCategory, "Germany")
+    expect:
+    offer.priceBeforeTax == offer.salePrice + BigDecimal.valueOf(offer.overhead)
+    where:
+    affiliationCategory << AffiliationCategory.values()
+  }
+
 
   def "The VAT for Germany applied is 19%"() {
     given:
@@ -162,12 +161,22 @@ class OfferV2Spec extends Specification {
   }
 
   def "expect the tax amount to be the cost before tax multiplied with the VAT rate."() {
-    given:
-    String country = "Germany"
-    OfferV2 offer = new OfferV2()
-    offer.setSelectedCustomerAffiliation(new Affiliation("", "", "", "", "", country, AffiliationCategory.EXTERNAL))
+    given: "an offer for external in Germany"
+    OfferV2 offer = createOfferWithItemsOfEachCategory(AffiliationCategory.EXTERNAL, "Germany")
     expect:
-    offer.getTaxAmount() == offer.getSalePrice() * offer.getVatRatio()
+    offer.getOverhead() > 0
+    offer.getSalePrice() > 0
+    offer.getVatRatio() > 0
+    offer.getTaxAmount() == ((offer.getSalePrice() + BigDecimal.valueOf(offer.getOverhead())) * offer.getVatRatio()).setScale(2, RoundingMode.HALF_UP)
+  }
+
+  def "expect the price after tax to be the price before tax + the tax amount"() {
+    given: "an offer for external in Germany"
+    OfferV2 offer = createOfferWithItemsOfEachCategory(affiliationCategory as AffiliationCategory, "Germany")
+    expect:
+    offer.getPriceAfterTax() == offer.priceBeforeTax + offer.getTaxAmount()
+    where:
+    affiliationCategory << AffiliationCategory.values()
   }
 
   def "expect the discount amount to be the sum of the items discount amounts"() {
@@ -189,7 +198,6 @@ class OfferV2Spec extends Specification {
                     .add(externalServices.getDiscountAmount())
     ).setScale(2, RoundingMode.HALF_UP)
   }
-
 
   private static createDataGenerationItem(OfferV2 offer) {
     def category = "Sequencing"
@@ -215,200 +223,14 @@ class OfferV2Spec extends Specification {
     return new ProductItem(offer, product, 10.0)
   }
 
-
-
-  def "the offer discount is the sum of the item discounts"() {
-//    def dataGenerationProductItem = createDataGenerationProductItem(20.9, 5.6)
-//    def dataAnalysisProductItem = createDataAnalysisProductItem(20.9, 5.6)
-//    def dataManagementProductItem = createDataManagementProductItem(20.9, 5.6)
-//    def externalServicesProductItem = createExternalServicesProductItem(20.9, 5.6)
-//
-//    def dataGenerationOfferItem = affiliationCategory == AffiliationCategory.INTERNAL ? offerItemFromInternal(dataGenerationProductItem) : offerItemFromExternal(dataGenerationProductItem)
-//    def dataAnalysisOfferItem = affiliationCategory == AffiliationCategory.INTERNAL ? offerItemFromInternal(dataAnalysisProductItem) : offerItemFromExternal(dataAnalysisProductItem)
-//    def dataManagementOfferItem = affiliationCategory == AffiliationCategory.INTERNAL ? offerItemFromInternal(dataManagementProductItem) : offerItemFromExternal(dataManagementProductItem)
-//    def externalServicesOfferItem = affiliationCategory == AffiliationCategory.INTERNAL ? offerItemFromInternal(externalServicesProductItem) : offerItemFromExternal(externalServicesProductItem)
-//
-//
-//    OfferV2 offerWithItems = createFilledOffer(
-//            [
-//                    dataGenerationProductItem,
-//                    dataAnalysisProductItem,
-//                    dataManagementProductItem,
-//                    externalServicesProductItem
-//            ], affiliation)
-//    when: "the net prices are recomputed"
-//    OfferV2 offerWithNet = withTotalDiscount(offerWithItems)
-//    then: "the offer discount is the sum of all its item's discounts"
-//    offerWithNet.getTotalDiscountAmount() == [
-//            dataGenerationOfferItem.getItemDiscount(),
-//            dataAnalysisOfferItem.getItemDiscount(),
-//            dataManagementOfferItem.getItemDiscount(),
-//            externalServicesOfferItem.getItemDiscount()
-//    ].stream().map(BigDecimal::valueOf).reduce(BigDecimal.ZERO, BigDecimal::add)
-//
-//    where: "the affiliation category varies"
-//    affiliationCategory << AffiliationCategory.values()
-//    affiliation = createAffiliation(affiliationCategory as AffiliationCategory, "Germany")
+  private static OfferV2 createOfferWithItemsOfEachCategory(AffiliationCategory affiliationCategory, String country) {
+    OfferV2 offer = new OfferV2()
+    offer.setSelectedCustomerAffiliation(new Affiliation("", "", "", "", "", country, affiliationCategory))
+    def dataGeneration = createDataGenerationItem(offer)
+    def dataAnalysis = createDataAnalysisItem(offer)
+    def dataManagement = createDataManagementItem(offer)
+    def externalServices = createExternalServiceItem(offer)
+    offer.setItems([dataGeneration, dataAnalysis, dataManagement, externalServices])
+    return offer
   }
-
-  def "when overheads are computed for an offer, then the item group overhead price equals GroupNet * #expectedRatio for #affiliationCategory affiliations and the total overhead is the sum of the group overheads"() {
-//    given: "an offer with items"
-//    OfferV2 offerWithItems = createFilledOffer(
-//            [
-//                    createDataGenerationProductItem(20.9, 5.6),
-//                    createDataAnalysisProductItem(20.9, 5.6),
-//                    createDataManagementProductItem(20.9, 5.6),
-//                    createExternalServicesProductItem(20.9, 5.6)
-//            ], affiliation)
-//
-//    and: "the offer only contains invalid overheads"
-//    offerWithItems.setOverhead(-1)
-//    offerWithItems.setOverheadRatio(-1)
-//    offerWithItems.setOverheadsDataAnalysis(-1.0)
-//    offerWithItems.setOverheadsDataGeneration(-1.0)
-//    offerWithItems.setOverheadsDataManagement(-1.0)
-//    offerWithItems.setOverheadsExternalServices(-1.0)
-//    when: "the overheads are recomputed"
-//    OfferV2 offerWithOverheads = withOverheads(offerWithItems)
-//
-//    OfferV2 offerWithNetPrices = withNetPrices(offerWithItems)
-//    then: "the group overheads are correct"
-//    offerWithOverheads.getOverheadsDataGeneration() == (offerWithNetPrices.getNetSumDataGeneration() * expectedRatio).setScale(2, RoundingMode.HALF_UP)
-//    offerWithOverheads.getOverheadsDataAnalysis() == (offerWithNetPrices.getNetSumDataAnalysis() * expectedRatio).setScale(2, RoundingMode.HALF_UP)
-//    offerWithOverheads.getOverheadsDataManagement() == (offerWithNetPrices.getNetSumDataManagement() * expectedRatio).setScale(2, RoundingMode.HALF_UP)
-//    offerWithOverheads.getOverheadsExternalServices() == (offerWithNetPrices.getNetSumExternalServices() * expectedRatio).setScale(2, RoundingMode.HALF_UP)
-//
-//    and: "the total offer overhead is correct"
-//    def expectedOverhead = offerWithOverheads.getOverheadsDataGeneration()
-//            .add(offerWithOverheads.getOverheadsDataAnalysis())
-//            .add(offerWithOverheads.getOverheadsDataManagement())
-//            .add(offerWithOverheads.getOverheadsExternalServices())
-//            .doubleValue()
-//    offerWithOverheads.getOverhead() == expectedOverhead
-//
-//    where:
-//    affiliationCategory << AffiliationCategory.values()
-//    affiliation = createAffiliation(affiliationCategory as AffiliationCategory, "Germany")
-//    expectedRatio = overheadRatio(affiliationCategory as AffiliationCategory)
-  }
-  // Upon offer update:
-/*
-  def "when an offer is processed, then the total net is correct for #affiliationCategory affiliations"() {
-    given: "an unprocessed offer with items"
-    OfferV2 unprocessedOffer = createUnprocessedOffer(affiliationCategory as AffiliationCategory)
-    and: "an offer with filled net prices"
-    OfferV2 offerWithNets = withNetPrices(withGroupedOfferItems(unprocessedOffer))
-
-    expect: "after processing the total net is correct"
-    process(unprocessedOffer).getTotalNetPrice() == offerWithNets.getTotalNetPrice()
-
-    where:
-    affiliationCategory << AffiliationCategory.values()
-  }
-
-  def "when an offer is processed, then the total discount is correct for #affiliationCategory affiliations"() {
-    given: "an unprocessed offer with items"
-    OfferV2 unprocessedOffer = createUnprocessedOffer(affiliationCategory as AffiliationCategory)
-    and: "an offer with filled net prices"
-    OfferV2 offerWithTotalDiscount = withTotalDiscount(withGroupedOfferItems(unprocessedOffer))
-
-    expect: "after processing the total net is correct"
-    process(unprocessedOffer).getTotalDiscountAmount() == offerWithTotalDiscount.getTotalDiscountAmount()
-
-    where:
-    affiliationCategory << AffiliationCategory.values()
-  }
-
-  def "when an offer is processed, then the group nets are correct for #affiliationCategory affiliations"() {
-    given: "an unprocessed offer with items"
-    OfferV2 unprocessedOffer = createUnprocessedOffer(affiliationCategory as AffiliationCategory)
-    and: "an offer with filled net prices"
-    OfferV2 offerWithNets = withNetPrices(withGroupedOfferItems(unprocessedOffer))
-
-    when: "the offer was processed"
-    OfferV2 processedOffer = process(unprocessedOffer)
-
-    then: "the group net prices are correct"
-    processedOffer.getNetSumDataGeneration() == offerWithNets.getNetSumDataGeneration()
-    processedOffer.getNetSumDataAnalysis() == offerWithNets.getNetSumDataAnalysis()
-    processedOffer.getNetSumDataManagement() == offerWithNets.getNetSumDataManagement()
-    processedOffer.getNetSumExternalServices() == offerWithNets.getNetSumExternalServices()
-
-    where:
-    affiliationCategory << AffiliationCategory.values()
-  }
-
-  def "when an offer is processed, then the group overheads are correct for #affiliationCategory affiliations"() {
-    given: "an unprocessed offer with items"
-    OfferV2 unprocessedOffer = createUnprocessedOffer(affiliationCategory as AffiliationCategory)
-    and: "an offer with filled net prices"
-    OfferV2 offerWithOverheads = withOverheads(withGroupedOfferItems(unprocessedOffer))
-
-    when: "the offer was processed"
-    OfferV2 processedOffer = process(unprocessedOffer)
-
-    then: "the group net prices are correct"
-    processedOffer.getOverheadsDataGeneration() == offerWithOverheads.getOverheadsDataGeneration()
-    processedOffer.getOverheadsDataAnalysis() == offerWithOverheads.getOverheadsDataAnalysis()
-    processedOffer.getOverheadsDataManagement() == offerWithOverheads.getOverheadsDataManagement()
-    processedOffer.getOverheadsExternalServices() == offerWithOverheads.getOverheadsExternalServices()
-
-    where:
-    affiliationCategory << AffiliationCategory.values()
-  }
-
-  def "when an offer is processed, then the total overhead is correct for #affiliationCategory affiliations"() {
-    given: "an unprocessed offer with items"
-    OfferV2 unprocessedOffer = createUnprocessedOffer(affiliationCategory as AffiliationCategory)
-    and: "an offer with filled net prices"
-    OfferV2 offerWithOverheads = withOverheads(withGroupedOfferItems(unprocessedOffer))
-
-    expect: "after processing the total net is correct"
-    process(unprocessedOffer).getOverhead() == offerWithOverheads.getOverhead()
-
-    where:
-    affiliationCategory << AffiliationCategory.values()
-  }
-
-  def "when a german offer is processed, then the vat is #vatRatioString"() {
-    given: "an unprocessed offer with items"
-    OfferV2 unprocessedOffer = createUnprocessedOffer(affiliationCategory as AffiliationCategory, country)
-    and: "an offer with filled VAT prices"
-    OfferV2 offerWithVat = withVat(withNetPrices(withGroupedOfferItems(unprocessedOffer)))
-
-    when: "the offer is processed"
-
-    OfferV2 processedOffer = process(unprocessedOffer)
-    then: "the vat ratio is correct"
-    processedOffer.getVatRatio() == expectedVatRatio
-    and: "the calculated vat is correct"
-    processedOffer.getTotalVat() == offerWithVat.getTotalVat()
-    where:
-    country = "Germany"
-    expectedVatRatio = vatRatio(country)
-    affiliationCategory << AffiliationCategory.values()
-    vatRatioString = "${expectedVatRatio*100.0}%"
-
-  }
-
-  def "when a foreign offer is processed, then the vat is #vatRatioString for #country"() {
-    given: "an unprocessed offer with items"
-    OfferV2 unprocessedOffer = createUnprocessedOffer(affiliationCategory as AffiliationCategory, country)
-    and: "an offer with filled VAT prices"
-    OfferV2 offerWithVat = withVat(withNetPrices(withGroupedOfferItems(unprocessedOffer)))
-
-    when: "the offer is processed"
-
-    OfferV2 processedOffer = process(unprocessedOffer)
-    then: "the vat ratio is correct"
-    processedOffer.getVatRatio() == expectedVatRatio
-    and: "the calculated vat is correct"
-    processedOffer.getTotalVat() == offerWithVat.getTotalVat()
-
-    where:
-    affiliationCategory << AffiliationCategory.values()
-    country = "Not-Germany"
-    expectedVatRatio = vatRatio(country)
-    vatRatioString = "${expectedVatRatio*100}%"
-  }*/
 }
