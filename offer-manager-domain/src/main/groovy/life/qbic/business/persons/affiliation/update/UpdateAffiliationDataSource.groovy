b0502465e10eb2c4b1680d30c33ac7e2a528fb06
Tobias Koch <KochTobi@users.noreply.github.com>
2022-04-05 12:38:59 +0200

    Write AffiliationDbConnector (#925)
    * Write AffiliationDbConnector
    
    Copy methods from PersonDbConnector only concerning affiliations
    
    * Replace PersonDbConnector affiliation handling
    
    * Remove affiliation methods from PersonDbConnector

--- offer-manager-domain/src/main/groovy/life/qbic/business/persons/affiliation/update/UpdateAffiliationDataSource.groovy
+++ offer-manager-domain/src/main/groovy/life/qbic/business/persons/affiliation/update/UpdateAffiliationDataSource.groovy
@@ -14,7 +14,8 @@ interface UpdateAffiliationDataSource {
     /**
      * Makes changes to an existing persistent affiliation
      * @param affiliation the affiliation to be updated
-     * @throws DatabaseQueryException
+     * @throws DatabaseQueryException in case of technical errors
+     * @throws AffiliationNotFoundException in case the affiliation was not found
      */
     void updateAffiliation(Affiliation affiliation) throws DatabaseQueryException, AffiliationNotFoundException
 }
