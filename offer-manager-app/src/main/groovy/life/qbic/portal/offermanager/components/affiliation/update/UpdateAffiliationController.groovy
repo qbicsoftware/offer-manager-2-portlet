e7f7ad225ed0948922c77bfaf93835531b949143
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-04-06 15:19:47 +0200

    Reformat code

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/affiliation/update/UpdateAffiliationController.groovy
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/affiliation/update/UpdateAffiliationController.groovy
@@ -3,11 +3,11 @@ package life.qbic.portal.offermanager.components.affiliation.update
 import life.qbic.business.persons.affiliation.Affiliation
 import life.qbic.business.persons.affiliation.update.UpdateAffiliationInput
 
-class UpdateAffiliationController implements UpdateAffiliationInput{
+class UpdateAffiliationController implements UpdateAffiliationInput {
 
 
-    @Override
-    void updateAffiliation(Affiliation affiliation) {
+  @Override
+  void updateAffiliation(Affiliation affiliation) {
 
-    }
+  }
 }
