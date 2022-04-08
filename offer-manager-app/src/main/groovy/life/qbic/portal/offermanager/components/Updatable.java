daac48a2b95d41c99ad58a73b78a6daeded394ff
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2022-04-08 16:13:05 +0200

    Add JavaDoc

--- offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/Updatable.java
+++ offer-manager-app/src/main/groovy/life/qbic/portal/offermanager/components/Updatable.java
@@ -1,4 +1,9 @@
 package life.qbic.portal.offermanager.components;
+
+/**
+ * Classes implementing this interface can be updated with a given object type
+ * @param <T> the type of object that can be consumed for an update
+ */
 public interface Updatable<T> {
-  public void update(T value);
+  void update(T value);
 }
